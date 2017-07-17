9c782384f7bddb0ebbdbf3429bad4f8e78f70aeb
mkim0710 <mkim0710@users.noreply.github.com>
2017-07-17 00:44:13 -0400

    Create tidyfunctions.source.r

---
+++ tidyfunctions.source.r
@@ -0,0 +1,9 @@
+function.sequence_with_leading_zeros = function(num) {
+  digits_with_leading_zeros = trunc(log10(num)) + 1
+  sprintf(paste0("%0",digits_with_leading_zeros,"d"), 1:num)
+}
+
+# > function.sequence_with_leading_zeros(9.9)
+# [1] "1" "2" "3" "4" "5" "6" "7" "8" "9"
+# > function.sequence_with_leading_zeros(10)
+#  [1] "01" "02" "03" "04" "05" "06" "07" "08" "09" "10"
