3fbc884414c4b1c3be0082e42ba9729719cd35bf
mkim0710 <mkim0710@users.noreply.github.com>
2017-07-18 18:02:52 -0400

    Create function.deparse.cat.width.source.r

---
+++ function.deparse.cat.width.source.r
@@ -0,0 +1,13 @@
+function.deparse.cat.width = function(x, width.cutoff = 500) {
+  # source("https://github.com/mkim0710/tidystat/raw/master/function.deparse.cat.width.source.r")
+  cat(deparse(x, width.cutoff = width.cutoff), sep = "\n")
+}
+
+#@ test) function.deparse.cat.width() --------
+# > dput(letters)
+# c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+# "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 
+# "z")
+# > function.deparse.cat.width(letters)
+# c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
