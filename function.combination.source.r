c71cc1d6c66b66a3406e789a380fbeb9d083d38d
mkim0710 <mkim0710@gmail.com>
2017-07-25 23:14:27 -0400

    function.combination

---
+++ function.combination.source.r
@@ -0,0 +1,15 @@
+function.permutation = function(n, x) {
+  return(factorial(n) / factorial(n-x))
+}
+
+function.combination = function(n, x) {
+  return(factorial(n) / (factorial(x) * factorial(n-x)))
+}
+
+# #@ test -----
+# function.permutation(4,0:4)
+# function.combination(4,0:4)
+# # > function.permutation(4,0:4)
+# # [1]  1  4 12 24 24
+# # > function.combination(4,0:4)
+# # [1] 1 4 6 4 1
