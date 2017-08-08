a1a9692027a0cbdb36ce2fd6d05c45c1052655b2
mkim0710 <mkim0710@users.noreply.github.com>
2017-08-08 17:52:41 -0400

    Update function.rz_transformation.source.r

--- function.rz_transformation.source.r
+++ function.rz_transformation.source.r
@@ -9,8 +9,8 @@ function.rz_diff = function(r1, r2, n1, n2 = NULL) {
     if(is.null(n2)) {
         n2 = n1
     }
-    F1 = .5 * log ( (1+r1)/(1-r1) )
-    F2 = .5 * log ( (1+r2)/(1-r2) )
-    rz_diff = (F1 - F2) / ( 1/(n1-3) + 1/(n2-3) )^.5
+    rz1 = .5 * log ( (1+r1)/(1-r1) )
+    rz2 = .5 * log ( (1+r2)/(1-r2) )
+    rz_diff = (rz1 - rz2) / ( 1/(n1-3) + 1/(n2-3) )^.5
 }
 
