469884641e24cc609a8dbfc1a73418ca7a8178b5
mkim0710 <mkim0710@users.noreply.github.com>
2017-08-08 17:52:04 -0400

    Update function.rz_transformation.source.r

--- function.rz_transformation.source.r
+++ function.rz_transformation.source.r
@@ -5,12 +5,12 @@ function.Fisher_rz_Transform = function(r) {
     .5 * log ( (1+r)/(1-r) )
 }
 
-function.z_diff = function(r1, r2, n1, n2 = NULL) {
+function.rz_diff = function(r1, r2, n1, n2 = NULL) {
     if(is.null(n2)) {
         n2 = n1
     }
     F1 = .5 * log ( (1+r1)/(1-r1) )
     F2 = .5 * log ( (1+r2)/(1-r2) )
-    z_diff = (F1 - F2) / ( 1/(n1-3) + 1/(n2-3) )^.5
+    rz_diff = (F1 - F2) / ( 1/(n1-3) + 1/(n2-3) )^.5
 }
 
