1977e640f8a7d7fdc4028babce4f55dff818a591
mkim0710 <mkim0710@gmail.com>
2017-08-28 19:35:04 -0400

    function.coxph_object.summary.OR

--- function.lm_object.summary.coefCI.source.r
+++ function.lm_object.summary.coefCI.source.r
@@ -181,77 +181,216 @@ glm_object %>% function.glm_object.summary.OR
 
 
 
-function.clogit_object.summary.OR = function(clogit_object, sprintf_fmt_decimal = 2, coef.exp = T) {
+# function.clogit_object.summary.OR = function(clogit_object, sprintf_fmt_decimal = 2, coef.exp = T) {
+#     # source("https://github.com/mkim0710/tidystat/raw/master/function.lm_object.summary.coefCI.source.r")
+#     if(!"clogit" %in% class(clogit_object)) {
+#         stop("!\"clogit\" %in% class(clogit_object)")
+#     }
+#     library(tidyverse)
+#     library(survival)
+#     clogit_object.confint.df = as.data.frame(confint(clogit_object))
+#     clogit_object.summary.coef.df = as.data.frame(coef(summary(clogit_object))) %>% rownames_to_column
+#     if (coef.exp == T) {
+#         clogit_object.confint.df = exp(clogit_object.confint.df)
+#         clogit_object.summary.coef.df$coef = exp(clogit_object.summary.coef.df$coef)
+#     }
+# 
+#     # # https://stackoverflow.com/questions/3443687/formatting-decimal-places-in-r
+#     # tmp.df = data.frame(coefCI = paste0(
+#     #     format(round(clogit_object.summary.coef.df$coef, round_digits), nsmall = round_digits)
+#     #     , " ("
+#     #     , format(round(clogit_object.confint.df$`2.5 %`, round_digits), nsmall = round_digits)
+#     #     , " ~ "
+#     #     , format(round(clogit_object.confint.df$`97.5 %`, round_digits), nsmall = round_digits)
+#     #     , ")"
+#     # ), stringsAsFactors = F)
+#     # # http://www.dummies.com/programming/r/how-to-format-numbers-in-r/
+#     # tmp.df = data.frame(coefCI = paste0(
+#     #     format(clogit_object.summary.coef.df$coef, digits = format_digits, nsmall = format_digits)
+#     #     , " ("
+#     #     , format(clogit_object.confint.df$`2.5 %`, digits = format_digits, nsmall = format_digits)
+#     #     , " ~ "
+#     #     , format(clogit_object.confint.df$`97.5 %`, digits = format_digits, nsmall = format_digits)
+#     #     , ")"
+#     # ), stringsAsFactors = F)
+#     # http://www.dummies.com/programming/r/how-to-format-numbers-in-r/
+#     # digits_total_incl_decimal = sprintf_fmt_decimal + 1 + (max(c(clogit_object.summary.coef.df$coef, clogit_object.confint.df$`2.5 %`, clogit_object.confint.df$`97.5 %`), na.rm = T) %>% log10 %>% trunc) + 1
+#     tmp.vec = c(clogit_object.summary.coef.df$coef, clogit_object.confint.df$`2.5 %`, clogit_object.confint.df$`97.5 %`)
+#     tmp.vec[is.infinite(tmp.vec)] = NA
+#     digits_total_incl_decimal = sprintf_fmt_decimal + 1 + (max(tmp.vec, na.rm = T) %>% log10 %>% trunc) + 1
+#     if (any(c(clogit_object.summary.coef.df$coef, clogit_object.confint.df$`2.5 %`, clogit_object.confint.df$`97.5 %`) < 0, na.rm = T) ) {
+#         digits_total_incl_decimal = digits_total_incl_decimal + 1
+#     }
+# 
+#     tmp.df = data.frame(coefCI = paste0(
+#         sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), clogit_object.summary.coef.df$coef)
+#         , " ("
+#         , sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), clogit_object.confint.df$`2.5 %`)
+#         , " ~ "
+#         , sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), clogit_object.confint.df$`97.5 %`)
+#         , ")"
+#     ), stringsAsFactors = F)
+#     # tmp.df$p_value = clogit_object.summary.coef.df$`Pr(>|z|)` %>% round(3) %>% as.character
+#     tmp.df$p_value = sprintf("%.3f", clogit_object.summary.coef.df$`Pr(>|z|)`)
+#     tmp.df$p_value[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.001] = "<0.001"
+# 
+#     tmp.df$star = "   "
+#     tmp.df$star[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.05] = "*  "
+#     tmp.df$star[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.01] = "** "
+#     tmp.df$star[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.001] = "***"
+# 
+#     out = cbind(
+#         clogit_object.summary.coef.df$rowname
+#         , tmp.df
+#         , clogit_object.summary.coef.df[ , 1:2]
+#         , clogit_object.confint.df
+#         , clogit_object.summary.coef.df[ , -1:(-2)]
+#         , stringsAsFactors = F
+#     )
+#     out$rowname = NULL
+#     names(out) [1] = "rowname"
+#     if (coef.exp == T) {
+#         out$`exp(coef)` = NULL
+#         names(out) [2] = "ORCI"
+#         names(out) [5] = "exp(coef)"
+#         names(out) [6] = "exp(2.5 %)"
+#         names(out) [7] = "exp(97.5 %)"
+#     }
+#     # out = out %>% as.tibble
+#     row.names(out) = NULL
+#     out
+# }
+# 
+# 
+# 
+# #@ test) function.clogit_object.summary.OR() ----
+# library(survival)
+# logan2 = data.frame(
+#     survival::logan[rep(1:nrow(survival::logan), length(levels(survival::logan$occupation))), ]
+#     , id = rep(1:nrow(survival::logan), length(levels(survival::logan$occupation)))
+#     , tocc = factor(rep(levels(survival::logan$occupation), each = nrow(survival::logan)))
+# )
+# logan2$case = (logan2$occupation == logan2$tocc)
+# logan2 = logan2[order(logan2$id), ]
+# logan2 %>% as.tibble
+# # > logan2 %>% as.tibble
+# # # A tibble: 4,190 x 7
+# #    occupation         focc education      race    id         tocc  case
+# #  *     <fctr>       <fctr>     <int>    <fctr> <int>       <fctr> <lgl>
+# #  1      sales professional        14 non-black     1         farm FALSE
+# #  2      sales professional        14 non-black     1   operatives FALSE
+# #  3      sales professional        14 non-black     1    craftsmen FALSE
+# #  4      sales professional        14 non-black     1        sales  TRUE
+# #  5      sales professional        14 non-black     1 professional FALSE
+# #  6  craftsmen        sales        13 non-black     2         farm FALSE
+# #  7  craftsmen        sales        13 non-black     2   operatives FALSE
+# #  8  craftsmen        sales        13 non-black     2    craftsmen  TRUE
+# #  9  craftsmen        sales        13 non-black     2        sales FALSE
+# # 10  craftsmen        sales        13 non-black     2 professional FALSE
+# # # ... with 4,180 more rows
+# clogit_object = clogit(case ~ tocc + tocc:education + strata(id), logan2)
+# clogit_object %>% function.clogit_object.summary.OR
+# # > clogit_object = clogit(case ~ tocc + tocc:education + strata(id), logan2)
+# # Warning message:
+# # In coxph(formula = Surv(rep(1, 4190L), case) ~ tocc + tocc:education +  :
+# #   X matrix deemed to be singular; variable 9
+# # > clogit_object %>% function.clogit_object.summary.OR
+# #                      rowname               ORCI p_value star    exp(coef)   exp(2.5 %) exp(97.5 %)   se(coef)          z     Pr(>|z|)
+# # 1                   toccfarm 0.15 (0.01 ~ 2.25)   0.170      0.1500985887 1.002427e-02  2.24750455 1.38078224  -1.373470 1.696063e-01
+# # 2             toccoperatives 3.21 (1.06 ~ 9.73)   0.039  *   3.2115387998 1.059825e+00  9.73178121 0.56564649   2.062684 3.914262e-02
+# # 3           toccprofessional 0.00 (0.00 ~ 0.00)  <0.001  *** 0.0003033725 7.712957e-05  0.00119325 0.69872442 -11.593339 0.000000e+00
+# # 4                  toccsales 0.01 (0.00 ~ 0.03)  <0.001  *** 0.0065438496 1.446554e-03  0.02960274 0.77008623  -6.530736 6.544743e-11
+# # 5    tocccraftsmen:education 0.72 (0.64 ~ 0.80)  <0.001  *** 0.7172834608 6.416297e-01  0.80185742 0.05686824  -5.843053 5.125262e-09
+# # 6         toccfarm:education 0.69 (0.55 ~ 0.87)   0.001  **  0.6905369692 5.496656e-01  0.86751158 0.11640997  -3.180877 1.468301e-03
+# # 7   toccoperatives:education 0.66 (0.58 ~ 0.74)  <0.001  *** 0.6555905786 5.846481e-01  0.73514131 0.05843284  -7.225710 4.984901e-13
+# # 8 toccprofessional:education 1.32 (1.20 ~ 1.46)  <0.001  *** 1.3208122354 1.195121e+00  1.45972295 0.05102123   5.453550 4.937392e-08
+# # 9        toccsales:education   NA (  NA ~   NA)      NA                NA           NA          NA 0.00000000         NA           NA
+
+function.coxph_object.summary.OR = function(coxph_object, sprintf_fmt_decimal = 2, coef.exp = T, is.clogit = "unknown") {
     # source("https://github.com/mkim0710/tidystat/raw/master/function.lm_object.summary.coefCI.source.r")
-    if(!"clogit" %in% class(clogit_object)) {
-        stop("!\"clogit\" %in% class(clogit_object)")
+    if (!"coxph" %in% class(coxph_object)) {
+        stop("!\"coxph\" %in% class(coxph_object)")
     }
+    if (is.clogit == "unknown") {
+        if ("clogit" %in% class(coxph_object)) {
+            warning("\"clogit\" %in% class(coxph_object)")
+            is.clogit = T
+        }
+    } else if (is.clogit == T) {
+        if (!"clogit" %in% class(coxph_object)) {
+            stop("!\"clogit\" %in% class(coxph_object)")
+        }
+    } 
     library(tidyverse)
     library(survival)
-    clogit_object.confint.df = as.data.frame(confint(clogit_object))
-    clogit_object.summary.coef.df = as.data.frame(coef(summary(clogit_object))) %>% rownames_to_column
+    coxph_object.confint.df = as.data.frame(confint(coxph_object))
+    coxph_object.summary.coef.df = as.data.frame(coef(summary(coxph_object))) %>% rownames_to_column
     if (coef.exp == T) {
-        clogit_object.confint.df = exp(clogit_object.confint.df)
-        clogit_object.summary.coef.df$coef = exp(clogit_object.summary.coef.df$coef)
+        coxph_object.confint.df = exp(coxph_object.confint.df)
+        coxph_object.summary.coef.df$coef = exp(coxph_object.summary.coef.df$coef)
     }
 
     # # https://stackoverflow.com/questions/3443687/formatting-decimal-places-in-r
     # tmp.df = data.frame(coefCI = paste0(
-    #     format(round(clogit_object.summary.coef.df$coef, round_digits), nsmall = round_digits)
+    #     format(round(coxph_object.summary.coef.df$coef, round_digits), nsmall = round_digits)
     #     , " ("
-    #     , format(round(clogit_object.confint.df$`2.5 %`, round_digits), nsmall = round_digits)
+    #     , format(round(coxph_object.confint.df$`2.5 %`, round_digits), nsmall = round_digits)
     #     , " ~ "
-    #     , format(round(clogit_object.confint.df$`97.5 %`, round_digits), nsmall = round_digits)
+    #     , format(round(coxph_object.confint.df$`97.5 %`, round_digits), nsmall = round_digits)
     #     , ")"
     # ), stringsAsFactors = F)
     # # http://www.dummies.com/programming/r/how-to-format-numbers-in-r/
     # tmp.df = data.frame(coefCI = paste0(
-    #     format(clogit_object.summary.coef.df$coef, digits = format_digits, nsmall = format_digits)
+    #     format(coxph_object.summary.coef.df$coef, digits = format_digits, nsmall = format_digits)
     #     , " ("
-    #     , format(clogit_object.confint.df$`2.5 %`, digits = format_digits, nsmall = format_digits)
+    #     , format(coxph_object.confint.df$`2.5 %`, digits = format_digits, nsmall = format_digits)
     #     , " ~ "
-    #     , format(clogit_object.confint.df$`97.5 %`, digits = format_digits, nsmall = format_digits)
+    #     , format(coxph_object.confint.df$`97.5 %`, digits = format_digits, nsmall = format_digits)
     #     , ")"
     # ), stringsAsFactors = F)
     # http://www.dummies.com/programming/r/how-to-format-numbers-in-r/
-    # digits_total_incl_decimal = sprintf_fmt_decimal + 1 + (max(c(clogit_object.summary.coef.df$coef, clogit_object.confint.df$`2.5 %`, clogit_object.confint.df$`97.5 %`), na.rm = T) %>% log10 %>% trunc) + 1
-    tmp.vec = c(clogit_object.summary.coef.df$coef, clogit_object.confint.df$`2.5 %`, clogit_object.confint.df$`97.5 %`)
+    # digits_total_incl_decimal = sprintf_fmt_decimal + 1 + (max(c(coxph_object.summary.coef.dfcoef,coxphobject.confint.df`2.5 %`, coxph_object.confint.df$`97.5 %`), na.rm = T) %>% log10 %>% trunc) + 1
+    tmp.vec = c(coxph_object.summary.coef.df$coef, coxph_object.confint.df$`2.5 %`, coxph_object.confint.df$`97.5 %`)
     tmp.vec[is.infinite(tmp.vec)] = NA
     digits_total_incl_decimal = sprintf_fmt_decimal + 1 + (max(tmp.vec, na.rm = T) %>% log10 %>% trunc) + 1
-    if (any(c(clogit_object.summary.coef.df$coef, clogit_object.confint.df$`2.5 %`, clogit_object.confint.df$`97.5 %`) < 0, na.rm = T) ) {
+    if (any(c(coxph_object.summary.coef.df$coef, coxph_object.confint.df$`2.5 %`, coxph_object.confint.df$`97.5 %`) < 0, na.rm = T) ) {
         digits_total_incl_decimal = digits_total_incl_decimal + 1
     }
 
     tmp.df = data.frame(coefCI = paste0(
-        sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), clogit_object.summary.coef.df$coef)
+        sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), coxph_object.summary.coef.df$coef)
         , " ("
-        , sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), clogit_object.confint.df$`2.5 %`)
+        , sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), coxph_object.confint.df$`2.5 %`)
         , " ~ "
-        , sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), clogit_object.confint.df$`97.5 %`)
+        , sprintf(paste0("%", digits_total_incl_decimal, ".", sprintf_fmt_decimal, "f"), coxph_object.confint.df$`97.5 %`)
         , ")"
     ), stringsAsFactors = F)
-    # tmp.df$p_value = clogit_object.summary.coef.df$`Pr(>|z|)` %>% round(3) %>% as.character
-    tmp.df$p_value = sprintf("%.3f", clogit_object.summary.coef.df$`Pr(>|z|)`)
-    tmp.df$p_value[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.001] = "<0.001"
+    # tmp.dfpvalue=coxphobject.summary.coef.df`Pr(>|z|)` %>% round(3) %>% as.character
+    tmp.df$p_value = sprintf("%.3f", coxph_object.summary.coef.df$`Pr(>|z|)`)
+    tmp.df$p_value[coxph_object.summary.coef.df$`Pr(>|z|)` <= 0.001] = "<0.001"
 
     tmp.df$star = "   "
-    tmp.df$star[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.05] = "*  "
-    tmp.df$star[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.01] = "** "
-    tmp.df$star[clogit_object.summary.coef.df$`Pr(>|z|)` <= 0.001] = "***"
+    tmp.df$star[coxph_object.summary.coef.df$`Pr(>|z|)` <= 0.05] = "*  "
+    tmp.df$star[coxph_object.summary.coef.df$`Pr(>|z|)` <= 0.01] = "** "
+    tmp.df$star[coxph_object.summary.coef.df$`Pr(>|z|)` <= 0.001] = "***"
 
     out = cbind(
-        clogit_object.summary.coef.df$rowname
+        coxph_object.summary.coef.df$rowname
         , tmp.df
-        , clogit_object.summary.coef.df[ , 1:2]
-        , clogit_object.confint.df
-        , clogit_object.summary.coef.df[ , -1:(-2)]
+        , coxph_object.summary.coef.df[ , 1:2]
+        , coxph_object.confint.df
+        , coxph_object.summary.coef.df[ , -1:(-2)]
         , stringsAsFactors = F
     )
     out$rowname = NULL
     names(out) [1] = "rowname"
     if (coef.exp == T) {
         out$`exp(coef)` = NULL
-        names(out) [2] = "ORCI"
+        if (is.clogit == T) {
+            names(out) [2] = "ORCI"
+        } else {
+            names(out) [2] = "HRCI"
+        }
         names(out) [5] = "exp(coef)"
         names(out) [6] = "exp(2.5 %)"
         names(out) [7] = "exp(97.5 %)"
@@ -261,9 +400,8 @@ function.clogit_object.summary.OR = function(clogit_object, sprintf_fmt_decimal 
     out
 }
 
-
-
-#@ test) function.clogit_object.summary.OR() ----
+#@ test) function.clogit_object.summary.OR() - clogit object ----
+library(tidyverse)
 library(survival)
 logan2 = data.frame(
     survival::logan[rep(1:nrow(survival::logan), length(levels(survival::logan$occupation))), ]
@@ -289,12 +427,12 @@ logan2 %>% as.tibble
 # 10  craftsmen        sales        13 non-black     2 professional FALSE
 # # ... with 4,180 more rows
 clogit_object = clogit(case ~ tocc + tocc:education + strata(id), logan2)
-clogit_object %>% function.clogit_object.summary.OR
+clogit_object %>% function.coxph_object.summary.OR
 # > clogit_object = clogit(case ~ tocc + tocc:education + strata(id), logan2)
 # Warning message:
 # In coxph(formula = Surv(rep(1, 4190L), case) ~ tocc + tocc:education +  :
 #   X matrix deemed to be singular; variable 9
-# > clogit_object %>% function.clogit_object.summary.OR
+# > clogit_object %>% function.coxph_object.summary.OR
 #                      rowname               ORCI p_value star    exp(coef)   exp(2.5 %) exp(97.5 %)   se(coef)          z     Pr(>|z|)
 # 1                   toccfarm 0.15 (0.01 ~ 2.25)   0.170      0.1500985887 1.002427e-02  2.24750455 1.38078224  -1.373470 1.696063e-01
 # 2             toccoperatives 3.21 (1.06 ~ 9.73)   0.039  *   3.2115387998 1.059825e+00  9.73178121 0.56564649   2.062684 3.914262e-02
@@ -305,6 +443,12 @@ clogit_object %>% function.clogit_object.summary.OR
 # 7   toccoperatives:education 0.66 (0.58 ~ 0.74)  <0.001  *** 0.6555905786 5.846481e-01  0.73514131 0.05843284  -7.225710 4.984901e-13
 # 8 toccprofessional:education 1.32 (1.20 ~ 1.46)  <0.001  *** 1.3208122354 1.195121e+00  1.45972295 0.05102123   5.453550 4.937392e-08
 # 9        toccsales:education   NA (  NA ~   NA)      NA                NA           NA          NA 0.00000000         NA           NA
+# Warning message:
+# In function.coxph_object.summary.OR(.) : "clogit" %in% class(coxph_object)
+
+
+
+#@ end -----
 
 
 
