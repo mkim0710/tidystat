cf22e1cda5742624e2101b276852252a28ef515f
mkim0710 <mkim0710@gmail.com>
2017-08-29 16:41:41 -0400

    data.NA.recode.source.r

---
+++ data.NA.recode.source.r
@@ -0,0 +1,30 @@
+# data.NA.recode.source.r
+
+data.NA.recode = function(data, numeric_NA_recode_into_0 = T, character_NA_recode_into = "", reorder_factor = T) {
+    # source("https://github.com/mkim0710/tidystat/raw/master/data.strata_list.Match.source.r")
+    library(tidyverse)
+    out = data %>% map_df(function(x){
+        if (is.numeric(x)) {
+            if (numeric_NA_recode_into_0 == T) {
+                x[is.na(x)] = 0
+            }
+        }
+        if (is.character(x)) {
+            x[is.na(x)] = character_NA_recode_into
+        }
+        if (is.factor(x)) {
+            if (reorder_factor == T) {
+                x = x %>% as.character
+                x[is.na(x)] = character_NA_recode_into
+                x = x %>% as.factor  # In this way, "" will be the first level
+            } else {
+                x = x %>% factor(exclude = NULL)
+                levels(x)[is.na(levels(x))] = character_NA_recode_into  # In this way, "character_NA_recode_into" will be the first level
+            }
+        }
+        x
+    })
+    out
+}
+
+#@ end ----
