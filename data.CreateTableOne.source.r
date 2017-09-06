5a96fcfdba7afe6f8ef05e1eed6c2113e992c323
mkim0710 <mkim0710@gmail.com>
2017-09-05 21:32:41 -0400

    data.CreateTableOne.source.r

--- data.CreateTableOne.source.r
+++ data.CreateTableOne.source.r
@@ -30,4 +30,246 @@ data.CreateTableOne.by_exposure %>% print(smd = T, nonnormal = vars4IQR, exact =
 
 
 
+#@ Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne -----
 
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% str(max.level = 1)
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% str(max.level = 1)
+# List of 5
+#  $ _3yr:List of 3
+#   ..- attr(*, "class")= chr "TableOne"
+#  $ _4yr:List of 3
+#   ..- attr(*, "class")= chr "TableOne"
+#  $ _5yr:List of 3
+#   ..- attr(*, "class")= chr "TableOne"
+#  $ _6yr:List of 3
+#   ..- attr(*, "class")= chr "TableOne"
+#  $ _7yr:List of 3
+#   ..- attr(*, "class")= chr "TableOne"
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable$`TRUE`$enddate.is
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable$`TRUE`$enddate.is %>% str
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable$`TRUE`$enddate.is %>% filter(level == "DTH_MDY")
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable$`TRUE`$enddate.is
+#       n miss p.miss          level  freq   percent cum.percent
+# 1 53377    0      0     2013-12-31  1163  2.178841    2.178841
+# 2 53377    0      0        DTH_MDY  4920  9.217453   11.396294
+# 3 53377    0      0   RECU_FR_DT_C 40873 76.574180   87.970474
+# 4 53377    0      0 RECU_FR_DT_C16  6421 12.029526  100.000000
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable$`TRUE`$enddate.is %>% str
+# 'data.frame':	4 obs. of  7 variables:
+#  $ n          : int  53377 53377 53377 53377
+#  $ miss       : int  0 0 0 0
+#  $ p.miss     : num  0 0 0 0
+#  $ level      : Factor w/ 4 levels "2013-12-31","DTH_MDY",..: 1 2 3 4
+#  $ freq       : 'table' int [1:4(1d)] 1163 4920 40873 6421
+#   ..- attr(*, "dimnames")=List of 1
+#   .. ..$ x: chr  "2013-12-31" "DTH_MDY" "RECU_FR_DT_C" "RECU_FR_DT_C16"
+#  $ percent    : table [1:4(1d)] 2.18 9.22 76.57 12.03
+#   ..- attr(*, "dimnames")=List of 1
+#   .. ..$ x: chr  "2013-12-31" "DTH_MDY" "RECU_FR_DT_C" "RECU_FR_DT_C16"
+#  $ cum.percent: num  2.18 11.4 87.97 100
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable$`TRUE`$enddate.is %>% filter(level == "DTH_MDY")
+#       n miss p.miss   level freq  percent cum.percent
+# 1 53377    0      0 DTH_MDY 4920 9.217453    11.39629
+
+attr(Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable,"xtabs")$enddate.is
+attr(Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable,"xtabs")$enddate.is %>% str
+attr(Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable,"xtabs")$enddate.is %>% sum
+# > attr(Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable,"xtabs")$enddate.is
+#                 is.dropped_5yr
+# enddate.is        FALSE   TRUE
+#   2013-12-31     385578   1163
+#   DTH_MDY         12341   4920
+#   RECU_FR_DT_C    56833  40873
+#   RECU_FR_DT_C16   6737   6421
+# > attr(Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable,"xtabs")$enddate.is %>% str
+#  'xtabs' int [1:4, 1:2] 385578 12341 56833 6737 1163 4920 40873 6421
+#  - attr(*, "dimnames")=List of 2
+#   ..$ enddate.is    : chr [1:4] "2013-12-31" "DTH_MDY" "RECU_FR_DT_C" "RECU_FR_DT_C16"
+#   ..$ is.dropped_5yr: chr [1:2] "FALSE" "TRUE"
+#  - attr(*, "call")= language xtabs(formula = formula, data = dat)
+# > attr(Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne$`_5yr`$CatTable,"xtabs")$enddate.is %>% sum
+# [1] 514866
+
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% map(function(ob) {
+    ob$CatTable$`TRUE`$enddate.is %>% filter(level %in% c("DTH_MDY", "RECU_FR_DT_C16") )
+})
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% map(function(ob) {
+# +     ob$CatTable$`TRUE`$enddate.is %>% filter(level %in% c("DTH_MDY", "RECU_FR_DT_C16") )
+# + })
+# $`_3yr`
+#       n miss p.miss          level freq   percent cum.percent
+# 1 33330    0      0        DTH_MDY 2090  6.270627    9.177918
+# 2 33330    0      0 RECU_FR_DT_C16 4317 12.952295  100.000000
+# 
+# $`_4yr`
+#       n miss p.miss          level freq   percent cum.percent
+# 1 43643    0      0        DTH_MDY 3471  7.953165    10.41862
+# 2 43643    0      0 RECU_FR_DT_C16 5430 12.441858   100.00000
+# 
+# $`_5yr`
+#       n miss p.miss          level freq   percent cum.percent
+# 1 53377    0      0        DTH_MDY 4920  9.217453    11.39629
+# 2 53377    0      0 RECU_FR_DT_C16 6421 12.029526   100.00000
+# 
+# $`_6yr`
+#       n miss p.miss          level freq  percent cum.percent
+# 1 63438    0      0        DTH_MDY 6552 10.32819    12.36325
+# 2 63438    0      0 RECU_FR_DT_C16 7333 11.55932   100.00000
+# 
+# $`_7yr`
+#       n miss p.miss          level freq  percent cum.percent
+# 1 74397    0      0        DTH_MDY 8120 10.91442    12.76127
+# 2 74397    0      0 RECU_FR_DT_C16 8312 11.17249   100.00000
+
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% map(function(ob) {
+    # Codes to insert inside in the beginning annonymous function for map
+    parent.x = get(".x", envir = parent.frame())
+    i = which(map_lgl(parent.x, function(children_from_parent.x) { identical(children_from_parent.x, ob) } ))
+
+    # Sys.time_1 <- Sys.time() ; print(paste0("Sys.time_1 : ", Sys.time() )) ;
+    # print(Sys.time())
+    #  print(paste0("Beginning .f() map from list element [[", i, "]] named: ", ifelse ( is.null(names(parent.x)[i]), "NULL", names(parent.x)[i] ), "  #----" ))
+    cat(paste0("Beginning .f() map from list element [[", i, "]] named: ", ifelse ( is.null(names(parent.x)[i]), "NULL", names(parent.x)[i] ), "  #---- \n" ))
+
+    out = ob$CatTable$`TRUE`$enddate.is %>% filter(level %in% c("DTH_MDY", "RECU_FR_DT_C16") )
+    out = out %>% rownames_to_column
+    names(out)[1] = "parent"
+    out$parent = names(parent.x)[i]
+    out
+}) %>% reduce(rbind)
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% map(function(ob) {
+# +     # Codes to insert inside in the beginning annonymous function for map
+# +     parent.x = get(".x", envir = parent.frame())
+# +     i = which(map_lgl(parent.x, function(children_from_parent.x) { identical(children_from_parent.x, ob) } ))
+# + 
+# +     # Sys.time_1 <- Sys.time() ; print(paste0("Sys.time_1 : ", Sys.time() )) ;
+# +     # print(Sys.time())
+# +     #  print(paste0("Beginning .f() map from list element [[", i, "]] named: ", ifelse ( is.null(names(parent.x)[i]), "NULL", names(parent.x)[i] ), "  #----" ))
+# +     cat(paste0("Beginning .f() map from list element [[", i, "]] named: ", ifelse ( is.null(names(parent.x)[i]), "NULL", names(parent.x)[i] ), "  #---- \n" ))
+# + 
+# +     out = ob$CatTable$`TRUE`$enddate.is %>% filter(level %in% c("DTH_MDY", "RECU_FR_DT_C16") )
+# +     out = out %>% rownames_to_column
+# +     names(out)[1] = "parent"
+# +     out$parent = names(parent.x)[i]
+# +     out
+# + }) %>% reduce(rbind)
+# Beginning .f() map from list element [[1]] named: _3yr  #---- 
+# Beginning .f() map from list element [[2]] named: _4yr  #---- 
+# Beginning .f() map from list element [[3]] named: _5yr  #---- 
+# Beginning .f() map from list element [[4]] named: _6yr  #---- 
+# Beginning .f() map from list element [[5]] named: _7yr  #---- 
+#    parent     n miss p.miss          level freq   percent cum.percent
+# 1    _3yr 33330    0      0        DTH_MDY 2090  6.270627    9.177918
+# 2    _3yr 33330    0      0 RECU_FR_DT_C16 4317 12.952295  100.000000
+# 3    _4yr 43643    0      0        DTH_MDY 3471  7.953165   10.418624
+# 4    _4yr 43643    0      0 RECU_FR_DT_C16 5430 12.441858  100.000000
+# 5    _5yr 53377    0      0        DTH_MDY 4920  9.217453   11.396294
+# 6    _5yr 53377    0      0 RECU_FR_DT_C16 6421 12.029526  100.000000
+# 7    _6yr 63438    0      0        DTH_MDY 6552 10.328194   12.363252
+# 8    _6yr 63438    0      0 RECU_FR_DT_C16 7333 11.559318  100.000000
+# 9    _7yr 74397    0      0        DTH_MDY 8120 10.914419   12.761267
+# 10   _7yr 74397    0      0 RECU_FR_DT_C16 8312 11.172494  100.000000
+
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is = Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne %>% map(function(ob) {
+    # Codes to insert inside in the beginning annonymous function for map
+    parent.x = get(".x", envir = parent.frame())
+    i = which(map_lgl(parent.x, function(children_from_parent.x) { identical(children_from_parent.x, ob) } ))
+
+    # Sys.time_1 <- Sys.time() ; print(paste0("Sys.time_1 : ", Sys.time() )) ;
+    # print(Sys.time())
+    #  print(paste0("Beginning .f() map from list element [[", i, "]] named: ", ifelse ( is.null(names(parent.x)[i]), "NULL", names(parent.x)[i] ), "  #----" ))
+    cat(paste0("Beginning .f() map from list element [[", i, "]] named: ", ifelse ( is.null(names(parent.x)[i]), "NULL", names(parent.x)[i] ), "  #---- \n" ))
+
+    # out = ob$CatTable$`TRUE`$enddate.is %>% filter(level %in% c("DTH_MDY", "RECU_FR_DT_C16") )
+    out = ob$CatTable$`TRUE`$enddate.is
+    out = out %>% rownames_to_column
+    names(out)[1] = "parent"
+    out$parent = names(parent.x)[i]
+    out
+}) %>% reduce(rbind)
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is
+#    parent     n miss p.miss          level  freq   percent cum.percent
+# 1    _3yr 33330    0      0     2013-12-31   969  2.907291    2.907291
+# 2    _3yr 33330    0      0        DTH_MDY  2090  6.270627    9.177918
+# 3    _3yr 33330    0      0   RECU_FR_DT_C 25954 77.869787   87.047705
+# 4    _3yr 33330    0      0 RECU_FR_DT_C16  4317 12.952295  100.000000
+# 5    _4yr 43643    0      0     2013-12-31  1076  2.465458    2.465458
+# 6    _4yr 43643    0      0        DTH_MDY  3471  7.953165   10.418624
+# 7    _4yr 43643    0      0   RECU_FR_DT_C 33666 77.139518   87.558142
+# 8    _4yr 43643    0      0 RECU_FR_DT_C16  5430 12.441858  100.000000
+# 9    _5yr 53377    0      0     2013-12-31  1163  2.178841    2.178841
+# 10   _5yr 53377    0      0        DTH_MDY  4920  9.217453   11.396294
+# 11   _5yr 53377    0      0   RECU_FR_DT_C 40873 76.574180   87.970474
+# 12   _5yr 53377    0      0 RECU_FR_DT_C16  6421 12.029526  100.000000
+# 13   _6yr 63438    0      0     2013-12-31  1291  2.035058    2.035058
+# 14   _6yr 63438    0      0        DTH_MDY  6552 10.328194   12.363252
+# 15   _6yr 63438    0      0   RECU_FR_DT_C 48262 76.077430   88.440682
+# 16   _6yr 63438    0      0 RECU_FR_DT_C16  7333 11.559318  100.000000
+# 17   _7yr 74397    0      0     2013-12-31  1374  1.846849    1.846849
+# 18   _7yr 74397    0      0        DTH_MDY  8120 10.914419   12.761267
+# 19   _7yr 74397    0      0   RECU_FR_DT_C 56591 76.066239   88.827506
+# 20   _7yr 74397    0      0 RECU_FR_DT_C16  8312 11.172494  100.000000
+
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% filter(level == "DTH_MDY") %>% mutate(n.increased = n - lag(n), freq.increased = freq - lag(freq), cancer.increased = n.increased - freq.increased)
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% filter(level == "DTH_MDY") %>% mutate(n.increased = n - lag(n), freq.increased = freq - lag(freq), cancer.increased = n.increased - freq.increased)
+#   parent     n miss p.miss   level freq   percent cum.percent n.increased freq.increased cancer.increased
+# 1   _3yr 33330    0      0 DTH_MDY 2090  6.270627    9.177918          NA             NA               NA
+# 2   _4yr 43643    0      0 DTH_MDY 3471  7.953165   10.418624       10313           1381             8932
+# 3   _5yr 53377    0      0 DTH_MDY 4920  9.217453   11.396294        9734           1449             8285
+# 4   _6yr 63438    0      0 DTH_MDY 6552 10.328194   12.363252       10061           1632             8429
+# 5   _7yr 74397    0      0 DTH_MDY 8120 10.914419   12.761267       10959           1568             9391
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% filter(level == "RECU_FR_DT_C16") %>% mutate(n.increased = n - lag(n), freq.increased = freq - lag(freq))
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% filter(level == "RECU_FR_DT_C16") %>% mutate(n.increased = n - lag(n), freq.increased = freq - lag(freq))
+#   parent     n miss p.miss          level freq  percent cum.percent n.increased freq.increased
+# 1   _3yr 33330    0      0 RECU_FR_DT_C16 4317 12.95230         100          NA             NA
+# 2   _4yr 43643    0      0 RECU_FR_DT_C16 5430 12.44186         100       10313           1113
+# 3   _5yr 53377    0      0 RECU_FR_DT_C16 6421 12.02953         100        9734            991
+# 4   _6yr 63438    0      0 RECU_FR_DT_C16 7333 11.55932         100       10061            912
+# 5   _7yr 74397    0      0 RECU_FR_DT_C16 8312 11.17249         100       10959            979
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% str
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% str
+# 'data.frame':	20 obs. of  8 variables:
+#  $ parent     : chr  "_3yr" "_3yr" "_3yr" "_3yr" ...
+#  $ n          : int  33330 33330 33330 33330 43643 43643 43643 43643 53377 53377 ...
+#  $ miss       : int  0 0 0 0 0 0 0 0 0 0 ...
+#  $ p.miss     : num  0 0 0 0 0 0 0 0 0 0 ...
+#  $ level      : Factor w/ 4 levels "2013-12-31","DTH_MDY",..: 1 2 3 4 1 2 3 4 1 2 ...
+#  $ freq       :Class 'table'  Named int [1:20] 969 2090 25954 4317 1076 3471 33666 5430 1163 4920 ...
+#   .. ..- attr(*, "names")= chr [1:20] "2013-12-31" "DTH_MDY" "RECU_FR_DT_C" "RECU_FR_DT_C16" ...
+#  $ percent    :Class 'table'  Named num [1:20] 2.91 6.27 77.87 12.95 2.47 ...
+#   .. ..- attr(*, "names")= chr [1:20] "2013-12-31" "DTH_MDY" "RECU_FR_DT_C" "RECU_FR_DT_C16" ...
+#  $ cum.percent: num  2.91 9.18 87.05 100 2.47 ...
+
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+    ggplot(aes(x = parent, y = freq, color = level)) + geom_point()
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+    ggplot(aes(x = as.factor(parent), y = freq, color = level)) + geom_point()
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+    ggplot(aes(x = as.numeric(as.factor(parent)), y = freq, color = level)) + geom_point()
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+# +     ggplot(aes(x = parent, y = freq, color = level)) + geom_point()
+# Don't know how to automatically pick scale for object of type table. Defaulting to continuous.
+# Error in dimnames(x) <- dnx : 'dimnames' applied to non-array
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+# +     ggplot(aes(x = as.factor(parent), y = freq, color = level)) + geom_point()
+# Don't know how to automatically pick scale for object of type table. Defaulting to continuous.
+# Error in dimnames(x) <- dnx : 'dimnames' applied to non-array
+# > Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+# +     ggplot(aes(x = as.numeric(as.factor(parent)), y = freq, color = level)) + geom_point()
+# Don't know how to automatically pick scale for object of type table. Defaulting to continuous.
+# Error in dimnames(x) <- dnx : 'dimnames' applied to non-array
+
+Heals_FinalCohortFile_161022do.dta_nan_as_factor_droplevels.is.dropped_34567yr.CreateTableOne.TRUE_enddate.is %>% 
+    ggplot(aes(x = parent, y = as.numeric(freq), group = level, color = level)) + geom_point() + geom_smooth(method = lm)
+
+
+#@ end -----
