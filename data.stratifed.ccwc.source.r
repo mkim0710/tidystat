fa503f548e2c30ae818bfdd55b88018b86d6845f
mkim0710 <mkim0710@users.noreply.github.com>
2017-12-19 18:15:31 -0500

    out = NA

--- data.stratifed.ccwc.source.r
+++ data.stratifed.ccwc.source.r
@@ -267,7 +267,7 @@ mycohort_1strata_tie %>% as.tibble
 # 12             322        38       49 FALSE Driver|>2750 KCals
 
 
-mycohort_1strata_tie %>% mutate(origin = 30) %>% data.ccwc(.event = "event", .entry = "entry_age", .exit = "exit_age", .origin = "origin", print.process = T)
+mycohort_1strata_tie %>% mutate(origin = 30) %>% data.ccwc(.event = "event", .entry = "entry_age", .exit = "exit_age", .origin = "origin", print.process = F)
 mycohort_1strata_tie %>% data.ccwc(.event = "event", .entry = "entry_age", .exit = "exit_age", .origin = NULL, print.process = F)
 # > mycohort_1strata_tie %>% mutate(origin = 30) %>% data.ccwc(.event = "event", .entry = "entry_age", .exit = "exit_age", .origin = "origin", print.process = T)
 # [1] "*** 1-th iteration for .event.exit_age.unique.sort: 12"
