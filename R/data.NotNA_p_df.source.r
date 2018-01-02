2bf9d565173f2d44a26ea77d4dfab244870e63aa
mkim0710 <mkim0710@users.noreply.github.com>
2018-01-02 15:39:20 -0500

    CreateTableOne(strata = "isExposed", data = ., test=F, includeNA = T) %>% print(smd = T, nonnormal = "age")

--- R/data.NotNA_p_df.source.r
+++ R/data.NotNA_p_df.source.r
@@ -3,9 +3,9 @@
 
 
 data.NotNA_p_df = function(data) {
-    out = data %>% map_df(is.na) %>% colSums %>% as.data.frame %>% rownames_to_column %>% rename(varname = rowname) %>% rownames_to_column
+    out = data %>% map_df(is.na) %>% colSums %>% as.data.frame %>% rownames_to_column %>% rename(varname = rowname) %>% rownames_to_column %>% rename(RowNum = rowname)
     colnames(out)[which(colnames(out) == ".")] = "IsNA"
-    out = out %>% mutate(NotNA = nrow(data) - nrow(data), NRow = nrow(data)) 
+    out = out %>% mutate(NotNA = nrow(data) - IsNA, NRow = nrow(data)) 
     out = out %>% add_column(IsNA_p_df = sprintf("%4.3f",out$IsNA/out$NRow), .after = "IsNA")
     out = out %>% add_column(NotNA_p_df = sprintf("%4.3f",out$NotNA/out$NRow), .after = "NotNA")
     out = out %>% arrange(IsNA)
@@ -20,9 +20,9 @@ data.NotNA_p_df = function(data) {
 library(tidyverse)
 load("data/library(mice) nhanes_mice.rda")
 nhanes_mice %>% {data.NotNA_p_df = function(data) {
-    out = data %>% map_df(is.na) %>% colSums %>% as.data.frame %>% rownames_to_column %>% rename(varname = rowname) %>% rownames_to_column
+    out = data %>% map_df(is.na) %>% colSums %>% as.data.frame %>% rownames_to_column %>% rename(varname = rowname) %>% rownames_to_column %>% rename(RowNum = rowname)
     colnames(out)[which(colnames(out) == ".")] = "IsNA"
-    out = out %>% mutate(NotNA = nrow(data) - nrow(data), NRow = nrow(data)) 
+    out = out %>% mutate(NotNA = nrow(data) - IsNA, NRow = nrow(data)) 
     out = out %>% add_column(IsNA_p_df = sprintf("%4.3f",out$IsNA/out$NRow), .after = "IsNA")
     out = out %>% add_column(NotNA_p_df = sprintf("%4.3f",out$NotNA/out$NRow), .after = "NotNA")
     out = out %>% arrange(IsNA)
@@ -30,11 +30,11 @@ nhanes_mice %>% {data.NotNA_p_df = function(data) {
 }; data.NotNA_p_df(.)}
 nhanes_mice %>% data.NotNA_p_df
 # > nhanes_mice %>% data.NotNA_p_df
-#   rowname varname IsNA IsNA_p_df NotNA NotNA_p_df NRow
-# 1       1     age     0   0.000     25    1.000   25
-# 2       3     hyp     8   0.320     17    0.680   25
-# 3       2     bmi     9   0.360     16    0.640   25
-# 4       4     chl    10   0.400     15    0.600   25
+#   RowNum varname IsNA IsNA_p_df NotNA NotNA_p_df NRow
+# 1      1     age    0     0.000    25      1.000   25
+# 2      3     hyp    8     0.320    17      0.680   25
+# 3      2     bmi    9     0.360    16      0.640   25
+# 4      4     chl   10     0.400    15      0.600   25
 
 
 #@ end -----
