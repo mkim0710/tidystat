d691f1c20abe55615a4d59d9f99dbfb6eb6dcb26
mkim0710 <mkim0710@users.noreply.github.com>
2018-03-05 21:59:20 -0500

    # data = data_frame(PERSON_ID = as.interger(PERSON_ID), WT = as.numeric(WT), DM = as.factor(DM), SDH = as.factor(SDH)) ----

--- R/function.learn_wt.list.source.r
+++ R/function.learn_wt.list.source.r
@@ -5,71 +5,80 @@ function.learn_wt.list = function(
     data = data
     , max_iteration = 10^3
     , early_stop_when_cost_diff_lt = .1^5
-    , Dist_RACE_target = Dist_RACE_target
-    , Dist_College_target = Dist_College_target
+    , Dist_DM_target = Dist_DM_target
+    , Dist_SDH_target = Dist_SDH_target
 ){
+    
+    # data = data_frame(PERSON_ID = as.interger(PERSON_ID), WT = as.numeric(WT), DM = as.factor(DM), SDH = as.factor(SDH))
 
     wt.normalized.list = list()
-    wt.normalized.list[[1]] = data$CAPI_WT / sum(data$CAPI_WT)
+    wt.normalized.list[[1]] = data$WT / sum(data$WT)
 
-    Dist_RACE.list = list()
-    Dist_RACE.list[[1]] = (
-        data %>% group_by(RACE) %>% summarise(CAPI_WT.sum_by_RACE = sum(CAPI_WT)) %>% as.data.frame %>% column_to_rownames("RACE") %>% t %>% {.[1,]}
-    ) / sum(data$CAPI_WT)
+    Dist_DM.list = list()
+    Dist_DM.list[[1]] = (
+        data %>% group_by(DM) %>% summarise(WT.sum_by_DM = sum(WT)) %>% as.data.frame %>% column_to_rownames("DM") %>% t %>% {.[1,]}
+    ) / sum(data$WT)
 
-    Dist_College.list = list()
-    Dist_College.list[[1]] = (
-        data %>% group_by(isCollege_MS_PhD) %>% summarise(CAPI_WT.sum_by_COLLEGE = sum(CAPI_WT)) %>% as.data.frame %>% column_to_rownames("isCollege_MS_PhD") %>% t %>% {.[1,]}
-    ) / sum(data$CAPI_WT)
+    Dist_SDH.list = list()
+    Dist_SDH.list[[1]] = (
+        data %>% group_by(SDH) %>% summarise(WT.sum_by_SDH = sum(WT)) %>% as.data.frame %>% column_to_rownames("SDH") %>% t %>% {.[1,]}
+    ) / sum(data$WT)
 
-    cost = vector(mode = "numeric", length = max_iteration)
-    cost[1] = .function.compute_cost(.Dist_College_current = Dist_College.list[[1]], .Dist_RACE_current = Dist_RACE.list[[1]], .Dist_RACE_target = Dist_RACE_target, .Dist_College_target = Dist_College_target)
-    
-    .function.compute_cost = function(.Dist_College_current, .Dist_RACE_current, .Dist_RACE_target, .Dist_College_target){
-        J = sum((.Dist_College_current - .Dist_College_target)^2)/length(.Dist_College_target) + sum((.Dist_RACE_current - .Dist_RACE_target)^2)/length(.Dist_RACE_target)
+    # sub-function for calculating cost ----
+    .function.compute_cost = function(.Dist_SDH_current, .Dist_DM_current, .Dist_DM_target, .Dist_SDH_target){
+        J = sum((.Dist_SDH_current - .Dist_SDH_target)^2)/length(.Dist_SDH_target) + sum((.Dist_DM_current - .Dist_DM_target)^2)/length(.Dist_DM_target)
         return(J)
     }
-
+    
+    cost = vector(mode = "numeric", length = max_iteration)
+    cost[1] = .function.compute_cost(.Dist_SDH_current = Dist_SDH.list[[1]], .Dist_DM_current = Dist_DM.list[[1]], .Dist_DM_target = Dist_DM_target, .Dist_SDH_target = Dist_SDH_target)
+    
     for(i in 2:max_iteration){
  
         wt.normalized.list[[i]] = wt.normalized.list[[i-1]]
-        for (k in 1:length(levels(data$RACE))) {
-            wt.normalized.list[[i]]
-            wt.normalized.list[[i]] [data$RACE==levels(data$RACE)[k]] = wt.normalized.list[[i]] [data$RACE==levels(data$RACE)[k]] * Dist_RACE_target[k] / Dist_RACE.list[[i-1]][k]
+        
+        # update weights for target distribution of DM & SDH ----
+        for (k in 1:length(levels(data$DM))) {
+            wt.normalized.list[[i]] [data$DM==levels(data$DM)[k]] = wt.normalized.list[[i]] [data$DM==levels(data$DM)[k]] * Dist_DM_target[k] / Dist_DM.list[[i-1]][k]
         }
-        for (k in 1:length(c(F,T))) {
-            wt.normalized.list[[i]] [data$isCollege_MS_PhD==c(F,T)[k]] = wt.normalized.list[[i]] [data$isCollege_MS_PhD==c(F,T)[k]] * Dist_College_target[k] / Dist_College.list[[i-1]][k]
+        for (k in 1:length(levels(data$SDH))) {
+            wt.normalized.list[[i]] [data$SDH==levels(data$SDH)[k]] = wt.normalized.list[[i]] [data$SDH==levels(data$SDH)[k]] * Dist_DM_target[k] / Dist_DM.list[[i-1]][k]
         }
+        
+        # for every iteration, updated weights will be stored ----
         wt.normalized.list[[i]] = wt.normalized.list[[i]] / sum(wt.normalized.list[[i]])
         
-        Dist_RACE.list[[i]]= (
-            data %>% mutate(CAPI_WT = wt.normalized.list[[i]]) %>% 
-                group_by(RACE) %>% summarise(CAPI_WT.sum_by_RACE = sum(CAPI_WT)) %>% as.data.frame %>% column_to_rownames("RACE") %>% t %>% {.[1,]}
-        # ) / sum(wt.normalized.list[[i]])
+        # for every iteration, updated distribution of DM given the updated weights will be stored ----
+        Dist_DM.list[[i]]= (
+            data %>% mutate(WT = wt.normalized.list[[i]]) %>% 
+                group_by(DM) %>% summarise(WT.sum_by_DM = sum(WT)) %>% as.data.frame %>% column_to_rownames("DM") %>% t %>% {.[1,]}
         )
         
-        Dist_College.list[[i]] = (
-            data %>% mutate(CAPI_WT = wt.normalized.list[[i]]) %>% 
-                group_by(isCollege_MS_PhD) %>% summarise(CAPI_WT.sum_by_COLLEGE = sum(CAPI_WT)) %>% as.data.frame %>% column_to_rownames("isCollege_MS_PhD") %>% t %>% {.[1,]}
-        # ) / sum(wt.normalized.list[[i]])
+        # for every iteration, updated distribution of SDH given the updated weights will be stored ----
+        Dist_SDH.list[[i]] = (
+            data %>% mutate(WT = wt.normalized.list[[i]]) %>% 
+                group_by(SDH) %>% summarise(WT.sum_by_SDH = sum(WT)) %>% as.data.frame %>% column_to_rownames("SDH") %>% t %>% {.[1,]}
         )
         
-        cost[i] = .function.compute_cost(.Dist_College_current = Dist_College.list[[i]], .Dist_RACE_current = Dist_RACE.list[[i]], .Dist_RACE_target = Dist_RACE_target, .Dist_College_target = Dist_College_target)
+        # for every iteration, updated cost given the updated weights will be stored ----
+        cost[i] = .function.compute_cost(.Dist_SDH_current = Dist_SDH.list[[i]], .Dist_DM_current = Dist_DM.list[[i]], .Dist_DM_target = Dist_DM_target, .Dist_SDH_target = Dist_SDH_target)
+        
+        # early stopping critera ----
         if (abs(cost[i] - cost[i-1]) < early_stop_when_cost_diff_lt) break
     }
     
     return = list(
         wt.normalized.list = wt.normalized.list
-        , Dist_RACE.df = (Dist_RACE.list %>% map(as.list) %>% bind_rows)
-        , Dist_College.df = (Dist_College.list %>% map(as.list) %>% bind_rows)
+        , Dist_DM.df = (Dist_DM.list %>% map(as.list) %>% bind_rows)
+        , Dist_SDH.df = (Dist_SDH.list %>% map(as.list) %>% bind_rows)
         , cost = cost
     )
     attr(return, "input") = list(
         data = data
         , max_iteration = max_iteration
         , early_stop_when_cost_diff_lt = early_stop_when_cost_diff_lt
-        , Dist_RACE_target = Dist_RACE_target
-        , Dist_College_target = Dist_College_target
+        , Dist_DM_target = Dist_DM_target
+        , Dist_SDH_target = Dist_SDH_target
     )
     return
 }
