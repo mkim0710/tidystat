f006efaa6a266d9630e4d45f6a354ea597fbdab0
mkim0710 <mkim0710@users.noreply.github.com>
2018-05-14 20:07:47 -0400

    update
    Former-commit-id: 0fd453576e3702d751960a079d2982c13b36cb16

---
+++ data/library(icd) icd_is_valid().r
@@ -0,0 +1,46 @@
+# library(icd) icd_is_valid().r
+
+icd10cm2016 %>% select(three_digit, sub_chapter) %>% distinct %>% str
+# > icd10cm2016 %>% select(three_digit, sub_chapter) %>% distinct %>% str
+# 'data.frame':	1910 obs. of  2 variables:
+#  $ three_digit: Factor w/ 1903 levels "A00","A01","A02",..: 1 2 3 4 5 6 7 8 9 10 ...
+#  $ sub_chapter: Factor w/ 279 levels "Intestinal Infectious Diseases",..: 1 1 1 1 1 1 1 1 1 1 ...
+
+
+icd_is_valid("I109")
+icd_is_valid("I10")
+icd_is_valid("IJK10")
+icd_is_valid("I1000")
+icd_is_valid("I10009")
+icd_is_valid("I10009p")
+icd_is_valid("I10009999")
+# > icd_is_valid("IJK10")
+# [1] FALSE
+# > icd_is_valid("I109")
+# [1] TRUE
+# > icd_is_valid("I10")
+# [1] TRUE
+# > icd_is_valid("IJK10")
+# [1] FALSE
+# > icd_is_valid("I1000")
+# [1] TRUE
+# > icd_is_valid("I10009")
+# [1] TRUE
+# > icd_is_valid("I10009p")
+# [1] TRUE
+# > icd_is_valid("I10009999")
+# [1] FALSE
+
+icd_is_valid
+# > icd_is_valid
+# function (...) 
+# {
+#     very_soft_deprecate("is_valid")
+#     is_valid(...)
+# }
+# <bytecode: 0x00000001b73e8120>
+# <environment: namespace:icd>
+
+
+#@ end ----
+
