f76690f0c6317f55fca65ed980a11782ba06e197
mkim0710 <mkim0710@gmail.com>
2018-10-14 19:18:03 +0900

    Create function.Stata2R.source.R
    Former-commit-id: d021f8f579d80591647a5f852982cd9db3b91f73

---
+++ R/function.Stata2R.source.R
@@ -0,0 +1,26 @@
+
+
+haven::read_stata()
+
+str(give.attr = F)
+
+# stata.covariates.list.sort.unequal.clean from stata.covariates.list.sort.unequal .r
+
+#@ tmp.vector2 ====
+tmp.vector2 <- tmp.vector
+# grep("Q_bool", tmp.vector2)
+tmp.vector2 <- gsub("^i.", "", tmp.vector2)
+tmp.vector2 <- gsub("Q_bool$", "", tmp.vector2)
+tmp.vector2 <- gsub("Q$", "", tmp.vector2)
+tmp.vector2 <- gsub("total_days`var'_ASPIRIN", "", tmp.vector2)
+tmp.vector2 <- gsub("NSAIDASPIRIN", "", tmp.vector2)
+tmp.vector2 <- gsub("SEX_F_M", "", tmp.vector2)
+tmp.vector2 <- unique(tmp.vector2)
+tmp.vector2 <- sort(tmp.vector2)
+# tmp.vector2[tmp.vector2==""] <- NA
+# tmp.vector2 <- na.omit(tmp.vector2)
+tmp.vector2 <- tmp.vector2[tmp.vector2 != ""]
+tmp.vector2
+
+
+
