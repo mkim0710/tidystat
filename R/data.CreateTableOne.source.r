c057591aae7ddfe8121f1d9e20b297ed40fcda59
mkim0710 <mkim0710@gmail.com>
2018-10-15 00:32:38 +0900

    Update data.CreateTableOne.source.r
    Former-commit-id: da4306815ad74977264c7bc98ae2aa2892430864

--- R/data.CreateTableOne.source.r
+++ R/data.CreateTableOne.source.r
@@ -22,10 +22,10 @@ openxlsx::openXL("dataset.tableone -clean.csv")
 # openxlsx::openXL("dataset.tableone -clean.xlsx")
 dataset.tableone %>% print(nonnormal = NULL, exact = NULL, quote = FALSE, noSpaces = TRUE, printToggle = FALSE) %>% as.data.frame(stringsAsFactors = F) %>% rownames_to_column %>% 
     openxlsx::write.xlsx("dataset.tableone.xlsx")
-openxlsx::openXL("dataset.tableone.xlsx")
+# openxlsx::openXL("dataset.tableone.xlsx")
 dataset.tableone %>% print(nonnormal = vars4IQR, exact = NULL, quote = FALSE, noSpaces = TRUE, printToggle = FALSE) %>% as.data.frame(stringsAsFactors = F) %>% rownames_to_column %>% 
     openxlsx::write.xlsx("dataset.tableone.IQR.xlsx")
-openxlsx::openXL("dataset.tableone.IQR.xlsx")
+# openxlsx::openXL("dataset.tableone.IQR.xlsx")
 
 
 #@ dataset.CreateTableOne.by_exposure -----
@@ -49,10 +49,10 @@ openxlsx::openXL("dataset.tableone_by_exposure -clean.csv")
 dataset.tableone_by_exposure %>% print(smd = T, nonnormal = NULL, exact = NULL, quote = FALSE, noSpaces = TRUE, printToggle = FALSE) %>% as.data.frame(stringsAsFactors = F) %>% rownames_to_column %>% {.[1, 5]="=NUMBERVALUE(MID(B2,1,SEARCH(\"(\",B2,1)-1))"; .} %>% 
     mutate(Group1 = {.[[2]]}, Group2 = {.[[3]]}) %>% separate(Group1, into = paste0("Group1", c("mean", "sd", "larger")), sep = "[\\(\\)]") %>% separate(Group2, into = paste0("Group2", c("mean", "sd", "larger")), sep = "[\\(\\)]") %>% mutate(Group1larger = ifelse(Group1mean>Group2mean, 1, 0), Group2larger = ifelse(Group1mean<Group2mean, 1, 0)) %>% 
     openxlsx::write.xlsx("dataset.tableone_by_exposure.xlsx")
-openxlsx::openXL("dataset.tableone_by_exposure.xlsx")
+# openxlsx::openXL("dataset.tableone_by_exposure.xlsx")
 dataset.tableone_by_exposure %>% print(smd = T, nonnormal = vars4IQR, exact = NULL, quote = FALSE, noSpaces = TRUE, printToggle = FALSE) %>% as.data.frame %>% rownames_to_column %>% 
     openxlsx::write.xlsx("dataset.tableone_by_exposure.IQR.xlsx")
-openxlsx::openXL("dataset.tableone_by_exposure.IQR.xlsx")
+# openxlsx::openXL("dataset.tableone_by_exposure.IQR.xlsx")
 
 
 
