7b929575d06cdb448ef0cb4619eaf547fd1a591c
mkim0710 <mkim0710@gmail.com>
2018-10-27 20:22:11 +0900

    Update function.numeric2integer.source.r
    Former-commit-id: 89d5c3620b8f61084d8d7aeee6c5366cc8bf8f51

--- R/function.numeric2integer.source.r
+++ R/function.numeric2integer.source.r
@@ -4,7 +4,54 @@ function.numeric2nearest_integer = function(vec) {
     round(vec + 10^(-9))
 }
 
+
+#@ ?round
 ?round
+# Description
+# ceiling takes a single numeric argument x and returns a numeric vector containing the smallest integers not less than the corresponding elements of x.
+# floor takes a single numeric argument x and returns a numeric vector containing the largest integers not greater than the corresponding elements of x.
+# trunc takes a single numeric argument x and returns a numeric vector containing the integers formed by truncating the values in x toward 0.
+# round rounds the values in its first argument to the specified number of decimal places (default 0). See ‘Details’ about “round to even” when rounding off a 5.
+# signif rounds the values in its first argument to the specified number of significant digits.
+# 
+# Usage
+# ceiling(x)
+# floor(x)
+# trunc(x, ...) 
+# round(x, digits = 0)
+# signif(x, digits = 6)
+
+
+round(.5 + -2:4) # IEEE / IEC rounding: -2  0  0  2  2  4  4
+## (this is *good* behaviour -- do *NOT* report it as bug !)
+
+( x1 <- seq(-2, 4, by = .5) )
+round(x1) #-- IEEE / IEC rounding !
+x1[trunc(x1) != floor(x1)]
+x1[round(x1) != floor(x1 + .5)]
+(non.int <- ceiling(x1) != floor(x1))
+# > ( x1 <- seq(-2, 4, by = .5) )
+#  [1] -2.0 -1.5 -1.0 -0.5  0.0  0.5  1.0  1.5  2.0  2.5  3.0  3.5  4.0
+# > round(x1) #-- IEEE / IEC rounding !
+#  [1] -2 -2 -1  0  0  0  1  2  2  2  3  4  4
+# > x1[trunc(x1) != floor(x1)]
+# [1] -1.5 -0.5
+# > x1[round(x1) != floor(x1 + .5)]
+# [1] -1.5  0.5  2.5
+# > (non.int <- ceiling(x1) != floor(x1))
+#  [1] FALSE  TRUE FALSE  TRUE FALSE  TRUE FALSE  TRUE FALSE  TRUE FALSE  TRUE FALSE
+
+x2 <- pi * 100^(-1:3)
+round(x2, 3)
+signif(x2, 3)
+# > x2 <- pi * 100^(-1:3)
+# > round(x2, 3)
+# [1]       0.031       3.142     314.159   31415.927 3141592.654
+# > signif(x2, 3)
+# [1] 3.14e-02 3.14e+00 3.14e+02 3.14e+04 3.14e+06
+
+
+#@ -----
 ceiling(c(-1.51, -1.5, -1.49, -0.51, -0.5, -0.49, 0.49, 0.5, 0.51, 1.49, 1.5, 1.51))
 floor(c(-1.51, -1.5, -1.49, -0.51, -0.5, -0.49, 0.49, 0.5, 0.51, 1.49, 1.5, 1.51))
 trunc(c(-1.51, -1.5, -1.49, -0.51, -0.5, -0.49, 0.49, 0.5, 0.51, 1.49, 1.5, 1.51))
