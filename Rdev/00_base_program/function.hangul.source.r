1b3a1d1a7529d502cf73dd910acae8842650c0e9
mkim0710 <mkim0710@gmail.com>
2019-05-03 16:24:26 +0900

    Update function.hangul.source.r
    Former-commit-id: 2f0087ae55aded03d6a9f4ebe63951235977d6cd

--- Rdev/00_base_program/function.hangul.source.r
+++ Rdev/00_base_program/function.hangul.source.r
@@ -8,6 +8,7 @@
 dput(unlist(strsplit(Sys.getlocale(), ";"))) #----
 dput(l10n_info()) #----
 dput(.Platform) #----
+dput(Sys.info()) #----
 # > dput(unlist(strsplit(Sys.getlocale(), ";"))) #----
 # c("LC_COLLATE=English_United States.1252", "LC_CTYPE=English_United States.1252", 
 # "LC_MONETARY=English_United States.1252", "LC_NUMERIC=C", "LC_TIME=English_United States.1252"
@@ -18,6 +19,10 @@ dput(.Platform) #----
 # list(OS.type = "windows", file.sep = "/", dynlib.ext = ".dll", 
 #     GUI = "RStudio", endian = "little", pkgType = "win.binary", 
 #     path.sep = ";", r_arch = "x64")
+# > dput(Sys.info()) #----
+# c(sysname = "Windows", release = ">= 8 x64", version = "build 9200", 
+# nodename = "NUC6I7KYK", machine = "x86-64", login = "mkim0", 
+# user = "mkim0", effective_user = "mkim0")
 
 
 
