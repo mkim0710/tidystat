f86fff04e25f43d727809b46f8f1f9799432d034
mkim0710 <mkim0710@gmail.com>
2019-05-17 20:41:37 +0900

    Update function.matrix.source.r

--- Rdev/function.matrix.source.r
+++ Rdev/function.matrix.source.r
@@ -38,8 +38,8 @@ mat
 
 
 mat %>% {data.frame(row = (which.min(.)-1) %% nrow(.) + 1, col = which.min(.) %/% nrow(.) + 1)} #----
-mat %>% {data.frame(row = (which(. == min(.))-1) %% nrow(.) + 1, col = which(. == min(.)) %/% nrow(.) + 1)} #----
-mat %>% {which(. == min(.), arr.ind = TRUE)} #----
+mat %>% {data.frame(row = (which(. == min(., na.rm = T))-1) %% nrow(.) + 1, col = which(. == min(., na.rm = T)) %/% nrow(.) + 1)} #----
+mat %>% {which(. == min(., na.rm = T), arr.ind = TRUE)} #----
 # > mat %>% {data.frame(row = (which.min(.)-1) %% nrow(.) + 1, col = which.min(.) %/% nrow(.) + 1)} #----
 #   row col
 # 1   1   1
@@ -60,8 +60,8 @@ mat %>% {which(. == min(.), arr.ind = TRUE)} #----
 
 
 mat %>% {data.frame(row = (which.max(.)-1) %% nrow(.) + 1, col = which.max(.) %/% nrow(.) + 1)} #----
-mat %>% {data.frame(row = (which(. == max(.))-1) %% nrow(.) + 1, col = which(. == max(.)) %/% nrow(.) + 1)} #----
-mat %>% {which(. == max(.), arr.ind = TRUE)} #----
+mat %>% {data.frame(row = (which(. == max(., na.rm = T))-1) %% nrow(.) + 1, col = which(. == max(., na.rm = T)) %/% nrow(.) + 1)} #----
+mat %>% {which(. == max(., na.rm = T), arr.ind = TRUE)} #----
 # > mat %>% {data.frame(row = (which.max(.)-1) %% nrow(.) + 1, col = which.max(.) %/% nrow(.) + 1)} #----
 #   row col
 # 1   2   2
