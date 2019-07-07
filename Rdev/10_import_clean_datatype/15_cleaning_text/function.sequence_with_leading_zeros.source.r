68e33546e7c72a5fa1efb84f0c442934f7db7e5c
mkim0710 <mkim0710@gmail.com>
2019-07-07 11:21:09 +0900

    Update function.sequence_with_leading_zeros.source.r
    Former-commit-id: 9ddd003c07b5aea1f9bf41963e319bd6b4d36db3

--- Rdev/10_import_clean_datatype/15_cleaning_text/function.sequence_with_leading_zeros.source.r
+++ Rdev/10_import_clean_datatype/15_cleaning_text/function.sequence_with_leading_zeros.source.r
@@ -8,3 +8,89 @@ function.sequence_with_leading_zeros = function(num) {
 # [1] "1" "2" "3" "4" "5" "6" "7" "8" "9"
 # > function.sequence_with_leading_zeros(10)
 #  [1] "01" "02" "03" "04" "05" "06" "07" "08" "09" "10"
+
+
+
+
+function.integer2factor_with_leading_zeros.v1 = function(vec_int) {
+    # source("https://github.com/mkim0710/tidystat/raw/master/function.sequence_with_leading_zeros.source.r")
+    if(min(vec_int) < 1) {
+        out = NA
+        attributes(out)$ErrorMessage = "min(vec_int) < 1"
+        warning("min(vec_int) < 1")
+    } else {
+        digits_with_leading_zeros = trunc(log10(max(vec_int))) + 1
+        levels.new = sprintf(paste0("%0",digits_with_leading_zeros,"d"), 1:max(vec_int))
+        vec.new = sprintf(paste0("%0",digits_with_leading_zeros,"d"), vec_int)
+        out = factor(vec_int, levels = levels.new)
+    }
+    out
+}
+tmp.vec1 = c(1, 3, 6, 9)
+tmp.vec2 = c(1:3, 10)
+tmp.vec3 = c(1, 3, 6, 9, -1)
+
+tmp.vec1 %>% function.integer2factor_with_leading_zeros.v1
+tmp.vec2 %>% function.integer2factor_with_leading_zeros.v1
+tmp.vec3 %>% function.integer2factor_with_leading_zeros.v1
+# > tmp.vec1 %>% function.integer2factor_with_leading_zeros.v1
+# [1] 1 3 6 9
+# Levels: 1 2 3 4 5 6 7 8 9
+# > tmp.vec2 %>% function.integer2factor_with_leading_zeros.v1
+# [1] <NA> <NA> <NA> 10  
+# Levels: 01 02 03 04 05 06 07 08 09 10
+# > tmp.vec3 %>% function.integer2factor_with_leading_zeros.v1
+# [1] NA
+# attr(,"ErrorMessage")
+# [1] "min(vec_int) < 1"
+# Warning message:
+# In function.integer2factor_with_leading_zeros.v1(.) : min(vec_int) < 1
+
+
+
+function.integer2factor_with_leading_zeros.v2 = function(vec_int) {
+    # source("https://github.com/mkim0710/tidystat/raw/master/function.sequence_with_leading_zeros.source.r")
+    if(min(vec_int) < 1) {
+        out = NA
+        attributes(out)$ErrorMessage = "min(vec_int) < 1"
+        warning("min(vec_int) < 1")
+    } else {
+        digits_with_leading_zeros = trunc(log10(max(vec_int))) + 1
+        # levels.new = sprintf(paste0("%0",digits_with_leading_zeros,"d"), 1:max(vec_int))
+        # vec.new = sprintf(paste0("%0",digits_with_leading_zeros,"d"), vec_int)
+        out = factor(vec_int, levels = 1:max(vec_int))
+        out = factor(vec_int, levels = 1:max(vec_int), labels = sprintf(paste0("%0",digits_with_leading_zeros,"d"), 1:max(vec_int)))
+    }
+    out
+}
+tmp.vec1 %>% function.integer2factor_with_leading_zeros.v2
+tmp.vec2 %>% function.integer2factor_with_leading_zeros.v2
+tmp.vec3 %>% function.integer2factor_with_leading_zeros.v2
+# > tmp.vec1 %>% function.integer2factor_with_leading_zeros.v2
+# [1] 1 3 6 9
+# Levels: 1 2 3 4 5 6 7 8 9
+# > tmp.vec2 %>% function.integer2factor_with_leading_zeros.v2
+# [1] 01 02 03 10
+# Levels: 01 02 03 04 05 06 07 08 09 10
+# > tmp.vec3 %>% function.integer2factor_with_leading_zeros.v2
+# [1] NA
+# attr(,"ErrorMessage")
+# [1] "min(vec_int) < 1"
+# Warning message:
+# In function.integer2factor_with_leading_zeros.v2(.) : min(vec_int) < 1
+
+library(microbenchmark)
+set.seed(1)
+mbm = microbenchmark(
+    v1 = sample(-99:999, 10) %>% function.integer2factor_with_leading_zeros.v2
+    , v2 = sample(-99:999, 10) %>% function.integer2factor_with_leading_zeros.v2
+)
+autoplot(mbm)
+mbm
+# > mbm
+# Unit: microseconds
+#  expr   min     lq    mean median      uq    max neval cld
+#    v1 163.9 206.85 747.820  308.5 1381.15 3775.7   100   a
+#    v2 162.5 208.50 857.026  316.2 1389.80 4772.8   100   a
+
+
