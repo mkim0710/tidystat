0b09d8b5c54022c016d429b0ec20cee3f1582478
mkim0710 <mkim0710@gmail.com>
2019-08-02 22:40:54 +0900

    Update tblPersonID_FilterName.ndDate.append_FilterRegexMet.source.r
    Former-commit-id: 2eeb9cdd665005b70da079b8b920e2a80efab49f

--- Rdev/20_tidy_group_by_match/25_study_population/tblPersonID_FilterName.ndDate.append_FilterRegexMet.source.r
+++ Rdev/20_tidy_group_by_match/25_study_population/tblPersonID_FilterName.ndDate.append_FilterRegexMet.source.r
@@ -222,3 +222,103 @@ tblPersonID_FilterName.ndDate %>% select(ENROLID) %>%
 # 10 218320801                                       1
 # # ... with 27,411 more rows
 
+
+
+
+
+
+
+
+
+
+
+#@@@ tblPersonID_FilterName.ndDate -----
+t0 = Sys.time()
+tblPersonID_FilterName.ndDate =
+    os.ID_DATE_DX.distinct.gather_DX.byID_min_rank_lmp.ID_lmp.ge_lmp_365_le_enddate.lmp_le2014 %>%
+    select(ENROLID) %>% distinct
+for (i in 1:nrow(Criteria.tbl.bind_rows)) {
+    CriteriaID.i = Criteria.tbl.bind_rows[i,]$CriteriaID
+    CodeType.i = Criteria.tbl.bind_rows[i,]$CodeType
+
+    t_begin.int.i = Criteria.tbl.bind_rows[i,]$t_begin.int
+    t_end.int.i = Criteria.tbl.bind_rows[i,]$t_end.int
+    if (is.na(t_end.int.i)) t_end.int.i = Inf
+
+    FilterName.i = Criteria.tbl.bind_rows[i,]$FilterName
+    FilterRegex.i = Criteria.tbl.bind_rows[i,]$FilterRegex
+    Code_vec.list.i = Criteria.tbl.bind_rows[i,]$Code_vec.list
+
+    if (is.na(Code_vec.list.i)) {
+        msg = paste0( 'is.na(Code_vec.list.i) for FilterName.i: ', FilterName.i )
+        print(msg); warning(msg)
+    } else if (paste0(FilterName.i, ".ndDate") %in% names(tblPersonID_FilterName.ndDate)) {
+        msg = paste0( paste0(FilterName.i, ".ndDate"), ' %in% names(tblPersonID_FilterName.ndDate)', '-> skipping for CriteriaID.i: ', CriteriaID.i )
+        print(msg); warning(msg)
+    } else {
+        print(paste0( 'Appending: ', paste0(FilterName.i, ".ndDate") ))
+        if (CodeType.i %in% c("ICD9", "ICD10")) {
+            tblPersonID_FilterName.ndDate =
+                tblPersonID_FilterName.ndDate %>%
+                    tblPersonID_FilterName.ndDate.append_FilterRegexMet(
+                        tblClaim_Date_Code = tblClaim_Date_Code4ICD
+                        # , CriteriaID.i = CriteriaID.i
+                        # , CodeType.i = CodeType.i
+                        , t_begin.int.i = t_begin.int.i
+                        , t_end.int.i = t_end.int.i
+                        , FilterName.i = FilterName.i
+                        , FilterRegex.i = FilterRegex.i
+                        # , Code_vec.list.i = Code_vec.list.i
+                    )
+        }
+
+        # if (CodeType.i == "Rx") { # missing value where TRUE/FALSE needed
+        if (CodeType.i %in% c("Rx")) {
+            tblPersonID_FilterName.ndDate = 
+                tblPersonID_FilterName.ndDate %>% 
+                tblPersonID_FilterName.ndDate.append_FilterMet(
+                    tblClaim_Date_Code = tblClaim_Date_Code4Rx
+                    , varname4PersonID = "ENROLID"
+                    , varname4t = "SVCDATE"
+                    , varname4t0 = "lmp"
+                    , t_begin.int.i = t_begin.int.i
+                    , t_end.int.i = t_end.int.i
+                    , varname4Code = "value"
+                    , FilterName.i = FilterName.i
+                    , varname4FilterMet.i = Code_vec.list.i
+                )
+
+        }
+    }
+}
+Sys.time() - t0
+# [1] "Appending: t_NInf_154.ICD250x1_250x3.ndDate"
+# [1] "Appending: t_NInf_0.RxCONCEPT_NDC_metformin.ndDate"
+# > Sys.time() - t0
+# Time difference of 16.99357 secs
+tblPersonID_FilterName.ndDate %>% na.omit
+# > tblPersonID_FilterName.ndDate %>% na.omit
+# # A tibble: 7 x 3
+#       ENROLID t_NInf_154.ICD250x1_250x3.ndDate t_NInf_0.RxCONCEPT_NDC_metformin.ndDate
+#         <dbl>                            <int>                                   <int>
+# 1   171946801                                2                                       1
+# 2  1210879901                                5                                       1
+# 3  1282775301                                1                                       7
+# 4  2068053701                                3                                       2
+# 5 27240989002                                1                                       1
+# 6 27945818803                                2                                       1
+# 7 28503004201                                1                                       2
+
+
+
+
+
+
+
+
+
+
+#@ end -----
+
+
+
