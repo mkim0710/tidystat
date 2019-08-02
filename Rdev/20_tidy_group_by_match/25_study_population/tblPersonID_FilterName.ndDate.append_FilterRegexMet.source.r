f792d9507dac8d5fe03715226684f1cf2e08c095
mkim0710 <mkim0710@gmail.com>
2019-08-02 23:03:13 +0900

    Update tblPersonID_FilterName.ndDate.append_FilterRegexMet.source.r
    Former-commit-id: 80cf3fa611e30d43b3586e1b4e4d3971f244e186

--- Rdev/20_tidy_group_by_match/25_study_population/tblPersonID_FilterName.ndDate.append_FilterRegexMet.source.r
+++ Rdev/20_tidy_group_by_match/25_study_population/tblPersonID_FilterName.ndDate.append_FilterRegexMet.source.r
@@ -235,20 +235,22 @@ tblPersonID_FilterName.ndDate %>% select(ENROLID) %>%
 
 
 
-# Criteria.tbl.bind_rows = bind_rows(Criteria.tbl.list$ExclusionCriteria.tbl, Criteria.tbl.list$InclusionCriteria.tbl)
+
+# tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation = bind_rows(Criteria.tbl.list$ExclusionCriteria.tbl, Criteria.tbl.list$InclusionCriteria.tbl)
 Criteria.tbl.list$ExclusionCriteria.tbl[3:4, ] %>% select(-note) %>% dput #----
 # > Criteria.tbl.list$ExclusionCriteria.tbl[3:4, ] %>% select(-note) %>% dput #----
-Criteria.tbl.bind_rows = structure(list(CriteriaID = c("Exc3", "Exc4"), CriteriaShortName = c("ICD9_DMt1",
-"Rx_Metformin.lt2"), t_begin.int = c(-Inf, -Inf), t_end.int = c(154,
-0), CodeType = c("ICD9", "Rx"), Code_vec = c(NA_character_, NA_character_
-), Code_vec.list = c("250x1, 250x3", "CONCEPT_NDC_metformin"),
+tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation = 
+    structure(list(CriteriaID = c("Exc3", "Exc4"), CriteriaShortName = c("ICD9_DMt1",
+    "Rx_Metformin.lt2"), t_begin.int = c(-Inf, -Inf), t_end.int = c(154,
+    0), CodeType = c("ICD9", "Rx"), Code_vec = c(NA_character_, NA_character_
+    ), Code_vec.list = c("250x1, 250x3", "CONCEPT_NDC_metformin"),
     FilterName = c("t_NInf_154.ICD250x1_250x3", "t_NInf_0.RxCONCEPT_NDC_metformin"
     ), FilterRegex = c("^250[0-9][13]$", "^(CONCEPT_NDC_metformin)"
     ), Evaluation = c("t_NInf_154.ICD250x1_250x3 > 0", "t_NInf_0.RxCONCEPT_NDC_metformin < 2"
     ), nID.nDate_ge1 = c(NA_real_, NA_real_), nID.nDate_ge2 = c(NA_real_,
     NA_real_), nID.nDate_ge3 = c(NA_real_, NA_real_)), row.names = 3:4, class = "data.frame")
-Criteria.tbl.bind_rows
-# > Criteria.tbl.bind_rows
+tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation
+# > tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation
 #   CriteriaID CriteriaShortName t_begin.int t_end.int CodeType Code_vec         Code_vec.list                       FilterName
 # 3       Exc3         ICD9_DMt1        -Inf       154     ICD9     <NA>          250x1, 250x3        t_NInf_154.ICD250x1_250x3
 # 4       Exc4  Rx_Metformin.lt2        -Inf         0       Rx     <NA> CONCEPT_NDC_metformin t_NInf_0.RxCONCEPT_NDC_metformin
@@ -264,17 +266,17 @@ t0 = Sys.time()
 tblPersonID_FilterName.ndDate =
     os.ID_DATE_DX.distinct.gather_DX.byID_min_rank_lmp.ID_lmp.ge_lmp_365_le_enddate.lmp_le2014 %>%
     select(ENROLID) %>% distinct
-for (i in 1:nrow(Criteria.tbl.bind_rows)) {
-    CriteriaID.i = Criteria.tbl.bind_rows[i,]$CriteriaID
-    CodeType.i = Criteria.tbl.bind_rows[i,]$CodeType
+for (i in 1:nrow(tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation)) {
+    CriteriaID.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$CriteriaID
+    CodeType.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$CodeType
 
-    t_begin.int.i = Criteria.tbl.bind_rows[i,]$t_begin.int
-    t_end.int.i = Criteria.tbl.bind_rows[i,]$t_end.int
+    t_begin.int.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$t_begin.int
+    t_end.int.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$t_end.int
     if (is.na(t_end.int.i)) t_end.int.i = Inf
 
-    FilterName.i = Criteria.tbl.bind_rows[i,]$FilterName
-    FilterRegex.i = Criteria.tbl.bind_rows[i,]$FilterRegex
-    Code_vec.list.i = Criteria.tbl.bind_rows[i,]$Code_vec.list
+    FilterName.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$FilterName
+    FilterRegex.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$FilterRegex
+    Code_vec.list.i = tblCriteriaID_FilterName_FilterRegex_varname4FilterMet_Evaluation[i,]$Code_vec.list
 
     if (is.na(Code_vec.list.i)) {
         msg = paste0( 'is.na(Code_vec.list.i) for FilterName.i: ', FilterName.i )
@@ -337,7 +339,6 @@ tblPersonID_FilterName.ndDate %>% na.omit
 # 7 28503004201                                1                                       2
 
 
-
 tblPersonID_FilterName.ndDate %>% na.omit %>% {attr(., "na.action") = NULL; .} %>% dput #----
 # > tblPersonID_FilterName.ndDate %>% na.omit %>% {attr(., "na.action") = NULL; .} %>% dput #----
 structure(list(ENROLID = c(171946801, 1210879901, 1282775301,
@@ -345,12 +346,6 @@ structure(list(ENROLID = c(171946801, 1210879901, 1282775301,
 5L, 1L, 3L, 1L, 2L, 1L), t_NInf_0.RxCONCEPT_NDC_metformin.ndDate = c(1L,
 1L, 7L, 2L, 1L, 1L, 2L)), row.names = c(NA, -7L), class = c("tbl_df",
 "tbl", "data.frame"))
-
-
-
-
-
-
 
 #@ end -----
 
