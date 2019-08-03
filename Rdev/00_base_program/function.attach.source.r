de6719fd2ce79606fc47f1ed43d6365ef0ecff84
mkim0710 <mkim0710@gmail.com>
2019-08-03 10:34:29 +0900

    Create function.attach.source.r

---
+++ Rdev/00_base_program/function.attach.source.r
@@ -0,0 +1,7 @@
+search()
+ls()
+function.attach = function(Parametmer.list) {
+    for (v in 1:length(Parametmer.list)) assign(names(Parametmer.list)[v], Parametmer.list[[v]], envir = .GlobalEnv)
+}
+search()
+ls()
