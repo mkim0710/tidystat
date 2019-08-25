08d2eb13bfac2247728e124b8e14d1a839b01b42
mkim0710 <mkim0710@gmail.com>
2019-08-25 14:40:36 +0900

    Update and rename function.regex.except.source.r to function.RegEx.except.source.r

--- Rdev/10_import_clean_datatype/15_cleaning_text/function.RegEx.except.source.r
+++ Rdev/10_import_clean_datatype/15_cleaning_text/function.RegEx.except.source.r
@@ -1,4 +1,4 @@
-# function.regex.except.source.r
+# function.RegEx.except.source.r
 
 # https://stackoverflow.com/questions/1687620/regex-match-everything-but-specific-pattern
 # https://stackoverflow.com/questions/406230/regular-expression-to-match-a-line-that-doesnt-contain-a-word/31825535
