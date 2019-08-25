95eb07048ecd36ff547469ae065bf837aea7215a
mkim0710 <mkim0710@gmail.com>
2019-08-26 02:03:26 +0900

    Update function.pipe.source.r
    Former-commit-id: 88557957ae176ebeebb92fb043ad64919bc52527

--- Rdev/00_base_program/function.pipe.source.r
+++ Rdev/00_base_program/function.pipe.source.r
@@ -25,12 +25,143 @@
 
 
 
+# https://github.com/mkim0710/tidystat/blob/master/Rdev/10_import_clean_datatype/13_duplicated/data.duplicated.source.r
+
+
+
+library(tidyverse)
+
+# https://stackoverflow.com/questions/22959635/remove-duplicated-rows-using-dplyr
+set.seed(1234)
+df.example = data.frame(x=sample(0:1,10,replace=T),y=sample(0:1,10,replace=T)) %>% rownames_to_column
+# df.example %>% tribble_paste #----
+tibble::tribble(
+    ~rowname, ~x, ~y,
+         "1", 0L, 1L,
+         "2", 1L, 1L,
+         "3", 1L, 0L,
+         "4", 1L, 1L,
+         "5", 1L, 0L,
+         "6", 1L, 1L,
+         "7", 0L, 0L,
+         "8", 0L, 0L,
+         "9", 1L, 0L,
+        "10", 1L, 0L
+    )
+
+
+df.example %>% group_by(x,y) %>% summarize(n())
+# > df.example %>% group_by(x,y) %>% summarize(n())
+# # A tibble: 4 x 3
+# # Groups:   x [2]
+#       x     y `n()`
+#   <int> <int> <int>
+# 1     0     0     2
+# 2     0     1     1
+# 3     1     0     4
+# 4     1     1     3
+
+
+df.example %>% 
+    (function(df) {
+        df %>% group_by(x,y) %>% summarize(n()) %>% 
+            left_join(df)
+    })
+# > df.example %>% 
+# +     (function(df) {
+# +         df %>% group_by(x,y) %>% summarize(n()) %>% 
+# +             left_join(df)
+# +     })
+# Joining, by = c("x", "y")
+# # A tibble: 10 x 4
+# # Groups:   x [2]
+#        x     y `n()` rowname
+#    <int> <int> <int> <chr>  
+#  1     0     0     2 7      
+#  2     0     0     2 8      
+#  3     0     1     1 1      
+#  4     1     0     4 3      
+#  5     1     0     4 5      
+#  6     1     0     4 9      
+#  7     1     0     4 10     
+#  8     1     1     3 2      
+#  9     1     1     3 4      
+# 10     1     1     3 6  
+
+
+
+
+df.example %>% 
+    (function(df.example) {
+        df.example %>% group_by(x,y) %>% summarize(n()) %>% 
+            left_join(df.example)
+    }) %>% group_by(x, y, `n()`) %>% nest
+# > df.example %>% 
+# +     (function(df.example) {
+# +         df.example %>% group_by(x,y) %>% summarize(n()) %>% 
+# +             left_join(df.example)
+# +     }) %>% group_by(x, y, `n()`) %>% nest
+# Joining, by = c("x", "y")
+# # A tibble: 4 x 4
+#       x     y `n()` data            
+#   <int> <int> <int> <list>          
+# 1     0     0     2 <tibble [2 x 1]>
+# 2     0     1     1 <tibble [1 x 1]>
+# 3     1     0     4 <tibble [4 x 1]>
+# 4     1     1     3 <tibble [3 x 1]>
+
+
+df.example %>% group_by(x, y) %>% nest %>% 
+    mutate(nrow = data %>% map_dbl(nrow))
+df.example %>% group_by(x, y) %>% nest %>% arrange(x, y) %>% 
+    mutate(nrow = data %>% map_dbl(nrow))
+# > df.example %>% group_by(x, y) %>% nest %>% 
+# +     mutate(nrow = data %>% map_dbl(nrow))
+# # A tibble: 4 x 4
+#       x     y data              nrow
+#   <int> <int> <list>           <dbl>
+# 1     0     1 <tibble [1 x 1]>     1
+# 2     1     1 <tibble [3 x 1]>     3
+# 3     1     0 <tibble [4 x 1]>     4
+# 4     0     0 <tibble [2 x 1]>     2
+# > df.example %>% group_by(x, y) %>% nest %>% arrange(x, y) %>% 
+# +     mutate(nrow = data %>% map_dbl(nrow))
+# # A tibble: 4 x 4
+#       x     y data              nrow
+#   <int> <int> <list>           <dbl>
+# 1     0     0 <tibble [2 x 1]>     2
+# 2     0     1 <tibble [1 x 1]>     1
+# 3     1     0 <tibble [4 x 1]>     4
+# 4     1     1 <tibble [3 x 1]>     3
+
+
+df.example %>% group_by(x, y) %>% nest %>% arrange(x, y) %>% 
+    mutate(data = data %>% map_chr(paste0, collapse = "|"))
+# > df.example %>% group_by(x, y) %>% nest %>% arrange(x, y) %>% 
+# +     mutate(data = data %>% map_chr(paste0, collapse = "|"))
+# # A tibble: 4 x 3
+#       x     y data                            
+#   <int> <int> <chr>                           
+# 1     0     0 "c(\"7\", \"8\")"               
+# 2     0     1 1                               
+# 3     1     0 "c(\"3\", \"5\", \"9\", \"10\")"
+# 4     1     1 "c(\"2\", \"4\", \"6\")" 
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+#@ ------
 d.ID_DATE_DX.distinct.byID_min_rank_lmp.ID_lmp.ge_lmp_365_le_enddate.CONCEPT_NDC_DM.na_rm.metformin.2797 %>% 
     filter(CONCEPT_NDC_metformin) %>% 
     filter(lmp + 7*6 <= SVCDATE & SVCDATE < lmp + 7*23) %>% 
