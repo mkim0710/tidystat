da6249beaeb2b1ec15e0340b61f0fb4deb75e7ad
mkim0710 <mkim0710@gmail.com>
2019-09-13 16:48:40 +0900

    Create function.readClipboard.source.r
    Former-commit-id: 0ec8c38535c24e624486bc53572c10ea183e4df2

---
+++ Rdev/00_base_program/function.readClipboard.source.r
@@ -0,0 +1,35 @@
+
+
+# https://www.dummies.com/programming/r/how-to-use-the-clipboard-to-copy-and-paste-data-in-r/
+
+library(tidyverse)
+
+readClipboard() %>% dput #----
+# > readClipboard() %>% dput #----
+# c("667\t2\t0\t0", "267\t7\t0\t0", "14\t83\t2\t1", "4\t18\t1\t0", 
+# "2\t13\t69\t1", "0\t1\t1\t22", "4\t12\t0\t184")
+
+read.table(file = "clipboard", sep = "\t", header=F) %>% dput #----
+# structure(list(V1 = c(667L, 267L, 14L, 4L, 2L, 0L, 4L), V2 = c(2L, 
+# 7L, 83L, 18L, 13L, 1L, 12L), V3 = c(0L, 0L, 2L, 1L, 69L, 1L, 
+# 0L), V4 = c(0L, 0L, 1L, 0L, 1L, 22L, 184L)), class = "data.frame", row.names = c(NA, 
+# -7L))
+
+tribble_paste = datapasta::tribble_paste
+# read.table(file = "clipboard", sep = "\t", header=F) %>% tribble_paste #----
+tibble::tribble(
+   ~V1, ~V2, ~V3,  ~V4,
+  667L,  2L,  0L,   0L,
+  267L,  7L,  0L,   0L,
+   14L, 83L,  2L,   1L,
+    4L, 18L,  1L,   0L,
+    2L, 13L, 69L,   1L,
+    0L,  1L,  1L,  22L,
+    4L, 12L,  0L, 184L
+  )
+
+
+# https://www.johndcook.com/blog/r_excel_clipboard/
+objectname %>% writeClipboard
+objectname %>% write.table("clipboard", sep="\t", row.names=FALSE, col.names=FALSE)
+
