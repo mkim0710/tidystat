7e0fc8d582b1c70f2c007e32186bf90260eda44d
mkim0710 <mkim0710@gmail.com>
2019-11-28 15:31:15 +0900

    Create function.alarm.source.r
    Former-commit-id: d182cd488aae5fbb1c10be6327e8003237fc7e35

---
+++ Rdev/00_base_program/function.alarm.source.r
@@ -0,0 +1,16 @@
+
+
+alarm()
+
+system("rundll32 user32.dll, MessageBeep -1")
+
+# https://stackoverflow.com/questions/3365657/is-there-a-way-to-make-r-beep-play-a-sound-at-the-end-of-a-script
+beep <- function(n = 3){
+    for(i in seq(n)){
+        system("rundll32 user32.dll,MessageBeep -1")
+        Sys.sleep(.5)
+    }
+}
+
+
+#@ end -----
