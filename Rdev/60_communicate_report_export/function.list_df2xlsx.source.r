685bc1fe6320a457453997f80f2fdeca287aad99
mkim0710 <mkim0710@gmail.com>
2020-01-01 16:40:07 +0900

    Update function.list_df2xlsx.source.r

--- Rdev/60_communicate_report_export/function.list_df2xlsx.source.r
+++ Rdev/60_communicate_report_export/function.list_df2xlsx.source.r
@@ -2,11 +2,11 @@
 
 function.list_df2xlsx = function(list_df, asTable = T, withFilter = T, overwrite2annotate = F, openXL.filename = " - annotated.xlsx") {
   library(openxlsx)
-  filename = deparse(substitute(list_df))
-  openxlsx::write.xlsx(eval(parse(text = filename)), file = paste0(filename, ".xlsx"), asTable = asTable, withFilter = withFilter)
-  file.copy(from = paste0(filename, ".xlsx"), to = paste0(filename, " - annotated.xlsx"), overwrite = overwrite2annotate)
-  # openxlsx::openXL(file = paste0(filename, ".xlsx"))
-  openxlsx::openXL(file = paste0(filename, openXL.filename))
+  objectname = deparse(substitute(list_df))
+  openxlsx::write.xlsx(eval(parse(text = objectname)), file = paste0(objectname, ".xlsx"), asTable = asTable, withFilter = withFilter)
+  file.copy(from = paste0(objectname, ".xlsx"), to = paste0(objectname, " - annotated.xlsx"), overwrite = overwrite2annotate)
+  # openxlsx::openXL(file = paste0(objectname, ".xlsx"))
+  openxlsx::openXL(file = paste0(objectname, openXL.filename))
 }
 
 
