60de8bf65f3e0b236b9bdb49c10212c5090d5332
mkim0710 <mkim0710@gmail.com>
2020-01-05 19:12:08 +0900

    Create calculate.smd.source.r

---
+++ Rdev/60_communicate_report_export/calculate.smd.source.r
@@ -0,0 +1,46 @@
+
+
+
+
+# https://github.com/kaz-yos/tableone/blob/48edcec1998969774e1b9349bd4528689fe1b2ee/R/modules-smd.R
+### Continuous/binary standardized mean differences
+## Expects continuous or 0,1 binary variable
+StdDiff <- function(variable, group, binary = FALSE, na.rm = TRUE) {
+
+    ## Check strata for all NA strata
+    logiAllNaStrata <- CheckNaOnlyStrata(variable, group)
+    ## If ANY strata have only NA's do not remove NA's
+    if (any(logiAllNaStrata)) {
+        warning("Variable has only NA's in at least one stratum. na.rm turned off.")
+        na.rm = FALSE
+    }
+
+    ## Proportion of 1 is the mean of variable
+    means <- tapply(variable, group, mean, na.rm = na.rm)
+
+    ## Variance is p(1-p)
+    if (binary) {
+        vars  <- means * (1 - means)
+    } else {
+        vars  <- tapply(variable, group, var, na.rm = na.rm)
+    }
+
+    ## Outer to obtain all pairwise differences
+    meanDiffs  <- outer(X = means, Y = means, FUN = "-")
+    ## Outer to obtain all pairwise variance mean
+    varMeans   <- outer(X = vars, Y = vars, FUN = "+") / 2
+
+    out <- meanDiffs / sqrt(varMeans)
+
+    ## If mean difference is zero and variance is zero,
+    ## only one constant exists across two groups.
+    ## In this case, the SMD can be defined zero, rather than NaN from 0/0.
+    out[is.na(out) & !is.na(meanDiffs) & (meanDiffs == 0) & !is.na(varMeans) & (varMeans == 0)] <- 0
+
+    ## This lower.tri() approach is actually giving 2vs1, 3vs1, etc
+    ## opposite of stated 1vs2, 1vs3. Only correct if abs() is used.
+    abs(out[lower.tri(out)])
+}
+
+
+
