55e289d2904c3645f0f8bd850cde2696c923c912
mkim0710 <mkim0710@gmail.com>
2020-04-26 00:58:17 +0900

    Update data.mutate_at.rename_at.source.r
    Former-commit-id: 3291e756fb6ffb32cb972381a591bda09d859f6a

--- Rdev/10_import_clean_datatype/13_missing_value/data.mutate_at.rename_at.source.r
+++ Rdev/10_import_clean_datatype/13_missing_value/data.mutate_at.rename_at.source.r
@@ -5,9 +5,13 @@
 
 ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>% 
     select_at(vars(matches("\\.ndDate$"))) %>% summary #-----
+ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>%
+    mutate_at(vars(matches("\\.ndDate$")), replace_na, 0) %>%
+    rename_at(vars(matches("\\.ndDate$")), list(~paste0(., ".replace_na"))) %>%
+    select_at(vars(matches("\\.replace_na"))) %>% summary #-----
 ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>% 
     mutate_at(vars(matches("\\.ndDate$")), replace_na, 0) %>% 
-    rename_at(vars(matches("\\.ndDate$")), list(~paste0(., ".replace_na"))) %>% 
+    rename_at(vars(matches("\\.ndDate$")), function(vec) paste0(vec, ".replace_na")) %>% 
     select_at(vars(matches("\\.replace_na"))) %>% summary #-----
 # > ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>% 
 # +     select_at(vars(matches("\\.ndDate$"))) %>% summary #-----
@@ -27,9 +31,9 @@ ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>%
 #  3rd Qu.: 6.000                               3rd Qu.: 4.000                                 3rd Qu.:2.000                                  
 #  Max.   :16.000                               Max.   :12.000                                 Max.   :4.000                                  
 #                                               NA's   :373                                    NA's   :541                                    
-# > ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>% 
-# +     mutate_at(vars(matches("\\.ndDate$")), replace_na, 0) %>% 
-# +     rename_at(vars(matches("\\.ndDate$")), list(~paste0(., ".replace_na"))) %>% 
+# > ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>%
+# +     mutate_at(vars(matches("\\.ndDate$")), replace_na, 0) %>%
+# +     rename_at(vars(matches("\\.ndDate$")), list(~paste0(., ".replace_na"))) %>%
 # +     select_at(vars(matches("\\.replace_na"))) %>% summary #-----
 #  Window4Eligible.RxCONCEPT_NDC_insulin.ndDate.replace_na Window4Eligible.RxCONCEPT_NDC_metformin.ndDate.replace_na Window4Eligible.RxCONCEPT_NDC_SU_TZD_etc.ndDate.replace_na
 #  Min.   : 1.000                                          Min.   :1.000                                             Min.   :0.000                                             
@@ -45,8 +49,24 @@ ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>%
 #  Mean   : 4.143                                          Mean   : 0.8463                                           Mean   :0.1192                                            
 #  3rd Qu.: 6.000                                          3rd Qu.: 1.0000                                           3rd Qu.:0.0000                                            
 #  Max.   :16.000                                          Max.   :12.0000                                           Max.   :4.0000                                            
-
-
+# > ID_Eligible_Exposure.TargetTrial2v40.2.206vs373.Outcome.Covariates.DDD.A1c %>% 
+# +     mutate_at(vars(matches("\\.ndDate$")), replace_na, 0) %>% 
+# +     rename_at(vars(matches("\\.ndDate$")), function(vec) paste0(vec, ".replace_na")) %>% 
+# +     select_at(vars(matches("\\.replace_na"))) %>% summary #-----
+#  Window4Eligible.RxCONCEPT_NDC_insulin.ndDate.replace_na Window4Eligible.RxCONCEPT_NDC_metformin.ndDate.replace_na Window4Eligible.RxCONCEPT_NDC_SU_TZD_etc.ndDate.replace_na
+#  Min.   : 1.000                                          Min.   :1.000                                             Min.   :0.000                                             
+#  1st Qu.: 1.000                                          1st Qu.:1.000                                             1st Qu.:0.000                                             
+#  Median : 2.000                                          Median :2.000                                             Median :0.000                                             
+#  Mean   : 2.339                                          Mean   :2.591                                             Mean   :1.193                                             
+#  3rd Qu.: 3.000                                          3rd Qu.:4.000                                             3rd Qu.:2.000                                             
+#  Max.   :13.000                                          Max.   :9.000                                             Max.   :9.000                                             
+#  Window4Exposure.RxCONCEPT_NDC_insulin.ndDate.replace_na Window4Exposure.RxCONCEPT_NDC_metformin.ndDate.replace_na Window4Exposure.RxCONCEPT_NDC_SU_TZD_etc.ndDate.replace_na
+#  Min.   : 1.000                                          Min.   : 0.0000                                           Min.   :0.0000                                            
+#  1st Qu.: 2.000                                          1st Qu.: 0.0000                                           1st Qu.:0.0000                                            
+#  Median : 4.000                                          Median : 0.0000                                           Median :0.0000                                            
+#  Mean   : 4.143                                          Mean   : 0.8463                                           Mean   :0.1192                                            
+#  3rd Qu.: 6.000                                          3rd Qu.: 1.0000                                           3rd Qu.:0.0000                                            
+#  Max.   :16.000                                          Max.   :12.0000                                           Max.   :4.0000                                            
 
 
 
