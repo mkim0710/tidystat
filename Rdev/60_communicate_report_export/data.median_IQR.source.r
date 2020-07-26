caa2c97aab74710227ed54b8db33c59ac08fe9bc
mkim0710 <mkim0710@gmail.com>
2020-07-26 17:53:11 +0900

    Create data.median_IQR.source.r
    Former-commit-id: 8cf3081c7cd608478fa550a9e0d30b0ee26019cb

---
+++ Rdev/60_communicate_report_export/data.median_IQR.source.r
@@ -0,0 +1,15 @@
+
+#@ data.median_IQR.source.r ------
+
+analyticDF_C24C221.drop_pmhx_negativetime.list.cut.addVars._5yr.Match1_5.assigned5532.recode %>% 
+    mutate(time = fuduration_yr/365) %>% 
+    group_by(is.Case) %>% summarise(mean(time), sd(time), quantile(time, probs = 1/4), median(time), quantile(time, probs = 3/4)) #-----
+# > analyticDF_C24C221.drop_pmhx_negativetime.list.cut.addVars._5yr.Match1_5.assigned5532.recode %>% 
+# +     mutate(time = fuduration_yr/365) %>% 
+# +     group_by(is.Case) %>% summarise(mean(time), sd(time), quantile(time, probs = 1/4), median(time), quantile(time, probs = 3/4)) #-----
+# # A tibble: 2 x 6
+#   is.Case `mean(time)` `sd(time)` `quantile(time, probs = 1/4)` `median(time)` `quantile(time, probs = 3/4)`
+#   <lgl>          <dbl>      <dbl>                         <dbl>          <dbl>                         <dbl>
+# 1 FALSE           6.67       1.01                          7.00           7.00                          7.00
+# 2 TRUE            3.58       2.01                          1.87           3.63                          5.32
+
