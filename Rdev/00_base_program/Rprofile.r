398473dae09827ec8d54fb41c4cdc53c63021877
mkim0710 <mkim0710@gmail.com>
2020-10-22 15:54:19 +0900

    Update Rprofile.r
    Former-commit-id: a2df1ef8fa2311980be29bd50861a8c3071ae690

--- Rdev/00_base_program/Rprofile.r
+++ Rdev/00_base_program/Rprofile.r
@@ -38,6 +38,8 @@ Sys.setlocale("LC_ALL", "korean") #----
 # https://medium.com/@ValidScience/how-to-fix-rstudios-package-installation-on-windows-10-c1e602bf3a1f
 
 ## Before edit -----
+# > Sys.getenv("R_USER") 
+# [1] "C:/Users/mkim0/Documents"
 # > Sys.getenv("R_LIBS_USER")
 # [1] "C:/Users/mkim0/Documents/R/win-library/4.0"
 
@@ -58,6 +60,7 @@ Sys.setlocale("LC_ALL", "korean") #----
 
 if(!nzchar(Sys.getenv("R_LIBS_USER")))
     Sys.setenv(R_LIBS_USER=
+#                file.path(Sys.getenv("R_USER"), "R",
                file.path("c:/R packages download", "R",
                          "win-library",
                          paste(R.version$major,
