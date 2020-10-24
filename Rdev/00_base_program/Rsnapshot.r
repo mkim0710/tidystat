3eecdb1406adfa0188db2f6e9eef8d2f7aa651f9
mkim0710 <mkim0710@gmail.com>
2020-10-25 07:49:18 +0900

    Update Rsnapshot.r

--- Rdev/00_base_program/Rsnapshot.r
+++ Rdev/00_base_program/Rsnapshot.r
@@ -2,5 +2,5 @@
 
 # https://cran.r-project.org/web/packages/icd/index.html
 # https://mran.microsoft.com/snapshot/2020-07-16/bin/windows/contrib/4.0/datapasta_3.1.0.zip
-# https://mran.microsoft.com/snapshot/2020-07-16/bin/windows/contrib/4.0/datapasta_3.1.0.zip
+# https://mran.microsoft.com/snapshot/2020-07-16/web/packages/icd/index.html
 
