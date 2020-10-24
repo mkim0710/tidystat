76d0d7fe49e86b94e4e84f9d2376d3bd8c5f579c
mkim0710 <mkim0710@gmail.com>
2020-10-25 07:48:23 +0900

    Create Rsnapshot.r
    Former-commit-id: f0949bc9b3efd64f92fbf517521841ae0b2dc8f5

---
+++ Rdev/00_base_program/Rsnapshot.r
@@ -0,0 +1,6 @@
+
+
+# https://cran.r-project.org/web/packages/icd/index.html
+# https://mran.microsoft.com/snapshot/2020-07-16/bin/windows/contrib/4.0/datapasta_3.1.0.zip
+# https://mran.microsoft.com/snapshot/2020-07-16/bin/windows/contrib/4.0/datapasta_3.1.0.zip
+
