9de13fcdff77460205a5e084cc47cad693c275b4
mkim0710 <mkim0710@gmail.com>
2020-10-25 07:49:18 +0900

    Update Rsnapshot.r

---
+++ git automatic commit.r
@@ -0,0 +1,14 @@
+# git automatic commit.r
+
+system("git pull")
+system("git add .")
+system('git commit -m "automatic commit using system()"')
+system("git push")
+
+
+# # https://stackoverflow.com/questions/22135465/cant-commit-after-starting-a-merge-in-sourcetree/22135512#22135512
+# # Do a git commit -a once you have resolved the conflicts. This is the last step when you are merging conflicts.
+# 
+# system('git commit -a')
+
+# Caution) do not forget to add .gitignore to .gitignore
