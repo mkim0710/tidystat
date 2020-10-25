9b13a525865ba109009f62b3f4fd7fce338410bd
mkim0710 <mkim0710@gmail.com>
2020-10-25 19:23:46 +0900

    Update RStudio.Cloud set up.r

--- RStudio.Cloud set up.r
+++ RStudio.Cloud set up.r
@@ -1,6 +1,6 @@
 # RStudio.Cloud set up.r 
-# https://rstudio.cloud/project/1815569
-# https://rstudio.cloud/project/1815559
+# https://rstudio.cloud/project/1815569  # RStudio.Cloud
+# https://rstudio.cloud/project/1815559  # tidystat
 
 
 for (packagename in c("tidyverse", "datapasta")) {
