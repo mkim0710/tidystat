fef97cef6105b18e29cd89cbfb850411f79c4aff
mkim0710 <mkim0710@gmail.com>
2020-10-25 16:34:19 +0900

    Update git automatic commit.r
    Former-commit-id: 2087e2a71558f1da476c2b69374aa6214b4e12c5

--- git automatic commit.r
+++ git automatic commit.r
@@ -6,6 +6,7 @@ system("git add .")
 system('git commit -m "automatic commit using system()"')
 system("git push")
 
+# system("git fetch")
 
 # # https://stackoverflow.com/questions/22135465/cant-commit-after-starting-a-merge-in-sourcetree/22135512#22135512
 # # Do a git commit -a once you have resolved the conflicts. This is the last step when you are merging conflicts.
