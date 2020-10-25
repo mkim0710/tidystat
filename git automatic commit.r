535d7489b7edade0c917960f8876ae40e22dfd27
mkim0710 <mkim0710@gmail.com>
2020-10-25 16:40:45 +0900

    Update git automatic commit.r
    Former-commit-id: 01dcdae31d80dd15dfd88721caf924b97aca303a

--- git automatic commit.r
+++ git automatic commit.r
@@ -1,10 +1,12 @@
 # git automatic commit.r
 # # To paste text in the terminal use Shift + Insert.
 
+system("git status")
 system("git pull")
 system("git add .")
 system('git commit -m "automatic commit using system()"')
 system("git push")
+# system("git status")
 
 # system("git fetch")
 
