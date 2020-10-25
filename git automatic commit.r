82958676452e954e92b7d62efea571a79808d810
mkim0710@RStudio.Cloud <mkim0710@gmail.com>
2020-10-25 12:02:22 +0000

    automatic commit using system()
    Former-commit-id: 9339ff22cd892653cff72a635db805f76d8edce6

--- git automatic commit.r
+++ git automatic commit.r
@@ -5,6 +5,7 @@ system("git status")
 system("git pull")
 system("git add .")
 system('git commit -m "automatic commit using system()"')
+system("git config --global push.default simple")
 system("git push")
 # system("git status")
 
