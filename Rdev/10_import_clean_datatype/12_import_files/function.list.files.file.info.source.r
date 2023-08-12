361784477c3bad5baec8b6082357536e62a62129
mkim0710 <mkim0710@gmail.com>
2023-08-13 05:45:46 +0900

    Update function.list.files.file.info.source.r
    Former-commit-id: b50c1622e9a2fd5d200b732e39a90f5b37fdc590

--- Rdev/10_import_clean_datatype/12_import_files/function.list.files.file.info.source.r
+++ Rdev/10_import_clean_datatype/12_import_files/function.list.files.file.info.source.r
@@ -4,6 +4,25 @@ library(tidyverse)
 getwd() %>% dput
 # > getwd() %>% dput
 # "../../[[[[SASproject]]]]/SAS_NHIS_HEALS"
+path4read = getwd()
+path4write = getwd()
+path4read %>% dput
+path4write %>% dput
+
+# tribble_paste = datapasta::tribble_paste
+# https://github.com/mkim0710/tidystat/blob/master/Rdev/env.custom.fun.t.tribble_construct.source.r
+load(url("https://github.com/mkim0710/tidystat/raw/master/Rdev/env.custom.fun.t.tribble_construct.RData"))
+# attach(env.custom)
+
+regex4filename = "\\.sas7bdat\\.rds(\\.xz)?$"
+env.custom$fun.path_files_size(path4read = path4read, regex4filename = regex4filename)
+filenames = list.files(path4read) %>% grep(regex4filename, ., value = T) 
+
+
+
+
+
+
 
 nchar(path4read)
 substr(path4read, nchar(path4read), nchar(path4read))
