c21197378b1b23986cbabd6eb5bc0a1b32cff920
mkim0710 <mkim0710@gmail.com>
2023-08-14 02:43:16 +0900

    Update dir() read_sas() write_rds().r

--- Rdev/10_import_clean_datatype/12_import_files/dir() read_sas() write_rds().r
+++ Rdev/10_import_clean_datatype/12_import_files/dir() read_sas() write_rds().r
@@ -5,21 +5,21 @@
 
 library(tidyverse)
 getwd() %>% dput #----
-
-path4read = getwd()
+path4read = "../github_tidystat/data"
 path4write = getwd()
-path4read %>% dput
-path4write %>% dput
 
 # tribble_paste = datapasta::tribble_paste
 # https://github.com/mkim0710/tidystat/blob/master/Rdev/env.custom.fun.t.tribble_construct.source.r
 load(url("https://github.com/mkim0710/tidystat/raw/master/Rdev/env.custom.fun.t.tribble_construct.RData"))
 # attach(env.custom)
 
+# regex4filename = "\\.(rdata|rda|rds)$"
+# regex4filename = "\\.rds$"
 # regex4filename = "\\.sas7bdat(\\.xz)?$"
 regex4filename = "\\.sas7bdat$"
 env.custom$fun.path_files_size(path4read = path4read, regex4filename = regex4filename)
 filenames = list.files(path4read) %>% grep(regex4filename, ., value = T) 
+filenames %>% dput #----
 
 
 
