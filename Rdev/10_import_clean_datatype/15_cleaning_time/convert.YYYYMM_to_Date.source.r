a5157e912fe098b81b7aa24c3d3b90e9ef94cac1
mkim0710 <mkim0710@gmail.com>
2023-08-30 16:17:31 +0900

    Create convert.YYYYMM_to_Date.source.r

---
+++ Rdev/10_import_clean_datatype/15_cleaning_time/convert.YYYYMM_to_Date.source.r
@@ -0,0 +1,10 @@
+convert.YYYYMM_to_Date <- function(string_YYYYMM) {
+    library(lubridate)
+    ceiling_date(as.Date(paste0(string_YYYYMM, "01"), format="%Y%m%d"), "month") - days(1)
+}
+dput(as.character(convert.YYYYMM_to_Date(202300+1:12)))
+c("2023-01-31", "2023-02-28", "2023-03-31", "2023-04-30", "2023-05-31", "2023-06-30",
+  "2023-07-31", "2023-08-31", "2023-09-30", "2023-10-31", "2023-11-30", "2023-12-31")
+dput(convert.YYYYMM_to_Date(202300+1:12))
+structure(c(19388, 19416, 19447, 19477, 19508, 19538, 19569, 
+19600, 19630, 19661, 19691, 19722), class = "Date")
