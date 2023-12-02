e5fc8e38cf649f7891eab10bcec0e294188c866a
mkim0710 <mkim0710@gmail.com>
2023-12-02 17:30:36 +0900

    Update and rename convert.YYYYMM_to_Date.source.r to function.YYYYMM_to_Date.source.r
    Former-commit-id: 1c186e7f260a26f2cfb7532956c39893515bf482

--- Rdev/10_import_clean_datatype/15_cleaning_time/function.YYYYMM_to_Date.source.r
+++ Rdev/10_import_clean_datatype/15_cleaning_time/function.YYYYMM_to_Date.source.r
@@ -1,31 +1,33 @@
 # https://github.com/mkim0710/tidystat/blob/master/Rdev/10_import_clean_datatype/15_cleaning_time/convert.YYYYMM_to_Date.source.r
-#%% convert.YYYYMM_to_Date() =====
-convert.YYYYMM_to_Date <- function(string_YYYYMM) {
+#%% function.YYYYMM_to_Date() =====
+function.YYYYMM_to_Date <- function(string_YYYYMM) {
     library(lubridate)
     ceiling_date(as.Date(paste0(string_YYYYMM, "01"), format="%Y%m%d"), "month") - days(1)
 }
-dput(as.character(convert.YYYYMM_to_Date(202300+1:12)))
+dput(as.character(function.YYYYMM_to_Date(202300+1:12)))
 c("2023-01-31", "2023-02-28", "2023-03-31", "2023-04-30", "2023-05-31", "2023-06-30",
   "2023-07-31", "2023-08-31", "2023-09-30", "2023-10-31", "2023-11-30", "2023-12-31")
-dput(convert.YYYYMM_to_Date(202300+1:12))
+dput(function.YYYYMM_to_Date(202300+1:12))
 structure(c(19388, 19416, 19447, 19477, 19508, 19538, 19569, 
 19600, 19630, 19661, 19691, 19722), class = "Date")
+
+
 
 
 
 # https://github.com/mkim0710/tidystat/blob/master/Rdev/10_import_clean_datatype/15_cleaning_time/convert.YYYYMM_to_Date.source.r
-#%% convert.YYYYMMDD_to_Date() =====
-convert.YYYYMMDD_to_Date <- function(string_YYYYMMDD) {
+#%% function.YYYYMMDD_to_Date() =====
+function.YYYYMMDD_to_Date <- function(string_YYYYMMDD) {
     as.Date(string_YYYYMMDD, format="%Y%m%d")
 }
 c("20230131", "20230228", "20230331", "20230430", "20230531", "20230630",
   "20230731", "20230831", "20230930", "20231031", "20231130", "20231231") %>% 
-    convert.YYYYMMDD_to_Date %>% dput
+    function.YYYYMMDD_to_Date %>% dput
 structure(c(19388, 19416, 19447, 19477, 19508, 19538, 19569, 
 19600, 19630, 19661, 19691, 19722), class = "Date")
 c("20230131", "20230228", "20230331", "20230430", "20230531", "20230630",
   "20230731", "20230831", "20230930", "20231031", "20231130", "20231231") %>% 
-    convert.YYYYMMDD_to_Date %>% as.character %>% dput
+    function.YYYYMMDD_to_Date %>% as.character %>% dput
 c("2023-01-31", "2023-02-28", "2023-03-31", "2023-04-30", "2023-05-31", "2023-06-30",
   "2023-07-31", "2023-08-31", "2023-09-30", "2023-10-31", "2023-11-30", "2023-12-31")
 
