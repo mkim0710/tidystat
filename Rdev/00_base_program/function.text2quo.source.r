9f640e5a1185c35f4c3f5405e6517a436353c000
mkim0710 <mkim0710@gmail.com>
2023-12-02 13:03:18 +0900

    Update function.text2quo.source.r

--- Rdev/00_base_program/function.text2quo.source.r
+++ Rdev/00_base_program/function.text2quo.source.r
@@ -221,31 +221,6 @@ eval(~txt) %>% dput
 
 
 
-eval(parse(text = paste(1, ">1")))
-eval(parse(text = paste(1.1, ">1")))
-eval(parse(text = paste(2, ">1")))
-eval(parse(text = paste(c(1,2), ">1")))
-eval(parse(text = paste(c(2,2), ">1")))
-eval(parse(text = paste(c(2,2,1), ">1")))
-eval(parse(text = paste(c(2,2,1.1), ">1")))
-# > eval(parse(text = paste(1, ">1")))
-# [1] FALSE
-# > eval(parse(text = paste(1.1, ">1")))
-# [1] TRUE
-# > eval(parse(text = paste(2, ">1")))
-# [1] TRUE
-# > eval(parse(text = paste(c(1,2), ">1")))
-# [1] TRUE
-# > eval(parse(text = paste(c(2,2), ">1")))
-# [1] TRUE
-# > eval(parse(text = paste(c(2,2,1), ">1")))
-# [1] FALSE
-# > eval(parse(text = paste(c(2,2,1.1), ">1")))
-# [1] TRUE
-
-
-
-
 #@ end -----
 
 
