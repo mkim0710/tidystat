adf141e3433c2f4fe3eb4f27939951f1f426e48f
mkim0710 <mkim0710@gmail.com>
2023-12-03 03:23:19 +0900

    Update function.str2.longest_common_prefix.source.r
    Former-commit-id: 5ce711559426b708071044bca80eba2d72417d0f

--- Rdev/00_base_program/function.str2.longest_common_prefix.source.r
+++ Rdev/00_base_program/function.str2.longest_common_prefix.source.r
@@ -1,3 +1,5 @@
+# https://github.com/mkim0710/tidystat/blob/master/Rdev/10_import_clean_datatype/array_list/function.list_df2array.source.r
+
 # Function to find the longest common prefix of two strings
 # https://github.com/mkim0710/tidystat/new/master/Rdev/00_base_program/function.str2.longest_common_prefix.source.r
 function.str2.longest_common_prefix <- function(str1, str2) {
@@ -6,6 +8,21 @@ function.str2.longest_common_prefix <- function(str1, str2) {
   min_length <- min(length(chars1), length(chars2))
   chars1 <- chars1[1:min_length]
   chars2 <- chars2[1:min_length]
-  prefix_length <- sum(map2_int(chars1, chars2, ~ ifelse(.x == .y, 1, 0)))
-  substring(str1, 1, prefix_length)
+  prefix_length <- which(map2_int(chars1, chars2, ~ ifelse(.x == .y, 1, 0)) == 0)[1] - 1
+  # print(prefix_length)
+  if (prefix_length == 0) {
+    return(NA)
+  } else {
+    return(substring(str1, 1, prefix_length))
+  }
 }
+
+function.str2.longest_common_prefix("TestString1", "TestString2")
+function.str2.longest_common_prefix("A01_XYZ", "A01_ABZ")
+function.str2.longest_common_prefix("Mismatch", "Example")
+# > function.str2.longest_common_prefix("TestString1", "TestString2")
+# [1] "TestString"
+# > function.str2.longest_common_prefix("A01_XYZ", "A01_ABZ")
+# [1] "A01_"
+# > function.str2.longest_common_prefix("Mismatch", "Example")
+# [1] NA
