a22f62bd3a1438bc10c16215fc29659167b90365
mkim0710 <mkim0710@gmail.com>
2023-12-02 16:06:05 +0900

    Update eval.parse.source.r

--- Rdev/00_base_program/eval.parse.source.r
+++ Rdev/00_base_program/eval.parse.source.r
@@ -1,5 +1,32 @@
 
 
+
+
+
+function.eval.parse.Vectorize = Vectorize(function(text4parse) {eval(parse(text = text4parse))}) 
+function.eval.parse.Vectorize(paste(c(2,1,1.1), ">1")) %>% str
+ # Named logi [1:3] TRUE FALSE TRUE
+ # - attr(*, "names")= chr [1:3] "2 >1" "1 >1" "1.1 >1"
+
+
+#---------------
+function.eval.parse.Vectorize_old = function(text4parse) {
+    Vectorize(function(x) {eval(parse(text = x))}) (text4parse)
+}
+function.eval.parse.Vectorize_old(paste(c(2,1,1.1), ">1")) %>% str
+ # Named logi [1:3] TRUE FALSE TRUE
+ # - attr(*, "names")= chr [1:3] "2 >1" "1 >1" "1.1 >1"
+
+
+#---------------
+temp.fun = function(x) eval(parse(text = paste(x, ">1")))
+Vectorize(temp.fun)(c(2, 1, 1.1))
+# > temp.fun = function(x) eval(parse(text = paste(x, ">1")))
+# > Vectorize(temp.fun)(c(2, 1, 1.1))
+# [1]  TRUE FALSE  TRUE
+
+
+#---------------
 eval(parse(text = paste(1, ">1")))
 eval(parse(text = paste(1.1, ">1")))
 eval(parse(text = paste(2, ">1")))
@@ -22,10 +49,5 @@ eval(parse(text = paste(c(2,2,1.1), ">1")))
 # > eval(parse(text = paste(c(2,2,1.1), ">1")))
 # [1] TRUE
 
-temp.fun = function(x) eval(parse(text = paste(x, ">1")))
-Vectorize(temp.fun)(c(2, 1, 1.1))
-# > temp.fun = function(x) eval(parse(text = paste(x, ">1")))
-# > Vectorize(temp.fun)(c(2, 1, 1.1))
-# [1]  TRUE FALSE  TRUE
 
 
