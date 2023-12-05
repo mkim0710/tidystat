a1eee4bc990216475ca9f9077f268706c85bf210
mkim0710 <mkim0710@gmail.com>
2023-12-06 04:24:26 +0900

    Update function.matrix.split.source.r

--- Rdev/50_model_formula_evaluation/function.matrix.split.source.r
+++ Rdev/50_model_formula_evaluation/function.matrix.split.source.r
@@ -1,6 +1,5 @@
 # https://github.com/mkim0710/tidystat/blob/master/Rdev/50_model_formula_evaluation/function.matrix.split.source.r
 function.matrix.split <- function(input_matrix, prop_train = 0.5, prop_validation = 0.2, set_seed = 123) {
-  # Ensure the proportions sum to 1
   if (prop_train + prop_validation > 1) {
     stop("The sum of prop_train and prop_validation should not exceed 1.")
   }
@@ -19,12 +18,17 @@ function.matrix.split <- function(input_matrix, prop_train = 0.5, prop_validatio
   shuffled_indices.validation <- shuffled_indices[(nrow_train + 1):(nrow_train + nrow_validation)]
   shuffled_indices.test <- shuffled_indices[(nrow_train + nrow_validation + 1):nrow_total]
 
-  # Subset input_matrix to create the sets
-  output_matrix_train <- input_matrix[shuffled_indices.train, ]
-  output_matrix_validation <- input_matrix[shuffled_indices.validation, ]
-  output_matrix_test <- input_matrix[shuffled_indices.test, ]
+  # Subset input_matrix to create the sets, preserving row names
+  output_matrix_train <- input_matrix[shuffled_indices.train, , drop = FALSE]
+  output_matrix_validation <- input_matrix[shuffled_indices.validation, , drop = FALSE]
+  output_matrix_test <- input_matrix[shuffled_indices.test, , drop = FALSE]
 
-  # Return a list of the three input_matrixsets
+  # Assign original row names to subsets
+  row.names(output_matrix_train) <- row.names(input_matrix)[shuffled_indices.train]
+  row.names(output_matrix_validation) <- row.names(input_matrix)[shuffled_indices.validation]
+  row.names(output_matrix_test) <- row.names(input_matrix)[shuffled_indices.test]
+
+  # Return a list of the three datasets
   return(list(train_set = output_matrix_train, validation_set = output_matrix_validation, test_set = output_matrix_test))
 }
 
