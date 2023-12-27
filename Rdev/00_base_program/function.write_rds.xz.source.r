b94e192b24b2b0c69bcd2d84a48ee081ecc70435
mkim0710 <mkim0710@gmail.com>
2023-12-28 02:59:19 +0900

    Update function.write_rds.xz.source.r

--- Rdev/00_base_program/function.write_rds.xz.source.r
+++ Rdev/00_base_program/function.write_rds.xz.source.r
@@ -1,4 +1,39 @@
 
+
+function.read_and_write_rds <- function(filename, compress = c("none", "gz", "bz2", "xz")) {
+  # Set the default compress method and ensure the user input is one of the allowed choices
+  compress <- match.arg(compress)
+
+  # Check if the file is either .rds or .rda
+  if (grepl("\\.[rR][dD][sS]$", filename)) {
+    objectname <- gsub("\\.[rR][dD][sS]$", "", filename)
+    object <- readRDS(filename)
+  } else if (grepl("\\.[rR][dD][aA]$", filename)) {
+    # Loading .rda file
+    env <- new.env()
+    load(filename, env)
+    objectnames <- ls(env)
+
+    # Check if there's exactly one object in the .rda file
+    if (length(objectnames) != 1) {
+      stop("The .rda file must contain exactly one object")
+    }
+    objectname <- objectnames[1]
+    object <- get(objectname, envir = env)
+  } else {
+    stop("The file must be either .rds or .rda")
+  }
+
+  # Save the data using write_rds
+  write_rds(object, paste0(objectname, ".rds"), compress = compress)
+}
+
+
+
+
+
+#@ -------
+
 objectname = "os.ID_DATE_DX.distinct.gather_DX.byID_min_rank_lmp.ID_lmp.ge_lmp_365_le_enddate.lmp_le2014"
 t0 = Sys.time()
 write_rds( get(objectname), file.path(path4write, paste0(objectname, ".rds")) )
