e30c72eda091fbba795b302569924403077d71ee
mkim0710 <mkim0710@gmail.com>
2024-04-05 03:33:49 +0900

    f_path.df_dirs_recursive.source.r
    Former-commit-id: 86c417725a17fd4e11d4f66dddafb428b85ffaaf

--- f_vec.extract_non_alphanumeric_list.source.r
+++ f_vec.extract_non_alphanumeric_list.source.r
@@ -1,17 +1,16 @@
-# f_extract_non_alphanumeric_chars.source.r
+# f_vec.extract_non_alphanumeric_list.source.r
 
 
-library(tidyverse)
 
-f_extract_non_alphanumeric_chars <- function(input_vec) {
-  extract_non_alnum <- function(string) {
+f_vec.extract_non_alphanumeric_list <- function(input_vec) {
+  f_str.extract_non_alphanumeric_vec <- function(string) {
     positions <- gregexpr("[^[:alnum:]]", string)  # Match any non-alphanumeric character
     non_alnum_chars <- regmatches(string, positions)
     unlist(non_alnum_chars)  # Unlist to get a vector of non-alphanumeric characters
   }
   
-  # Use map to apply extract_non_alnum to each element and maintain list structure
-  result_list <- map(input_vec, extract_non_alnum)
+  # Use map to apply f_str.extract_non_alphanumeric_vec to each element and maintain list structure
+  result_list <- map(input_vec, f_str.extract_non_alphanumeric_vec)
   
   # Set the names of the list elements to match the input vector
   names(result_list) <- input_vec
@@ -21,9 +20,11 @@ f_extract_non_alphanumeric_chars <- function(input_vec) {
 
 # Example usage of the function
 input_vec <- c("Hello, World!", "R4 & Python?", "123% Ready!", "12345")
-result <- f_extract_non_alphanumeric_chars(input_vec)
+result <- f_vec.extract_non_alphanumeric_list(input_vec)
 result %>% dput
 list(`Hello, World!` = c(",", " ", "!"), `R4 & Python?` = c(" ", "&", " ", "?"), `123% Ready!` = c("%", " ", "!"), `12345` = character(0))
 
 
-#@ end ----
+
+
+#@ end -----
