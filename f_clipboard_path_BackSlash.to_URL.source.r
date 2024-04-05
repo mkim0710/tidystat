7bed57e8495b75940bc18eae82b3a4feadb62a51
mkim0710 <mkim0710@gmail.com>
2024-04-05 23:51:51 +0900

    Update f_clipboard_path_BackSlash.to_URL.source.r

--- f_clipboard_path_BackSlash.to_URL.source.r
+++ f_clipboard_path_BackSlash.to_URL.source.r
@@ -64,6 +64,7 @@ f_clipboard_path_BackSlash.to_URL(clipboard_path_BackSlash = FALSE)
 
 # Below is the path when opened in the Edge browser: 
 # file:///D:/OneDrive/[Note]/[Note]%20Stat/[[Stat]]%20CRAN%20R/[[[[R]]]]%20Cheatsheets/[[[[Cheat%20Sheets]]]]%20RStudio/data-import%20&%20transformation%20cheatsheet%202017.01.pdf
+# file:///D:/OneDrive/[Note]/[Note]%2520Stat/[[Stat]]%2520CRAN%2520R/[[[[R]]]]%2520Cheatsheets/[[[[Cheat%2520Sheets]]]]%2520RStudio/data-import%2520&%2520transformation%2520cheatsheet%25202017.01.pdf
 #         D:/OneDrive/[Note]/[Note]%20Stat/[[Stat]]%20CRAN%20R/[[[[R]]]]%20Cheatsheets/[[[[Cheat%20Sheets]]]]%20RStudio/data-import%20&%20transformation%20cheatsheet%202017.01.pdf
 
 
