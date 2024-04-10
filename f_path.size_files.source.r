1f462e27b10683711ece5e4e2da13edf0361add8
mkim0710 <mkim0710@gmail.com>
2024-04-11 04:00:45 +0900

    Update f_path.size_files.source.r
    Former-commit-id: 467ce3842d813215c96efb20682956cd178f3906

--- f_path.size_files.source.r
+++ f_path.size_files.source.r
@@ -33,12 +33,20 @@ for (env.custom.dependancy in c("f_df.tribble_construct")) {
 
 
 objectname = "f_path.size_files"
-object = function(path4read = getwd(), regex4filename = "\\.(rdata|rda|rds|csv|sas7bdat)(\\.[gx]z)?$") {
-    # filenames = list.files(path = path4read) %>% {grep(regex4filename, .,  ignore.case = T, value = T)}
-    filenames = list.files(path = path4read, pattern = regex4filename, ignore.case = T)
-    if (length(filenames) == 0) {
-        cat("No files found matching the regex4filename: ", regex4filename, "\n")
-        return()
+object = function(path4read = getwd(), literal_filename = NA, regex4filename = "\\.(rdata|rda|rds|csv|sas7bdat)(\\.[gx]z)?$") {
+    if (is.na(literal_filename)) {
+        # filenames = list.files(path = path4read) %>% {grep(regex4filename, .,  ignore.case = T, value = T)}
+        filenames = list.files(path = path4read, pattern = regex4filename, ignore.case = T)
+        if (length(filenames) == 0) {
+            cat("No files found matching the regex4filename: ", regex4filename, "\n")
+            return()
+        }
+    } else {
+        filenames = list.files(path = path4read) %>% {grep(literal_filename, .,  ignore.case = T, value = T, fixed = T)}
+        if (length(filenames) == 0) {
+            cat("No files found matching the literal_filename: ", literal_filename, "\n")
+            return()
+        }
     }
     filenames %>% {cat(deparse(., width.cutoff=120), '\n', sep='')} # dput() cat(deparse(., width.cutoff=120)), width.cutoff=500 is the max ----
     cat("----\n"); #----
@@ -57,7 +65,6 @@ if(!objectname %in% names(env.custom)) {
     env.custom[[objectname]] = object
     cat("> env.custom$", objectname, "()\n", sep = ""); get(objectname, envir = env.custom)() # Run the loaded function by default
 }
-
 
 # > for (env.custom.dependancy in c("f_df.tribble_construct")) {
 # +     if(!env.custom.dependancy %in% names(env.custom)) {
@@ -82,6 +89,34 @@ if(!objectname %in% names(env.custom)) {
 #   "env.custom.fun.t.tribble_construct.RData.xz", 14792, "14,792", "14.4", "0.0141", "1.38e-05"
 #   )
 # ----
+
+
+# > env.custom$f_path.size_files(path = "data", literal_filename = "rds")
+# c("ATC_RxNorm_NDC.rds", "fhs.index100.rds", "fhs.index100ge11le20.rds", "fhs.index100le10.rds", "Holdings_VT_URTH_VWO.list.rds", "icd10cm2019.rds", "ICD9_3char_sub_chapter.tibble.rds", "icd9_chapters.expand_range.ICD9_3char.rds", "icd9_chapters.expand_range.rds", "icd9_sub_chapters.expand_range.ICD9_3char.rds", "icd9_sub_chapters.expand_range.rds", "jinseob2kim_jinseob2kim.github.io_master_fstexample.rds", "jinseob2kim_lecture-snuhlab_master_data_example_bnc.rds", "jinseob2kim_lecture-snuhlab_master_data_example_g1e.rds", "RDataMining-Tweets-20160212.rds")
+# ----
+# tibble::tribble(
+#                                                   ~filename,    ~size,       ~bytes,         ~KB,        ~MB,        ~GB,
+#                                        "ATC_RxNorm_NDC.rds", 79768376, "79,768,376", "77,898.80", "76.07305", "7.43e-02",
+#                                          "fhs.index100.rds",   342010, "   342,010", "   333.99", " 0.32617", "3.19e-04",
+#                                  "fhs.index100ge11le20.rds",    35515, "    35,515", "    34.68", " 0.03387", "3.31e-05",
+#                                      "fhs.index100le10.rds",    35747, "    35,747", "    34.91", " 0.03409", "3.33e-05",
+#                             "Holdings_VT_URTH_VWO.list.rds",   387992, "   387,992", "   378.90", " 0.37002", "3.61e-04",
+#                                           "icd10cm2019.rds",  1432851, " 1,432,851", " 1,399.27", " 1.36647", "1.33e-03",
+#                         "ICD9_3char_sub_chapter.tibble.rds",     8459, "     8,459", "     8.26", " 0.00807", "7.88e-06",
+#                 "icd9_chapters.expand_range.ICD9_3char.rds",     2919, "     2,919", "     2.85", " 0.00278", "2.72e-06",
+#                            "icd9_chapters.expand_range.rds",    42833, "    42,833", "    41.83", " 0.04085", "3.99e-05",
+#             "icd9_sub_chapters.expand_range.ICD9_3char.rds",     5776, "     5,776", "     5.64", " 0.00551", "5.38e-06",
+#                        "icd9_sub_chapters.expand_range.rds",    46898, "    46,898", "    45.80", " 0.04473", "4.37e-05",
+#   "jinseob2kim_jinseob2kim.github.io_master_fstexample.rds",    86895, "    86,895", "    84.86", " 0.08287", "8.09e-05",
+#   "jinseob2kim_lecture-snuhlab_master_data_example_bnc.rds",    24732, "    24,732", "    24.15", " 0.02359", "2.30e-05",
+#   "jinseob2kim_lecture-snuhlab_master_data_example_g1e.rds",    51743, "    51,743", "    50.53", " 0.04935", "4.82e-05",
+#                           "RDataMining-Tweets-20160212.rds",   282733, "   282,733", "   276.11", " 0.26964", "2.63e-04"
+#   )
+# ----
+# Warning message:
+# In grep(literal_filename, ., ignore.case = T, value = T, fixed = T) :
+#   argument 'ignore.case = TRUE' will be ignored
+
 
 
 #@ end -----
