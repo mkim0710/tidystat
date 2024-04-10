1f07b3679f606b8a5685ec9d4a5ba7b090fb46be
mkim0710 <mkim0710@gmail.com>
2024-04-11 01:49:05 +0900

    f_path.list_subpath.source.r

--- f_path.df_dirs_recursive.df_files.source.r
+++ f_path.df_dirs_recursive.df_files.source.r
@@ -2,7 +2,11 @@
 #         https://github.com/mkim0710/tidystat/blob/master/f_path.df_dirs_recursive.df_files.dev.r
 # source("https://github.com/mkim0710/tidystat/raw/master/f_path.df_dirs_recursive.df_files.source.r")
 
+# gitignore_escaped_select.UC.regex from gitignore.Rmd
+# f_vec.extract_non_alphanumeric_list.dev.r
 
+
+library(tidyverse)
 # if(!exists("env.custom", envir = .GlobalEnv)) assign("env.custom", new.env(), envir = .GlobalEnv)
 if(!exists("env.custom", envir = .GlobalEnv)) assign("env.custom", new.env(), envir = .GlobalEnv)
 # env.custom = env.custom %>% as.environment
@@ -10,21 +14,23 @@ if(!exists("env.custom", envir = .GlobalEnv)) assign("env.custom", new.env(), en
 if(!"env.internal" %in% names(env.custom)) eval(parse(text = "env.custom$env.internal = new.env()"), envir = .GlobalEnv)
 
 
-cat("Loading: ", "env.custom$gitignore_escaped_select.UC", "\n")
-env.custom$gitignore_escaped_select.UC = c(
-    "\\.RPROJ\\.USER", "\\.RHISTORY", "\\.RDATA", "\\.RUSERDATA",
-    ".*\\.ZIP", ".*\\.7Z", ".*\\.RPROJ", "\\.GITIGNORE",
-    "MH\\).*\\.R", ".*\\.LNK", ".*ALIAS", ".*\\- VOID.*\\..*", ".*\\-VOID.*\\..*",
-    ".*\\- INFO\\..*", ".*\\-INFO\\..*", ".*\\-DEV.*\\..*", ".*DEBUG.*\\..*",
-    "\\-TMP", ".*\\- TMP.*\\..*", ".*\\-TMP.*\\..*", ".*\\-LOG.*\\..*",
-    ".*\\.RTF", "[\\/\\\\]\\.[^\\/\\\\]"
-  )
-cat("Loading: ", "env.custom$f_path.df_dirs_recursive.df_files", "\n")
-env.custom$f_path.df_dirs_recursive.df_files <- function(
-  input_path = ".", 
-  max_depth = 9, 
-  current_depth = 0, 
-  gitignore_escaped_select.UC = env.custom$gitignore_escaped_select.UC
+objectname = "gitignore_escaped_select.UC.regex"
+object = "^(\\.RPROJ\\.USER)|(\\.RHISTORY)|(\\.RDATA)|(\\.RUSERDATA)|(.*\\.ZIP)|(.*\\.7Z)|(.*\\.R\\.GZ)|(.*\\.R\\.XZ)|(.*\\.RMD\\.GZ)|(.*\\.RMD\\.XZ)|(.*\\.RPROJ)|(\\.GITIGNORE)|(\\.GIT)|(\\.GITHUB)|(MH\\).*\\.R)|(.*\\.LNK)|(.*ALIAS)|(.*\\- VOID.*\\..*)|(.*\\-VOID.*\\..*)|(.*\\- INFO\\..*)|(.*\\-INFO\\..*)|(.*\\-DEV.*\\..*)|(.*DEBUG.*\\..*)|(\\-TMP)|(.*\\- TMP.*\\..*)|(.*\\-TMP.*\\..*)|(.*\\- OLD\\..*)|(.*\\-OLD\\..*)|(.*\\-LOG.*\\..*)|(.*\\.RTF)$"    
+if(!objectname %in% names(env.custom)) {
+    cat("Loading: ", "env.custom$", objectname, "\n", sep = ""); 
+    env.custom[[objectname]] = object
+    # cat("> env.custom$", objectname, "()\n", sep = ""); tmp.function = env.custom[[objectname]]; tmp.function()  # Run the loaded function by default
+}
+
+
+
+
+objectname = "f_path.df_dirs_recursive.df_files"
+object = function(
+        input_path = ".", 
+        max_depth = 9, 
+        current_depth = 0, 
+        gitignore_escaped_select.UC.regex = env.custom$gitignore_escaped_select.UC.regex
 ) {
     if (!dir.exists(input_path)) {
         stop("The specified input_path does not exist or is not a directory.")
@@ -36,12 +42,13 @@ env.custom$f_path.df_dirs_recursive.df_files <- function(
     }
     
     if (!exists("f_path.df_dirs_recursive.df_files", envir = .GlobalEnv)) assign("f_path.df_dirs_recursive.df_files", env.custom$f_path.df_dirs_recursive.df_files, envir = .GlobalEnv)
-
-    dirs <- list.dirs(input_path, full.names = TRUE, recursive = FALSE) %>%
-        str_subset(paste0(gitignore_escaped_select.UC, collapse = "|") %>% regex(ignore_case = TRUE), negate = TRUE)
+    
+    dirs = list.dirs(input_path, full.names = TRUE, recursive = FALSE) %>% 
+        str_subset(paste0("^", input_path, .Platform$file.sep, "\\.") %>% regex(ignore_case = TRUE), negate = TRUE) %>%  # exclude basename starting with dot
+        str_subset(gitignore_escaped_select.UC.regex %>% regex(ignore_case = TRUE), negate = TRUE)
     
     df_dirs_recursive0 <- tibble(path.level = integer(), full_path = character())
-
+    
     if (current_depth == 0) {
         df_dirs_recursive0 <- bind_rows(df_dirs_recursive0, tibble(path.level = 0L, full_path = input_path))
     }
@@ -50,7 +57,7 @@ env.custom$f_path.df_dirs_recursive.df_files <- function(
         df_dirs_recursive0 <- bind_rows(df_dirs_recursive0, tibble(path.level = current_depth + 1, full_path = dir))
         
         # Recursively process subdirectories
-        df_dirs_recursive0 <- bind_rows(df_dirs_recursive0, f_path.df_dirs_recursive.df_files(dir, max_depth, current_depth + 1, gitignore_escaped_select.UC))
+        df_dirs_recursive0 <- bind_rows(df_dirs_recursive0, f_path.df_dirs_recursive.df_files(dir, max_depth, current_depth + 1, gitignore_escaped_select.UC.regex))
     }
     
     if (current_depth == 0) {
@@ -79,11 +86,12 @@ env.custom$f_path.df_dirs_recursive.df_files <- function(
             as.tibble
         
         df_dirs_recursive.ls_files1 = df_dirs_recursive2 %>% mutate(
-            files = full_path %>% map(function(chr) {list.files(chr, include.dirs = FALSE) %>% str_subset(paste0(gitignore_escaped_select.UC, collapse = "|") %>% regex(ignore_case = TRUE), negate = TRUE)}) 
+            files = full_path %>% map(function(chr) {list.files(chr, include.dirs = FALSE) %>% str_subset(paste0(gitignore_escaped_select.UC.regex, collapse = "|") %>% regex(ignore_case = TRUE), negate = TRUE)}) 
             , files.r = full_path %>% map(function(chr) {list.files(chr, "\\.r$", ignore.case = T, include.dirs = FALSE)}) 
             , files.rmd = full_path %>% map(function(chr) {list.files(chr, "\\.rmd$", ignore.case = T, include.dirs = FALSE)}) 
-            , files.rds = full_path %>% map(function(chr) {list.files(chr, "\\.rds(.xz)?$", ignore.case = T, include.dirs = FALSE)}) 
-            , files.rda = full_path %>% map(function(chr) {list.files(chr, "\\.rda(ta)?(.xz)?$", ignore.case = T, include.dirs = FALSE)}) 
+            # , files.rds = full_path %>% map(function(chr) {list.files(chr, "\\.rds(.xz)?$", ignore.case = T, include.dirs = FALSE)}) 
+            # , files.rda = full_path %>% map(function(chr) {list.files(chr, "\\.rda(ta)?(.xz)?$", ignore.case = T, include.dirs = FALSE)}) 
+            , files.data = full_path %>% map(function(chr) {list.files(chr, "\\.(rdata|rda|rds|csv|sas7bdat)(\\.[gx]z)?$", ignore.case = T, include.dirs = FALSE)}) 
         )
         
         df_dirs_recursive.ls_files2 = df_dirs_recursive.ls_files1 %>% 
@@ -124,31 +132,43 @@ env.custom$f_path.df_dirs_recursive.df_files <- function(
                             ~ifelse(length(.y)>0, {rep("\t", .x-0) %>% paste(collapse = "") %>% paste0(.y) %>% c("") %>% paste(collapse = "\n")}, "")
                         ) 
                     )
-                , print_tree_path_files.rds = 
+                # , print_tree_path_files.rds = 
+                #     paste0(
+                #         "@", 
+                #         print_tree_path,
+                #         "/\n",
+                #         map2_chr(
+                #             path.level, 
+                #             files.rds, 
+                #             # ~paste0(paste(c("|->", rep("\t", .x-0)), collapse = ""), .y) %>% paste(collapse = "\n") 
+                #             ~ifelse(length(.y)>0, {rep("\t", .x-0) %>% paste(collapse = "") %>% paste0(.y) %>% c("") %>% paste(collapse = "\n")}, "")
+                #         ) 
+                #     )
+                # , print_tree_path_files.rda = 
+                #     paste0(
+                #         "@", 
+                #         print_tree_path,
+                #         "/\n",
+                #         map2_chr(
+                #             path.level, 
+                #             files.rda, 
+                #             # ~paste0(paste(c("|->", rep("\t", .x-0)), collapse = ""), .y) %>% paste(collapse = "\n") 
+                #             ~ifelse(length(.y)>0, {rep("\t", .x-0) %>% paste(collapse = "") %>% paste0(.y) %>% c("") %>% paste(collapse = "\n")}, "")
+                #         ) 
+                #     )
+                , print_tree_path_files.data =
                     paste0(
-                        "@", 
+                        "@",
                         print_tree_path,
                         "/\n",
                         map2_chr(
-                            path.level, 
-                            files.rds, 
-                            # ~paste0(paste(c("|->", rep("\t", .x-0)), collapse = ""), .y) %>% paste(collapse = "\n") 
+                            path.level,
+                            files.data,
+                            # ~paste0(paste(c("|->", rep("\t", .x-0)), collapse = ""), .y) %>% paste(collapse = "\n")
                             ~ifelse(length(.y)>0, {rep("\t", .x-0) %>% paste(collapse = "") %>% paste0(.y) %>% c("") %>% paste(collapse = "\n")}, "")
-                        ) 
+                        )
                     )
-                , print_tree_path_files.rda = 
-                    paste0(
-                        "@", 
-                        print_tree_path,
-                        "/\n",
-                        map2_chr(
-                            path.level, 
-                            files.rda, 
-                            # ~paste0(paste(c("|->", rep("\t", .x-0)), collapse = ""), .y) %>% paste(collapse = "\n") 
-                            ~ifelse(length(.y)>0, {rep("\t", .x-0) %>% paste(collapse = "") %>% paste0(.y) %>% c("") %>% paste(collapse = "\n")}, "")
-                        ) 
-                    )
-                ) %>%
+            ) %>%
             as.tibble
         df_out = df_dirs_recursive.ls_files2
     } else {
@@ -156,7 +176,34 @@ env.custom$f_path.df_dirs_recursive.df_files <- function(
     }
     return(df_out)
 }
+if(!objectname %in% names(env.custom)) {
+    cat("Loading: ", "env.custom$", objectname, "\n", sep = ""); 
+    env.custom[[objectname]] = object
+    # cat("> env.custom$", objectname, "()\n", sep = ""); get(objectname, envir = env.custom)() # Run the loaded function by default
+}
 
+# > cat("> env.custom$", objectname, "()\n", sep = ""); tmp.function = env.custom[[objectname]]; tmp.function()  # Run the loaded function by default
+# > env.custom$f_path.df_dirs_recursive.df_files()
+# .Platform$file.sep:  / 
+# input_path0:  . 
+# Finished iterating through paths.
+# # A tibble: 61 × 15
+#    path.level path       path.basename path.parent full_path normalized_path print_tree_path files files.r files.rmd files.data print_tree_path_files print_tree_path_file…¹
+#         <dbl> <chr>      <chr>         <chr>       <chr>     <chr>           <chr>           <lis> <list>  <list>    <list>     <chr>                 <chr>                 
+#  1          0 ""         .             .           .         D:/OneDrive/[]… "."             <chr> <chr>   <chr [1]> <chr [1]>  "@./\n-info\nbfg-1.1… "@./\n-void.r\nenv.cu…
+#  2          1 "-info"    -info         .           ./-info   D:/OneDrive/[]… "-info"         <chr> <chr>   <chr [0]> <chr [0]>  "@-info/\n\tFACETS_d… "@-info/\n"           
+#  3          1 "Rdev"     Rdev          .           ./Rdev    D:/OneDrive/[]… "Rdev"          <chr> <chr>   <chr [0]> <chr [0]>  "@Rdev/\n\t-dev\n\t0… "@Rdev/\n\tdata.varia…
+#  4          2 "Rdev/-de… -dev          ./Rdev      ./Rdev/-… D:/OneDrive/[]… "\t-dev"        <chr> <chr>   <chr [0]> <chr [0]>  "@\t-dev/\n"          "@\t-dev/\n\t\tobject…
+#  5          2 "Rdev/00_… 00_base_prog… ./Rdev      ./Rdev/0… D:/OneDrive/[]… "\t00_base_pro… <chr> <chr>   <chr [0]> <chr [0]>  "@\t00_base_program/… "@\t00_base_program/\…
+#  6          2 "Rdev/00_… 00_protocol   ./Rdev      ./Rdev/0… D:/OneDrive/[]… "\t00_protocol" <chr> <chr>   <chr [0]> <chr [0]>  "@\t00_protocol/\n\t… "@\t00_protocol/\n"   
+#  7          3 "Rdev/00_… 01_sample_si… ./Rdev/00_… ./Rdev/0… D:/OneDrive/[]… "\t\t01_sample… <chr> <chr>   <chr [0]> <chr [0]>  "@\t\t01_sample_size… "@\t\t01_sample_size/…
+#  8          3 "Rdev/00_… 05_count_eli… ./Rdev/00_… ./Rdev/0… D:/OneDrive/[]… "\t\t05_count_… <chr> <chr>   <chr [0]> <chr [0]>  "@\t\t05_count_eligi… "@\t\t05_count_eligib…
+#  9          3 "Rdev/00_… 06_count_exp… ./Rdev/00_… ./Rdev/0… D:/OneDrive/[]… "\t\t06_count_… <chr> <chr>   <chr [0]> <chr [0]>  "@\t\t06_count_expos… "@\t\t06_count_exposu…
+# 10          3 "Rdev/00_… 07_count_out… ./Rdev/00_… ./Rdev/0… D:/OneDrive/[]… "\t\t07_count_… <chr> <chr>   <chr [0]> <chr [0]>  "@\t\t07_count_outco… "@\t\t07_count_outcom…
+# # ℹ 51 more rows
+# # ℹ abbreviated name: ¹​print_tree_path_files.r
+# # ℹ 2 more variables: print_tree_path_files.rmd <chr>, print_tree_path_files.data <chr>
+# # ℹ Use `print(n = ...)` to see more rows
 
 
 
@@ -165,8 +212,8 @@ env.custom$f_path.df_dirs_recursive.df_files <- function(
 # ls.str(env.custom$env.internal) #-----
 # # > ls.str(env.custom) #-----
 # # env.internal : <environment: 0x000001f6d74c17b0> 
-# # f_path.df_dirs_recursive.df_files : function (input_path = ".", max_depth = 9, current_depth = 0, gitignore_escaped_select.UC = env.custom$gitignore_escaped_select.UC)  
-# # gitignore_escaped_select.UC :  chr [1:23] "\\.RPROJ\\.USER" "\\.RHISTORY" "\\.RDATA" "\\.RUSERDATA" ".*\\.ZIP" ".*\\.7Z" ".*\\.RPROJ" "\\.GITIGNORE" ...
+# # f_path.df_dirs_recursive.df_files : function (input_path = ".", max_depth = 9, current_depth = 0, gitignore_escaped_select.UC.regex = env.custom$gitignore_escaped_select.UC.regex)  
+# # gitignore_escaped_select.UC.regex :  chr [1:23] "\\.RPROJ\\.USER" "\\.RHISTORY" "\\.RDATA" "\\.RUSERDATA" ".*\\.ZIP" ".*\\.7Z" ".*\\.RPROJ" "\\.GITIGNORE" ...
 # # > ls.str(env.custom$env.internal) #-----
 
 #@ end ----
---
+++ f_path.list_subpath.source.r
@@ -0,0 +1,324 @@
+# f_path.list_subpath.source.r
+# f_path.list_subpath.dev.r
+#         https://github.com/mkim0710/tidystat/blob/master/f_path.list_subpath.dev.r
+# source("https://github.com/mkim0710/tidystat/raw/master/f_path.list_subpath.source.r")
+
+library(tidyverse)
+
+# if(!exists("env.custom", envir = .GlobalEnv)) assign("env.custom", new.env(), envir = .GlobalEnv)
+if(!exists("env.custom", envir = .GlobalEnv)) assign("env.custom", new.env(), envir = .GlobalEnv)
+# env.custom = as.environment(env.custom)
+# if(!exists("env.internal", envir = env.custom)) eval(parse(text = "env.custom$env.internal = new.env()"), envir = .GlobalEnv)
+if(!"env.internal" %in% names(env.custom)) eval(parse(text = "env.custom$env.internal = new.env()"), envir = .GlobalEnv)
+
+if(!"path" %in% names(env.custom)) {
+    env.custom$path = list()
+    if(!"source_base_local" %in% names(env.custom$path)) {
+        env.custom$path$source_base_local = "D:/OneDrive/[][Rproject]/github_tidystat"
+        cat("env.custom$path$source_base_local: ", env.custom$path$source_base_local, "\n")
+    }
+    if(!"source_base_github" %in% names(env.custom$path)) {
+        env.custom$path$source_base_github = "https://github.com/mkim0710/tidystat/raw/master"
+        cat("env.custom$path$source_base_github: ", env.custom$path$source_base_github, "\n")
+    }
+}
+# #@ for (env.custom.dependancy in c("")) { -----
+# for (env.custom.dependancy in c("f_df.tribble_construct")) {
+#     if(!env.custom.dependancy %in% names(env.custom)) {
+#         cat(paste0("sys.nframe() = ", sys.nframe(), "\n"))
+#         objectname = env.custom.dependancy
+#         source(file.path(file.path(env.custom$path$source_base_local, ""), paste0(objectname, ".source.r")))
+#     }
+# }
+
+
+
+#@ objectname = "f_path.list_subpath.DepthFirstSearch" =========
+objectname = "f_path.list_subpath.DepthFirstSearch"
+object = function(input_path = ".", max_depth = 3, include_input_path = TRUE, print.intermediate = FALSE, .initial_max_depth = NA) {
+    # Initialize .initial_max_depth with max_depth on the first call
+    if (is.na(.initial_max_depth)) {
+        .initial_max_depth <- max_depth
+    }
+    
+    if (!file.exists(input_path) || !file.info(input_path)$isdir) {
+        stop("The specified input_path does not exist or is not a directory.")
+    }
+    
+    list_subpath <- list()
+    
+    if (max_depth == 0) {
+        if (include_input_path) {
+            list_subpath <- c(list_subpath, input_path)
+        }
+        return(unlist(list_subpath, use.names = FALSE))
+    }
+    
+    vec_files_subpath.excl_origin <- list.files(input_path, full.names = TRUE)
+    for (i_files_subpath in vec_files_subpath.excl_origin) {
+        if (file.info(i_files_subpath)$isdir) {
+            subvec_files_subpath.excl_origin <- env.custom$f_path.list_subpath.DepthFirstSearch(i_files_subpath, max_depth - 1, FALSE, print.intermediate, .initial_max_depth = .initial_max_depth)
+            
+            if (print.intermediate && length(subvec_files_subpath.excl_origin) > 0) {
+                cat("Subdirectories of ", i_files_subpath, ": ", length(subvec_files_subpath.excl_origin), "\n")
+            }
+            
+            list_subpath <- c(list_subpath, i_files_subpath, subvec_files_subpath.excl_origin)
+        }
+    }
+    
+    if (include_input_path) {
+        list_subpath <- c(input_path, list_subpath)
+    }
+    
+    vec_subpath <- unlist(list_subpath, use.names = FALSE)
+    
+    # Print the structure of the final list of subpaths if at the top level
+    if (print.intermediate && max_depth == .initial_max_depth) {
+        cat("Structure of the final list of subpaths at the top level:\n")
+        str(vec_subpath)
+    }
+    
+    return(vec_subpath)
+}
+if(!objectname %in% names(env.custom)) {
+    cat("Loading: ", "env.custom$", objectname, "\n", sep = ""); 
+    env.custom[[objectname]] = object
+    # cat("> env.custom$", objectname, "()\n", sep = ""); get(objectname, envir = env.custom)() # Run the loaded function by default
+}
+
+
+
+
+
+#@ objectname = "f_path.list_subpath.BreathFirstSearch" =========
+objectname = "f_path.list_subpath.BreathFirstSearch"
+object = function(input_path = ".", max_depth = 3, include_input_path = TRUE, print.intermediate = FALSE) {
+    if (!file.exists(input_path) || !file.info(input_path)$isdir) {
+        stop("The specified input_path does not exist or is not a directory.")
+    }
+    
+    # Initialize the queue with the input_path at depth 0
+    list_list_path_depth <- list(list(path = input_path, depth = 0))
+    list_subpath <- list()
+    
+    # Process the queue
+    while (length(list_list_path_depth) > 0) {
+        # Dequeue the first element
+        list_path_depth.current <- list_list_path_depth[[1]]
+        list_list_path_depth <- list_list_path_depth[-1]  # Remove the first element
+        
+        if (print.intermediate) {
+            cat("Processing: ", list_path_depth.current$path, " at depth ", list_path_depth.current$depth, "\n")
+            cat("Queue length: ", length(list_list_path_depth), "\n")
+        }
+        
+        # Add the current path to list_subpath if it's within the max_depth
+        if (list_path_depth.current$depth <= max_depth) {
+            if (list_path_depth.current$depth >= 0 || include_input_path) {
+                list_subpath <- c(list_subpath, list(list_path_depth.current$path))
+            }
+            
+            # List subdirectories and enqueue them with incremented depth
+            vec_files_subpath <- list.files(list_path_depth.current$path, full.names = TRUE)
+            for (i_files_subpath in vec_files_subpath) {
+                if (file.info(i_files_subpath)$isdir) {
+                    # Enqueue subdirectories with incremented depth
+                    list_list_path_depth <- c(list_list_path_depth, list(list(path = i_files_subpath, depth = list_path_depth.current$depth + 1)))
+                }
+            }
+        }
+    }
+
+    # Optionally remove the input_path from the result
+    if (!include_input_path) {
+        list_subpath <- list_subpath[-1]
+    }
+    
+    # Flatten the list_subpath to make it a character vector
+    vec_subpath <- unlist(list_subpath, use.names = FALSE)
+    
+    # Print the final vector of subpaths if requested
+    if (print.intermediate) {
+        cat("Structure of the final list of subpaths:\n")
+        print(str(vec_subpath))
+    }
+        
+    return(vec_subpath)
+}
+if(!objectname %in% names(env.custom)) {
+    cat("Loading: ", "env.custom$", objectname, "\n", sep = ""); 
+    env.custom[[objectname]] = object
+    # cat("> env.custom$", objectname, "()\n", sep = ""); get(objectname, envir = env.custom)() # Run the loaded function by default
+}
+
+
+
+
+#@ objectname = "f_path.list_subpath" =========
+objectname = "f_path.list_subpath"
+object = function(input_path = ".", max_depth = 3, include_input_path = TRUE, print.intermediate = FALSE, BreadthFirstSearch = FALSE) {
+    if (BreadthFirstSearch) {
+        env.custom$f_path.list_subpath.BreathFirstSearch(input_path, max_depth, include_input_path, print.intermediate)
+    } else {
+        env.custom$f_path.list_subpath.DepthFirstSearch(input_path, max_depth, include_input_path, print.intermediate)
+    }
+}
+if(!objectname %in% names(env.custom)) {
+    cat("Loading: ", "env.custom$", objectname, "\n", sep = ""); 
+    env.custom[[objectname]] = object
+    # cat("> env.custom$", objectname, "()\n", sep = ""); get(objectname, envir = env.custom)() # Run the loaded function by default
+}
+
+
+
+
+
+#@ end -----
+
+# > f_path.list_subpath(print.intermediate = T) %>% str
+# Subdirectories of  ./data :  1 
+# Subdirectories of  ./other :  8 
+# Subdirectories of  ./Rdev/00_protocol :  4 
+# Subdirectories of  ./Rdev/10_import_clean_datatype :  15 
+# Subdirectories of  ./Rdev/20_tidy_group_by_match :  3 
+# Subdirectories of  ./Rdev/40_visualize_explore_bivariate_stratified :  4 
+# Subdirectories of  ./Rdev/50_model_formula_evaluation :  7 
+# Subdirectories of  ./Rdev :  42 
+# Structure of the final list of subpaths at the top level:
+#  chr [1:62] "." "./-info" "./-tmp" "./data" "./data/ATC_RxNorm_NDC" "./examples" "./other" "./other/Bash" "./other/Batch" "./other/C" "./other/Excel" "./other/ODBC" ...
+#  chr [1:62] "." "./-info" "./-tmp" "./data" "./data/ATC_RxNorm_NDC" "./examples" "./other" "./other/Bash" "./other/Batch" "./other/C" "./other/Excel" "./other/ODBC" ...
+# > f_path.list_subpath(print.intermediate = T, BreadthFirstSearch = T) %>% str
+# Processing:  .  at depth  0 
+# Queue length:  0 
+# Processing:  ./-info  at depth  1 
+# Queue length:  9 
+# Processing:  ./-tmp  at depth  1 
+# Queue length:  8 
+# Processing:  ./data  at depth  1 
+# Queue length:  7 
+# Processing:  ./examples  at depth  1 
+# Queue length:  7 
+# Processing:  ./other  at depth  1 
+# Queue length:  6 
+# Processing:  ./Rdev  at depth  1 
+# Queue length:  13 
+# Processing:  ./Rmd  at depth  1 
+# Queue length:  21 
+# Processing:  ./Rplot  at depth  1 
+# Queue length:  20 
+# Processing:  ./tests  at depth  1 
+# Queue length:  19 
+# Processing:  ./vignettes  at depth  1 
+# Queue length:  18 
+# Processing:  ./data/ATC_RxNorm_NDC  at depth  2 
+# Queue length:  17 
+# Processing:  ./other/Bash  at depth  2 
+# Queue length:  16 
+# Processing:  ./other/Batch  at depth  2 
+# Queue length:  15 
+# Processing:  ./other/C  at depth  2 
+# Queue length:  14 
+# Processing:  ./other/Excel  at depth  2 
+# Queue length:  13 
+# Processing:  ./other/ODBC  at depth  2 
+# Queue length:  12 
+# Processing:  ./other/SAS  at depth  2 
+# Queue length:  11 
+# Processing:  ./other/Stata  at depth  2 
+# Queue length:  10 
+# Processing:  ./other/VBA  at depth  2 
+# Queue length:  9 
+# Processing:  ./Rdev/-dev  at depth  2 
+# Queue length:  8 
+# Processing:  ./Rdev/00_base_program  at depth  2 
+# Queue length:  7 
+# Processing:  ./Rdev/00_protocol  at depth  2 
+# Queue length:  6 
+# Processing:  ./Rdev/10_import_clean_datatype  at depth  2 
+# Queue length:  9 
+# Processing:  ./Rdev/20_tidy_group_by_match  at depth  2 
+# Queue length:  23 
+# Processing:  ./Rdev/30_transform_scale_categorical  at depth  2 
+# Queue length:  25 
+# Processing:  ./Rdev/40_visualize_explore_bivariate_stratified  at depth  2 
+# Queue length:  24 
+# Processing:  ./Rdev/50_model_formula_evaluation  at depth  2 
+# Queue length:  27 
+# Processing:  ./Rdev/60_communicate_report_export  at depth  2 
+# Queue length:  33 
+# Processing:  ./Rdev/00_protocol/01_sample_size  at depth  3 
+# Queue length:  32 
+# Processing:  ./Rdev/00_protocol/05_count_eligible  at depth  3 
+# Queue length:  31 
+# Processing:  ./Rdev/00_protocol/06_count_exposure  at depth  3 
+# Queue length:  30 
+# Processing:  ./Rdev/00_protocol/07_count_outcome  at depth  3 
+# Queue length:  29 
+# Processing:  ./Rdev/10_import_clean_datatype/11_metadata  at depth  3 
+# Queue length:  28 
+# Processing:  ./Rdev/10_import_clean_datatype/12_import_files  at depth  3 
+# Queue length:  27 
+# Processing:  ./Rdev/10_import_clean_datatype/12_import_sqlite  at depth  3 
+# Queue length:  26 
+# Processing:  ./Rdev/10_import_clean_datatype/12_import_vocabulary  at depth  3 
+# Queue length:  25 
+# Processing:  ./Rdev/10_import_clean_datatype/13_duplicated  at depth  3 
+# Queue length:  24 
+# Processing:  ./Rdev/10_import_clean_datatype/13_missing_value  at depth  3 
+# Queue length:  23 
+# Processing:  ./Rdev/10_import_clean_datatype/13_split_fold  at depth  3 
+# Queue length:  22 
+# Processing:  ./Rdev/10_import_clean_datatype/15_cleaning_text  at depth  3 
+# Queue length:  21 
+# Processing:  ./Rdev/10_import_clean_datatype/15_cleaning_time  at depth  3 
+# Queue length:  20 
+# Processing:  ./Rdev/10_import_clean_datatype/16_categorical_factor  at depth  3 
+# Queue length:  19 
+# Processing:  ./Rdev/10_import_clean_datatype/17_categorical_indicators  at depth  3 
+# Queue length:  18 
+# Processing:  ./Rdev/10_import_clean_datatype/18_dichotomous_logical  at depth  3 
+# Queue length:  17 
+# Processing:  ./Rdev/10_import_clean_datatype/19_datetime  at depth  3 
+# Queue length:  16 
+# Processing:  ./Rdev/10_import_clean_datatype/19_numeric_integer  at depth  3 
+# Queue length:  15 
+# Processing:  ./Rdev/10_import_clean_datatype/array_list  at depth  3 
+# Queue length:  14 
+# Processing:  ./Rdev/20_tidy_group_by_match/23_group_by_PersonID  at depth  3 
+# Queue length:  13 
+# Processing:  ./Rdev/20_tidy_group_by_match/25_study_population  at depth  3 
+# Queue length:  12 
+# Processing:  ./Rdev/20_tidy_group_by_match/27_match  at depth  3 
+# Queue length:  11 
+# Processing:  ./Rdev/40_visualize_explore_bivariate_stratified/43_network  at depth  3 
+# Queue length:  10 
+# Processing:  ./Rdev/40_visualize_explore_bivariate_stratified/44_map  at depth  3 
+# Queue length:  9 
+# Processing:  ./Rdev/40_visualize_explore_bivariate_stratified/45_bivariate_measures  at depth  3 
+# Queue length:  8 
+# Processing:  ./Rdev/40_visualize_explore_bivariate_stratified/47_bivariate_partial_stratified  at depth  3 
+# Queue length:  7 
+# Processing:  ./Rdev/50_model_formula_evaluation/51_model_formula  at depth  3 
+# Queue length:  6 
+# Processing:  ./Rdev/50_model_formula_evaluation/53_model_selection  at depth  3 
+# Queue length:  5 
+# Processing:  ./Rdev/50_model_formula_evaluation/55_model_weighted  at depth  3 
+# Queue length:  4 
+# Processing:  ./Rdev/50_model_formula_evaluation/56_model_bootstrap  at depth  3 
+# Queue length:  3 
+# Processing:  ./Rdev/50_model_formula_evaluation/57_model_time2event  at depth  3 
+# Queue length:  2 
+# Processing:  ./Rdev/50_model_formula_evaluation/57_model_trajectory  at depth  3 
+# Queue length:  1 
+# Processing:  ./Rdev/50_model_formula_evaluation/59_model_evaluation  at depth  3 
+# Queue length:  0 
+# Structure of the final list of subpaths:
+#  chr [1:62] "." "./-info" "./-tmp" "./data" "./examples" "./other" "./Rdev" "./Rmd" "./Rplot" "./tests" "./vignettes" "./data/ATC_RxNorm_NDC" "./other/Bash" ...
+# NULL
+#  chr [1:62] "." "./-info" "./-tmp" "./data" "./examples" "./other" "./Rdev" "./Rmd" "./Rplot" "./tests" "./vignettes" "./data/ATC_RxNorm_NDC" "./other/Bash" ...
+# > f_path.list_subpath(getwd()) %>% str
+#  chr [1:62] "D:/OneDrive/[][Rproject]/github_tidystat" "D:/OneDrive/[][Rproject]/github_tidystat/-info" "D:/OneDrive/[][Rproject]/github_tidystat/-tmp" ...
+
+
+
