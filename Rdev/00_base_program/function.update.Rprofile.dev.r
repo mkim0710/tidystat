cc12d9ebbf2677418d939516b72d24eab93665f4
mkim0710 <mkim0710@gmail.com>
2024-05-01 22:22:35 +0900

    Update and rename Rprofile.r to function.update.Rprofile.dev.r
    Former-commit-id: 9071f7a7f1b31d8b80396994cf08962c70c52c68

--- Rdev/00_base_program/function.update.Rprofile.dev.r
+++ Rdev/00_base_program/function.update.Rprofile.dev.r
@@ -1,4 +1,60 @@
+# Define the path to the .Rprofile file in the user's home directory
+r_profile_path <- file.path(Sys.getenv("HOME"), ".Rprofile")
 
+# Function to update or add language setting
+update_language_setting <- function(file_path, setting) {
+  # Read the current contents of the file
+  lines <- readLines(file_path, warn = FALSE)
+  
+  # Pattern to find existing language setting
+  pattern <- "^Sys.setenv\\(LANG ="
+  
+  # Check if the language setting line exists
+  lang_line_index <- grep(pattern, lines)
+  
+  # If it exists, update the line
+  if (length(lang_line_index) > 0) {
+    lines[lang_line_index] <- setting
+  } else {
+    # If not, append the setting
+    lines <- c(lines, setting)
+  }
+  
+  # Write the updated or new lines back to the file
+  writeLines(lines, file_path)
+}
+
+# Define the language setting line
+language_setting <- "Sys.setenv(LANG = \"en\")  # Set default language to English"
+
+# Check if the .Rprofile file exists
+if (!file.exists(r_profile_path)) {
+  # Create the file if it does not exist
+  file.create(r_profile_path)
+}
+
+# Update or add the language setting
+update_language_setting(r_profile_path, language_setting)
+
+# Optional: Open the file in the default text editor for manual inspection
+file.edit(r_profile_path)
+
+
+
+
+
+
+
+
+
+
+
+
+# ################################################################################
+# --------------------------------------------------------------------------------
+# ================================================================================
+# --------------------------------------------------------------------------------
+# ################################################################################
 
 # https://community.rstudio.com/t/how-to-change-rstudio-display-language-into-english/46615
 Sys.getlocale()
@@ -46,8 +102,11 @@ Sys.setlocale("LC_ALL", "korean") #----
 
 
 
-
-#@ --------------
+# ################################################################################
+# --------------------------------------------------------------------------------
+# ================================================================================
+# --------------------------------------------------------------------------------
+# ################################################################################
 # https://stackoverflow.com/questions/31707941/how-do-i-change-the-default-library-path-for-r-packages
 # See help(Startup) and help(.libPaths) as you have several possibilities where this may have gotten set. Among them are
 # setting R_LIBS_USER
