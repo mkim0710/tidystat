172fe6ef0684f004b730af99e03453422c69482c
mkim0710 <mkim0710@gmail.com>
2024-05-02 00:41:24 +0900

    Update function.update.Rprofile.dev.r
    Former-commit-id: a112a13d38aeba7bbf3f34b78dfe608c31f5560b

--- Rdev/00_base_program/function.update.Rprofile.dev.r
+++ Rdev/00_base_program/function.update.Rprofile.dev.r
@@ -6,6 +6,61 @@
 # --------------------------------------------------------------------------------
 # ################################################################################
 
+# Function to search and modify or create .Rprofile for setting locale
+function2find = "Sys.setlocale"
+function2rewrite = "Sys.setlocale('LC_ALL', 'en_US.UTF-8')"
+
+# Paths to search for .Rprofile
+paths4Rprofile <- c("./.Rprofile", "~/.Rprofile")
+
+# Initialize variable
+path4Rprofile <- NULL
+
+# Check if the .Rprofile file exists
+for (path in paths4Rprofile) {
+  if (file.exists(path)) {
+    cat(paste0("Found the file: ", path, "\n"))
+    path4Rprofile <- path
+    break
+  }
+}
+
+# Process the .Rprofile file if found
+if (!is.null(path4Rprofile)) {
+  # Read the contents of the .Rprofile file
+  profile_content <- readLines(path4Rprofile)
+  
+  # Check if the language setting already exists in the file
+  if (any(grepl(function2find, profile_content))) {
+    cat("Language setting already exists in .Rprofile. Updating...\n")
+    profile_content <- gsub(paste0(function2find, ".*?$"), "", profile_content, perl = TRUE)
+  } else {
+    cat("Adding language setting to .Rprofile...\n")
+  }
+  # Append the new setting
+  profile_content <- c(profile_content, function2rewrite)
+  
+  # Write the updated contents back to the .Rprofile file
+  writeLines(profile_content, path4Rprofile)
+} else {
+  # Prompt for creating a new .Rprofile if none exists
+  createRprofile <- tolower(readline(prompt = "Do you want to create a new Rprofile file? (yes/no): "))
+  
+  # Create .Rprofile if user confirms
+  if (createRprofile == "yes" || createRprofile == "y") {
+    cat("Creating new .Rprofile with language setting...\n")
+    writeLines(function2rewrite, "~/.Rprofile")
+    cat("Created -- ~/.Rprofile -- with: ", function2rewrite, ".\n")
+  } else {
+    cat("Operation aborted. No .Rprofile file created.\n")
+  }
+}
+
+# ################################################################################
+# --------------------------------------------------------------------------------
+# ================================================================================
+# --------------------------------------------------------------------------------
+# ################################################################################
 
 # Define the path to the .Rprofile file in the user's home directory
 r_profile_path <- file.path(Sys.getenv("HOME"), ".Rprofile")
@@ -14,13 +69,10 @@ r_profile_path <- file.path(Sys.getenv("HOME"), ".Rprofile")
 update_language_setting <- function(file_path, setting) {
   # Read the current contents of the file
   lines <- readLines(file_path, warn = FALSE)
-  
   # Pattern to find existing language setting
   pattern <- "^Sys.setenv\\(LANG ="
-  
   # Check if the language setting line exists
   lang_line_index <- grep(pattern, lines)
-  
   # If it exists, update the line
   if (length(lang_line_index) > 0) {
     lines[lang_line_index] <- setting
@@ -28,7 +80,6 @@ update_language_setting <- function(file_path, setting) {
     # If not, append the setting
     lines <- c(lines, setting)
   }
-  
   # Write the updated or new lines back to the file
   writeLines(lines, file_path)
 }
