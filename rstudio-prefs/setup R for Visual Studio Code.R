22991f6f561c374fc1556f4fdd96e32544a78a8d
mkim0710 <mkim0710@gmail.com>
2024-05-19 14:10:09 +0900

    )) {if(!require(
    Former-commit-id: c37e6e4e15d15afb424951a565f6202c6fe84ffb

--- rstudio-prefs/setup R for Visual Studio Code.R
+++ rstudio-prefs/setup R for Visual Studio Code.R
@@ -1,8 +1,8 @@
 # https://code.visualstudio.com/docs/languages/r
-for(packagename in c("languageserver")){if(!require(packagename,character.only=TRUE))install.packages(packagename) else library(packagename,character.only=TRUE)}
+for(packagename in c("languageserver")) {if(!require(packagename,character.only=TRUE))install.packages(packagename) else library(packagename,character.only=TRUE)}
 
 # https://renkun.me/2020/06/16/using-httpgd-in-vscode-a-web-based-svg-graphics-device/
-for(packagename in c("httpgd")){if(!require(packagename,character.only=TRUE))install.packages(packagename) else library(packagename,character.only=TRUE)}
+for(packagename in c("httpgd")) {if(!require(packagename,character.only=TRUE))install.packages(packagename) else library(packagename,character.only=TRUE)}
 
 
 
--- rstudio-prefs/templates/Untitled1.source.r
+++ rstudio-prefs/templates/Untitled1.source.r
@@ -17,7 +17,7 @@ Sys.setlocale("LC_ALL", "en_US.utf8")  # Note that setting category "LC_ALL" set
 # Sys.setlocale("LC_MESSAGES", "en_US.utf8")  # Note that the LANGUAGE environment variable has precedence over "LC_MESSAGES" in selecting the language for message translation on most R platforms.  # LC_MESSAGES does not exist in Windows
 Sys.setenv(LANGUAGE="en")  # Note that the LANGUAGE environment variable has precedence over "LC_MESSAGES" in selecting the language for message translation on most R platforms.
 #|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
-for(packagename in c("tidyverse")){if(!require(packagename,character.only=TRUE))install.packages(packagename) else library(packagename,character.only=TRUE)}
+for(packagename in c("tidyverse")) {if(!require(packagename,character.only=TRUE))install.packages(packagename) else library(packagename,character.only=TRUE)}
 if(!exists("env.custom", envir=.GlobalEnv))
     assign("env.custom", new.env(), envir=.GlobalEnv)
 ## env.custom\$path ====
