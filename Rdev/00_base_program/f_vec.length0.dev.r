187fb0c3c7ce29f1d8bdbff4a0c0943a124afbdf
mkim0710 <mkim0710@gmail.com>
2024-06-03 14:20:38 +0900

    Create f_vec.length0.dev.r

---
+++ Rdev/00_base_program/f_vec.length0.dev.r
@@ -0,0 +1,37 @@
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# Caution) character(0), numeric(0): vector of length 0
+character(NULL)
+character(NA)
+character(0)
+character(1)
+character(2)
+# > character(NULL)
+# Error in character(NULL) : invalid 'length' argument
+# > character(NA)
+# Error in character(NA) : invalid 'length' argument
+# > character(0)
+# character(0)
+# > character(1)
+# [1] ""
+# > character(2)
+# [1] "" ""
+numeric(NULL)
+numeric(NA)
+numeric(0)
+numeric(1)
+numeric(2)
+# > numeric(NULL)
+# Error in numeric(NULL) : invalid 'length' argument
+# > numeric(NA)
+# Error in numeric(NA) : invalid 'length' argument
+# > numeric(0)
+# numeric(0)
+# > numeric(1)
+# [1] 0
+# > numeric(2)
+# [1] 0 0
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
