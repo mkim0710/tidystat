53e1c6829d115d9640a19c6bcb58a5e3590858ea
mkim0710 <mkim0710@gmail.com>
2024-06-03 12:39:11 +0900

    Create f_vec.dput_line_by_line.dev.r
    Former-commit-id: ec5d195e2be08bb2061cf2250d30ba3f3c86b190

---
+++ Rdev/00_base_program/f_vec.dput_line_by_line.dev.r
@@ -0,0 +1,34 @@
+
+f_vec.dput_line_by_line <- function(vec) {
+    vec |> paste0(collapse = '",\n  "') %>% paste0('c("',.,'")') |> cat()
+}
+c("C059", "D050000L", "NimbusMonoPS", "NimbusRoman", "NimbusSans", 
+"NimbusSansNarrow", "P052", "Roboto", "RobotoCondensed", "StandardSymbolsPS", 
+"texgyreadventor", "texgyrebonum", "texgyrechorus", "texgyrecursor", 
+"texgyredejavu", "texgyreheros", "texgyreheroscn", "texgyrepagella", 
+"texgyreschola", "texgyretermes", "URWBookman", "URWGothic", 
+"Z003") |> dput_line_by_line()
+# > fc_list_family.font_file_names.supertypes() |> dput_line_by_line()
+# c("C059",
+#   "D050000L",
+#   "NimbusMonoPS",
+#   "NimbusRoman",
+#   "NimbusSans",
+#   "NimbusSansNarrow",
+#   "P052",
+#   "Roboto",
+#   "RobotoCondensed",
+#   "StandardSymbolsPS",
+#   "texgyreadventor",
+#   "texgyrebonum",
+#   "texgyrechorus",
+#   "texgyrecursor",
+#   "texgyredejavu",
+#   "texgyreheros",
+#   "texgyreheroscn",
+#   "texgyrepagella",
+#   "texgyreschola",
+#   "texgyretermes",
+#   "URWBookman",
+#   "URWGothic",
+#   "Z003")
