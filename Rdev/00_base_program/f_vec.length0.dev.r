e299b5687833b0773c514cde72931fa086f0f63b
mkim0710 <mkim0710@gmail.com>
2024-06-03 14:24:01 +0900

    Update f_vec.length0.dev.r

--- Rdev/00_base_program/f_vec.length0.dev.r
+++ Rdev/00_base_program/f_vec.length0.dev.r
@@ -2,21 +2,34 @@
 #|________________________________________________________________________________|#  
 #|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
 # Caution) character(0), numeric(0): vector of length 0
-character(NULL)
-character(NA)
-character(0)
-character(1)
-character(2)
-# > character(NULL)
-# Error in character(NULL) : invalid 'length' argument
-# > character(NA)
-# Error in character(NA) : invalid 'length' argument
-# > character(0)
+?vector
+vector(mode = "logical", length = 0)
+vector(mode = "numeric", length = 0)
+vector(mode = "character", length = 0)
+# > vector(mode = "logical", length = 0)
+# logical(0)
+# > vector(mode = "numeric", length = 0)
+# numeric(0)
+# > vector(mode = "character", length = 0)
 # character(0)
-# > character(1)
-# [1] ""
-# > character(2)
-# [1] "" ""
+
+?logical
+logical(NULL)
+logical(NA)
+logical(0)
+logical(1)
+logical(2)
+# > logical(NULL)
+# Error in logical(NULL) : invalid 'length' argument
+# > logical(NA)
+# Error in logical(NA) : invalid 'length' argument
+# > logical(0)
+# logical(0)
+# > logical(1)
+# [1] FALSE
+# > logical(2)
+# [1] FALSE FALSE
+?numeric
 numeric(NULL)
 numeric(NA)
 numeric(0)
@@ -32,6 +45,21 @@ numeric(2)
 # [1] 0
 # > numeric(2)
 # [1] 0 0
+?character
+character(NULL)
+character(NA)
+character(0)
+character(1)
+character(2)
+# > character(NULL)
+# Error in character(NULL) : invalid 'length' argument
+# > character(NA)
+# Error in character(NA) : invalid 'length' argument
+# > character(0)
+# character(0)
+# > character(1)
+# [1] ""
+# > character(2)
+# [1] "" ""
 #|________________________________________________________________________________|#  
 #|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
-
