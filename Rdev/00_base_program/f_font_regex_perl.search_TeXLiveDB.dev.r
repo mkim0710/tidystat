7475fcc34bedb4511bf7768b85736c0cf8162441
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-06-04 04:16:14 +0000

    f_vec1_vec2.setdiff
    Former-commit-id: 61dd84373841e0bd00188089cbf52403b5ec2893

---
+++ Rdev/00_base_program/f_font_regex_perl.search_TeXLiveDB.dev.r
@@ -0,0 +1,129 @@
+# f_font_regex_perl.search_TeXLiveDB.dev.r
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+# check fonts and install - tinytex@Rocker.r
+# https://chatgpt.com/c/8d563256-b9b9-4685-b048-343944290455
+# https://chatgpt.com/c/798e2ff5-96bf-481c-8d2d-77129a2812d7
+# https://chatgpt.com/c/2926bf45-de5a-44e6-9a5c-3515412462ef
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+library(tidyverse)
+if (!requireNamespace("tinytex", quietly = TRUE)) {
+  install.packages("tinytex")
+}
+library(tinytex)
+# https://stackoverflow.com/questions/51468447/is-there-r-code-for-moving-the-focus-to-the-rstudio-console
+# https://docs.posit.co/ide/server-pro/rstudio_ide_commands/rstudio_ide_commands.html
+rstudioapi::executeCommand("activateConsole"); tinytex::install_tinytex(); rstudioapi::executeCommand("activateSource")
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# @@ START) fonts to install ====
+##$ vec_font_regex_perl =====
+# c("Roboto Condensed", "Roboto Serif", "Cascadia Code Semibold", "NanumMyeongjo", "NanumGothic", "D2Coding")
+vec_font_regex_perl = c("Roboto Condensed" = "(?i)roboto.*condensed",
+  "Roboto Serif" = "(?i)roboto.*serif",
+  "Cascadia Code Semibold" = "(?i)cascadia.*code.*semibold",
+  "NanumMyeongjo" = "(?i)nanum.*myeongjo",
+  "NanumGothic" = "(?i)nanum.*gothic",
+  "D2Coding" = "(?i)d2coding")
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|# 
+### % f_font_regex_perl.search_TeXLiveDB ====
+f_font_regex_perl.search_TeXLiveDB <- function(font_regex_perl) {
+    if(length(font_regex_perl) > 1) error("font_regex_perl must be a single string.")
+    vec_path.file <- tinytex::tlmgr_search(what = font_regex_perl, file = TRUE, all = FALSE, global = TRUE, stdout = TRUE)
+    if (length(vec_path.file) > 0) {
+        return(vec_path.file)
+    } else {
+        return(NULL)
+    }
+}
+
+vec_font_regex_perl.TexLiveDB_list = vec_font_regex_perl %>% map(f_font_regex_perl.search_TeXLiveDB)
+vec_font_regex_perl.TexLiveDB_list %>% str
+# > vec_font_regex_perl.TexLiveDB_list %>% str
+# List of 6
+#  $ Roboto Condensed      : chr [1:1845] "tlmgr: package repository https://mirror.navercorp.com/CTAN/systems/texlive/tlnet (not verified: gpg unavailable)" "roboto:" "\ttexmf-dist/fonts/tfm/google/roboto/RobotoSerifCondensed-BoldItalic-lf-ts1--base.tfm" "\ttexmf-dist/fonts/vf/google/roboto/RobotoSerifCondensed-Italic-tosf-lgr.vf" ...
+#  $ Roboto Serif          : chr [1:2475] "tlmgr: package repository https://mirror.navercorp.com/CTAN/systems/texlive/tlnet (not verified: gpg unavailable)" "roboto:" "\ttexmf-dist/fonts/type1/google/roboto/RobotoSerif-ExtraBoldItalic.pfb" "\ttexmf-dist/fonts/vf/google/roboto/RobotoSerif-LightItalic-lf-ts1.vf" ...
+#  $ Cascadia Code Semibold: chr [1:52] "tlmgr: package repository https://mirror.navercorp.com/CTAN/systems/texlive/tlnet (not verified: gpg unavailable)" "cascadia-code:" "\ttexmf-dist/fonts/tfm/public/cascadia-code/CascadiaCode-SemiBold-sup-ot1.tfm" "\ttexmf-dist/fonts/tfm/public/cascadia-code/CascadiaCode-SemiBold-tlf-t1.tfm" ...
+#  $ NanumMyeongjo         : chr "tlmgr: package repository https://mirror.navercorp.com/CTAN/systems/texlive/tlnet (not verified: gpg unavailable)"
+#  $ NanumGothic           : chr "tlmgr: package repository https://mirror.navercorp.com/CTAN/systems/texlive/tlnet (not verified: gpg unavailable)"
+#  $ D2Coding              : chr "tlmgr: package repository https://mirror.navercorp.com/CTAN/systems/texlive/tlnet (not verified: gpg unavailable)"
+
+
+
+# Function to check fonts and print their paths if installed
+f_vec_font_filename.search_path <- function(vec_font_filename) {
+    if(is.null(names(vec_font_filename))) vec_font_filename = vec_font_filename %>% set_names()
+    names(vec_font_filename)[names(vec_font_filename)==""] = vec_font_filename[names(vec_font_filename)==""]
+    for (font_name in names(vec_font_filename)) {
+        vec_font_path <- f_font_filename.search_path(vec_font_filename[font_name])
+        if (!is.null(vec_font_path)) {
+            message(font_name, " is already installed at the following path(s):\n", paste(vec_font_path, collapse = "\n"))
+        } else {
+            message(font_name, " is not installed.")
+        }
+    }
+}
+
+
+"noname" |> f_vec_font_filename.search_path() |> dput()
+"inconsolata" |> f_vec_font_filename.search_path() |> dput()
+# > "noname" |> f_vec_font_filename.search_path() |> dput()
+# tlmgr search --all 'noname'
+# noname is not installed.
+# NULL
+# > "inconsolata" |> f_vec_font_filename.search_path() |> dput()
+# tlmgr search --all 'inconsolata'
+# inconsolata is already installed at the following path(s):
+# 	texmf-dist/fonts/enc/dvips/inconsolata
+# 	texmf-dist/fonts/map/dvips/inconsolata
+# 	texmf-dist/fonts/opentype/public/inconsolata
+# 	texmf-dist/fonts/tfm/public/inconsolata
+# 	texmf-dist/fonts/type1/public/inconsolata
+# 	texmf-dist/tex/context/fonts/mkiv
+# 	texmf-dist/tex/latex/inconsolata
+# .
+# NULL
+
+
+
+c(
+  "fontspec",
+  "xunicode",
+  "xltxtra",
+  "cm-unicode",
+  "unicode-math",
+  "roboto",
+  "cascadia-code",
+  "nanum-type1",
+  "nanum-myeongjo",
+  "nanum-gothic",
+  "d2coding"
+)
+
+
+
+
+
+
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#@@ END -----
+cat("* To revert to the last commited file, run the following terminal command:\n", 
+    '"git checkout -- ',rstudioapi::getSourceEditorContext()$path,'" |> system(intern=TRUE)',"\n", sep="")
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
