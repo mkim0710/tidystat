51303eb5caeee174e4739584b4a2fc79cdebbce6
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-06-04 07:52:01 +0000

    list_vec |> f_list.format_line_by_line(space_between_vec_elements = "")

--- Rdev/00_base_program/f_font_regex_perl.search_TeXLiveDB.dev.r
+++ Rdev/00_base_program/f_font_regex_perl.search_TeXLiveDB.dev.r
@@ -20,7 +20,68 @@ library(tinytex)
 rstudioapi::executeCommand("activateConsole"); tinytex::install_tinytex(); rstudioapi::executeCommand("activateSource")
 #|________________________________________________________________________________|#  
 #|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
-# @@ START) fonts to install ====
+#% |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE) -----
+"(?i)inconsolata.*regular" |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE)
+"(?i)inconsolata.*regular" |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE, stdout = TRUE) |> dput()
+"(?i)inconsolata.*regular" |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE, stdout = TRUE) |> paste0(collapse="\n") |> cat()
+# > "(?i)inconsolata.*regular" |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE)
+# tlmgr search --file '(?i)inconsolata.*regular'
+# inconsolata:
+# 	texmf-dist/fonts/opentype/public/inconsolata/Inconsolatazi4-Regular.otf
+# 	texmf-dist/fonts/opentype/public/inconsolata/InconsolataN-Regular.otf
+# 	texmf-dist/fonts/type1/public/inconsolata/InconsolataN-Regular.pfb
+# > "(?i)inconsolata.*regular" |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE, stdout = TRUE) |> dput()
+# tlmgr search --file '(?i)inconsolata.*regular'
+# c("inconsolata:", "\ttexmf-dist/fonts/opentype/public/inconsolata/InconsolataN-Regular.otf", 
+# "\ttexmf-dist/fonts/opentype/public/inconsolata/Inconsolatazi4-Regular.otf", 
+# "\ttexmf-dist/fonts/type1/public/inconsolata/InconsolataN-Regular.pfb"
+# )
+# > "(?i)inconsolata.*regular" |> tinytex::tlmgr_search(file = TRUE, all = FALSE, global = FALSE, stdout = TRUE) |> paste0(collapse="\n") |> cat()
+# tlmgr search --file '(?i)inconsolata.*regular'
+# inconsolata:
+# 	texmf-dist/fonts/type1/public/inconsolata/InconsolataN-Regular.pfb
+# 	texmf-dist/fonts/opentype/public/inconsolata/Inconsolatazi4-Regular.otf
+# 	texmf-dist/fonts/opentype/public/inconsolata/InconsolataN-Regular.otf
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# @@ START) font_filename ====
+### % f_vec_font_filename.search_path ====
+f_vec_font_filename.search_path <- function(vec_font_filename) {
+    if(is.null(names(vec_font_filename))) vec_font_filename = vec_font_filename %>% set_names()
+    names(vec_font_filename)[names(vec_font_filename)==""] = vec_font_filename[names(vec_font_filename)==""]
+    for (font_name in names(vec_font_filename)) {
+        vec_font_path <- f_font_filename.search_path(vec_font_filename[font_name])
+        if (!is.null(vec_font_path)) {
+            message(font_name, " is already installed at the following path(s):\n", paste(vec_font_path, collapse = "\n"))
+        } else {
+            message(font_name, " is not installed.")
+        }
+    }
+}
+
+"noname" |> f_vec_font_filename.search_path() |> dput()
+"inconsolata" |> f_vec_font_filename.search_path() |> dput()
+# > "noname" |> f_vec_font_filename.search_path() |> dput()
+# tlmgr search --all 'noname'
+# noname is not installed.
+# NULL
+# > "inconsolata" |> f_vec_font_filename.search_path() |> dput()
+# tlmgr search --all 'inconsolata'
+# inconsolata is already installed at the following path(s):
+# 	texmf-dist/fonts/enc/dvips/inconsolata
+# 	texmf-dist/fonts/map/dvips/inconsolata
+# 	texmf-dist/fonts/opentype/public/inconsolata
+# 	texmf-dist/fonts/tfm/public/inconsolata
+# 	texmf-dist/fonts/type1/public/inconsolata
+# 	texmf-dist/tex/context/fonts/mkiv
+# 	texmf-dist/tex/latex/inconsolata
+# .
+# NULL
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# @@ START) font_regex_perl  ====
 ##$ vec_font_regex_perl =====
 # c("Roboto Condensed", "Roboto Serif", "Cascadia Code Semibold", "NanumMyeongjo", "NanumGothic", "D2Coding")
 vec_font_regex_perl = c("Roboto Condensed" = "(?i)roboto.*condensed",
@@ -29,6 +90,57 @@ vec_font_regex_perl = c("Roboto Condensed" = "(?i)roboto.*condensed",
   "NanumMyeongjo" = "(?i)nanum.*myeongjo",
   "NanumGothic" = "(?i)nanum.*gothic",
   "D2Coding" = "(?i)d2coding")
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|# 
+### % f_font_regex_perl.search_path_local ====
+f_font_regex_perl.search_path_local <- function(font_regex_perl) {
+    if(length(font_regex_perl) > 1) error("font_regex_perl must be a single string.")
+    vec_path.file <- tinytex::tlmgr_search(what = font_regex_perl, file = FALSE, all = TRUE, global = FALSE, stdout = TRUE)
+    vec_path = vec_path.file %>% dirname() %>% unique() %>% sort()
+    if (length(vec_path) > 0) {
+        return(vec_path)
+    } else {
+        return(NULL)
+    }
+}
+
+"inconsolata" |> f_font_regex_perl.search_path_local() |> paste0(collapse="\n") |> cat()
+# > "inconsolata" |> f_font_regex_perl.search_path_local() |> paste0(collapse="\n") |> cat()
+# tlmgr search --all 'inconsolata'
+# 	texmf-dist/fonts/enc/dvips/inconsolata
+# 	texmf-dist/fonts/map/dvips/inconsolata
+# 	texmf-dist/fonts/opentype/public/inconsolata
+# 	texmf-dist/fonts/tfm/public/inconsolata
+# 	texmf-dist/fonts/type1/public/inconsolata
+# 	texmf-dist/tex/context/fonts/mkiv
+# 	texmf-dist/tex/latex/inconsolata
+"inconsolata" |> f_font_regex_perl.search_path_local() |> env1$f_vec.dput_line_by_line()
+c("	texmf-dist/fonts/enc/dvips/inconsolata",
+  "	texmf-dist/fonts/map/dvips/inconsolata",
+  "	texmf-dist/fonts/opentype/public/inconsolata",
+  "	texmf-dist/fonts/tfm/public/inconsolata",
+  "	texmf-dist/fonts/type1/public/inconsolata",
+  "	texmf-dist/tex/context/fonts/mkiv",
+  "	texmf-dist/tex/latex/inconsolata",
+  ".")
+
+c("\ttexmf-dist/fonts/enc/dvips/inconsolata", "\ttexmf-dist/fonts/map/dvips/inconsolata",
+"\ttexmf-dist/fonts/opentype/public/inconsolata", "\ttexmf-dist/fonts/tfm/public/inconsolata",
+"\ttexmf-dist/fonts/type1/public/inconsolata", "\ttexmf-dist/tex/latex/inconsolata",
+".") %>% env1$f_vec.dput_line_by_line()
+
+if(.Platform$OS.type == "windows") browseURL(paste0(Sys.getenv("APPDATA"),"/TinyTeX/texmf-dist/fonts"))
+
+
+vec_font_regex_perl = c("Roboto Condensed" = "(?i)roboto.*condensed",
+  "Roboto Serif" = "(?i)roboto.*serif",
+  "Cascadia Code Semibold" = "(?i)cascadia.*code.*semibold",
+  "NanumMyeongjo" = "(?i)nanum.*myeongjo",
+  "NanumGothic" = "(?i)nanum.*gothic",
+  "D2Coding" = "(?i)d2coding")
+vec_font_regex_perl |> map(f_font_regex_perl.search_path_local) |> dput()
 
 #|________________________________________________________________________________|#  
 #|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|# 
@@ -56,39 +168,9 @@ vec_font_regex_perl.TexLiveDB_list %>% str
 
 
 
-# Function to check fonts and print their paths if installed
-f_vec_font_filename.search_path <- function(vec_font_filename) {
-    if(is.null(names(vec_font_filename))) vec_font_filename = vec_font_filename %>% set_names()
-    names(vec_font_filename)[names(vec_font_filename)==""] = vec_font_filename[names(vec_font_filename)==""]
-    for (font_name in names(vec_font_filename)) {
-        vec_font_path <- f_font_filename.search_path(vec_font_filename[font_name])
-        if (!is.null(vec_font_path)) {
-            message(font_name, " is already installed at the following path(s):\n", paste(vec_font_path, collapse = "\n"))
-        } else {
-            message(font_name, " is not installed.")
-        }
-    }
-}
 
 
-"noname" |> f_vec_font_filename.search_path() |> dput()
-"inconsolata" |> f_vec_font_filename.search_path() |> dput()
-# > "noname" |> f_vec_font_filename.search_path() |> dput()
-# tlmgr search --all 'noname'
-# noname is not installed.
-# NULL
-# > "inconsolata" |> f_vec_font_filename.search_path() |> dput()
-# tlmgr search --all 'inconsolata'
-# inconsolata is already installed at the following path(s):
-# 	texmf-dist/fonts/enc/dvips/inconsolata
-# 	texmf-dist/fonts/map/dvips/inconsolata
-# 	texmf-dist/fonts/opentype/public/inconsolata
-# 	texmf-dist/fonts/tfm/public/inconsolata
-# 	texmf-dist/fonts/type1/public/inconsolata
-# 	texmf-dist/tex/context/fonts/mkiv
-# 	texmf-dist/tex/latex/inconsolata
-# .
-# NULL
+
 
 
 
