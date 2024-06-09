5ae255627ee3289dc1eb85627f0a82573b8699d2
mkim0710 <mkim0710@gmail.com>
2024-06-09 14:12:22 +0900

    pre-commit hooks

--- git/hooks/install-git-hooks.sh
+++ git/hooks/install-git-hooks.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cp git/hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+# In Windows, you don't need to explicitly set executable permissions on the hook script because Git for Windows will execute the script as long as it has the correct shebang (#!/bin/sh) and is located in the correct directory (.git/hooks).
+
