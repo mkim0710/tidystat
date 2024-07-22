ec6acd756789901a0becdcb14973f9ef9d506dae
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-07-22 06:08:47 +0000

    emptyenv() -> base -> unknown (<environment: 0x55e96efc8b58>) -> package:methods -> package:datasets -> package:utils -> package:grDevices -> package:graphics -> package:stats -> unknown (<environment: 0x55e970e13a70>) -> package:tidyverse -> package:ggplot2 -> package:tibble -> package:tidyr -> package:readr -> package:purrr -> package:dplyr -> package:stringr -> package:forcats -> package:lubridate -> R_GlobalEnv -> f_global_env -> f_internal_env
    Former-commit-id: 336b11784a8cdad1d390a5850a46dded9314789f

--- Rdev/00_base_program/print_environment_hierarchy.dev.r
+++ Rdev/00_base_program/print_environment_hierarchy.dev.r
@@ -227,6 +227,212 @@ f_global()
 # Current environment path:
 #  emptyenv() -> <environment: base> -> [1] "Autoloads" -> attr(,"name") -> <environment: 0x55e96efc8b58> -> [1] "/usr/local/lib/R/library/methods" -> attr(,"path") -> [1] "package:methods" -> attr(,"name") -> <environment: package:methods> -> [1] "/usr/local/lib/R/library/datasets" -> attr(,"path") -> [1] "package:datasets" -> attr(,"name") -> <environment: package:datasets> -> [1] "/usr/local/lib/R/library/utils" -> attr(,"path") -> [1] "package:utils" -> attr(,"name") -> <environment: package:utils> -> [1] "/usr/local/lib/R/library/grDevices" -> attr(,"path") -> [1] "package:grDevices" -> attr(,"name") -> <environment: package:grDevices> -> [1] "/usr/local/lib/R/library/graphics" -> attr(,"path") -> [1] "package:graphics" -> attr(,"name") -> <environment: package:graphics> -> [1] "/usr/local/lib/R/library/stats" -> attr(,"path") -> [1] "package:stats" -> attr(,"name") -> <environment: package:stats> -> [1] "tools:rstudio" -> attr(,"name") -> <environment: 0x55e970e13a70> -> [1] "/usr/local/lib/R/site-library/tidyverse" -> attr(,"path") -> [1] "package:tidyverse" -> attr(,"name") -> <environment: package:tidyverse> -> [1] "/usr/local/lib/R/site-library/ggplot2" -> attr(,"path") -> [1] "package:ggplot2" -> attr(,"name") -> <environment: package:ggplot2> -> [1] "/usr/local/lib/R/site-library/tibble" -> attr(,"path") -> [1] "package:tibble" -> attr(,"name") -> <environment: package:tibble> -> [1] "/usr/local/lib/R/site-library/tidyr" -> attr(,"path") -> [1] "package:tidyr" -> attr(,"name") -> <environment: package:tidyr> -> [1] "/usr/local/lib/R/site-library/readr" -> attr(,"path") -> [1] "package:readr" -> attr(,"name") -> <environment: package:readr> -> [1] "/usr/local/lib/R/site-library/purrr" -> attr(,"path") -> [1] "package:purrr" -> attr(,"name") -> <environment: package:purrr> -> [1] "/usr/local/lib/R/site-library/dplyr" -> attr(,"path") -> [1] "package:dplyr" -> attr(,"name") -> <environment: package:dplyr> -> [1] "/usr/local/lib/R/site-library/stringr" -> attr(,"path") -> [1] "package:stringr" -> attr(,"name") -> <environment: package:stringr> -> [1] "/usr/local/lib/R/site-library/forcats" -> attr(,"path") -> [1] "package:forcats" -> attr(,"name") -> <environment: package:forcats> -> [1] "/usr/local/lib/R/site-library/lubridate" -> attr(,"path") -> [1] "package:lubridate" -> attr(,"name") -> <environment: package:lubridate> -> <environment: R_GlobalEnv> -> f_global_env -> f_internal_env
 
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# Function to print the environment hierarchy
+# Function to print the environment hierarchy
+print_environment_hierarchy <- function(env) {
+  env_names <- c()
+  while (!identical(env, emptyenv())) {
+    if (exists(".__name__", envir = env, inherits = FALSE)) {
+      env_name <- get(".__name__", envir = env)
+    } else if (identical(env, globalenv())) {
+      env_name <- "R_GlobalEnv"
+    } else if (identical(env, baseenv())) {
+      env_name <- "base"
+    } else {
+      env_name <- paste0("<environment: ", capture.output(print(env)), ">")
+    }
+    env_names <- c(env_names, env_name)
+    env <- parent.env(env)
+  }
+  env_names <- c(env_names, "emptyenv()")
+  env_names <- rev(env_names)
+  cat("Current environment path:\n", paste(env_names, collapse = " -> "), "\n")
+}
+
+# Example of nested functions with manually named environments
+f_global <- function() {
+  assign(".__name__", "f_global_env", envir = environment())
+  
+  f_internal <- function() {
+    assign(".__name__", "f_internal_env", envir = environment())
+    print("Debug: Current Environment Path")
+    print_environment_hierarchy(environment())
+  }
+  
+  f_internal()
+}
+
+# Calling the global function
+f_global()
+# [1] "Debug: Current Environment Path"
+# Current environment path:
+#  emptyenv() -> base -> <environment: [1] "Autoloads"> -> <environment: attr(,"name")> -> <environment: <environment: 0x55e96efc8b58>> -> <environment: [1] "/usr/local/lib/R/library/methods"> -> <environment: attr(,"path")> -> <environment: [1] "package:methods"> -> <environment: attr(,"name")> -> <environment: <environment: package:methods>> -> <environment: [1] "/usr/local/lib/R/library/datasets"> -> <environment: attr(,"path")> -> <environment: [1] "package:datasets"> -> <environment: attr(,"name")> -> <environment: <environment: package:datasets>> -> <environment: [1] "/usr/local/lib/R/library/utils"> -> <environment: attr(,"path")> -> <environment: [1] "package:utils"> -> <environment: attr(,"name")> -> <environment: <environment: package:utils>> -> <environment: [1] "/usr/local/lib/R/library/grDevices"> -> <environment: attr(,"path")> -> <environment: [1] "package:grDevices"> -> <environment: attr(,"name")> -> <environment: <environment: package:grDevices>> -> <environment: [1] "/usr/local/lib/R/library/graphics"> -> <environment: attr(,"path")> -> <environment: [1] "package:graphics"> -> <environment: attr(,"name")> -> <environment: <environment: package:graphics>> -> <environment: [1] "/usr/local/lib/R/library/stats"> -> <environment: attr(,"path")> -> <environment: [1] "package:stats"> -> <environment: attr(,"name")> -> <environment: <environment: package:stats>> -> <environment: [1] "tools:rstudio"> -> <environment: attr(,"name")> -> <environment: <environment: 0x55e970e13a70>> -> <environment: [1] "/usr/local/lib/R/site-library/tidyverse"> -> <environment: attr(,"path")> -> <environment: [1] "package:tidyverse"> -> <environment: attr(,"name")> -> <environment: <environment: package:tidyverse>> -> <environment: [1] "/usr/local/lib/R/site-library/ggplot2"> -> <environment: attr(,"path")> -> <environment: [1] "package:ggplot2"> -> <environment: attr(,"name")> -> <environment: <environment: package:ggplot2>> -> <environment: [1] "/usr/local/lib/R/site-library/tibble"> -> <environment: attr(,"path")> -> <environment: [1] "package:tibble"> -> <environment: attr(,"name")> -> <environment: <environment: package:tibble>> -> <environment: [1] "/usr/local/lib/R/site-library/tidyr"> -> <environment: attr(,"path")> -> <environment: [1] "package:tidyr"> -> <environment: attr(,"name")> -> <environment: <environment: package:tidyr>> -> <environment: [1] "/usr/local/lib/R/site-library/readr"> -> <environment: attr(,"path")> -> <environment: [1] "package:readr"> -> <environment: attr(,"name")> -> <environment: <environment: package:readr>> -> <environment: [1] "/usr/local/lib/R/site-library/purrr"> -> <environment: attr(,"path")> -> <environment: [1] "package:purrr"> -> <environment: attr(,"name")> -> <environment: <environment: package:purrr>> -> <environment: [1] "/usr/local/lib/R/site-library/dplyr"> -> <environment: attr(,"path")> -> <environment: [1] "package:dplyr"> -> <environment: attr(,"name")> -> <environment: <environment: package:dplyr>> -> <environment: [1] "/usr/local/lib/R/site-library/stringr"> -> <environment: attr(,"path")> -> <environment: [1] "package:stringr"> -> <environment: attr(,"name")> -> <environment: <environment: package:stringr>> -> <environment: [1] "/usr/local/lib/R/site-library/forcats"> -> <environment: attr(,"path")> -> <environment: [1] "package:forcats"> -> <environment: attr(,"name")> -> <environment: <environment: package:forcats>> -> <environment: [1] "/usr/local/lib/R/site-library/lubridate"> -> <environment: attr(,"path")> -> <environment: [1] "package:lubridate"> -> <environment: attr(,"name")> -> <environment: <environment: package:lubridate>> -> R_GlobalEnv -> f_global_env -> f_internal_env 
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# Function to print the environment hierarchy
+print_environment_hierarchy <- function(env) {
+  env_names <- c()
+  while (!identical(env, emptyenv())) {
+    if (exists(".__name__", envir = env, inherits = FALSE)) {
+      env_name <- get(".__name__", envir = env)
+    } else if (identical(env, globalenv())) {
+      env_name <- "R_GlobalEnv"
+    } else if (identical(env, baseenv())) {
+      env_name <- "base"
+    } else if (identical(env, emptyenv())) {
+      env_name <- "emptyenv()"
+    } else {
+      env_name <- "unknown"
+    }
+    env_names <- c(env_names, env_name)
+    env <- parent.env(env)
+  }
+  env_names <- c(env_names, "emptyenv()")
+  env_names <- rev(env_names)
+  cat("Current environment path:\n", paste(env_names, collapse = " -> "), "\n")
+}
+
+# Example of nested functions with manually named environments
+f_global <- function() {
+  assign(".__name__", "f_global_env", envir = environment())
+  
+  f_internal <- function() {
+    assign(".__name__", "f_internal_env", envir = environment())
+    print("Debug: Current Environment Path")
+    print_environment_hierarchy(environment())
+  }
+  
+  f_internal()
+}
+
+# Calling the global function
+f_global()
+# > f_global()
+# [1] "Debug: Current Environment Path"
+# Current environment path:
+#  emptyenv() -> base -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> unknown -> R_GlobalEnv -> f_global_env -> f_internal_env 
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# Function to print the environment hierarchy
+print_environment_hierarchy <- function(env) {
+  env_names <- c()
+  while (!identical(env, emptyenv())) {
+    if (exists(".__name__", envir = env, inherits = FALSE)) {
+      env_name <- get(".__name__", envir = env)
+    } else if (identical(env, globalenv())) {
+      env_name <- "R_GlobalEnv"
+    } else if (identical(env, baseenv())) {
+      env_name <- "base"
+    } else if (!is.null(attr(env, "name")) && grepl("^package:", attr(env, "name"))) {
+      env_name <- attr(env, "name")
+    } else {
+      env_name <- "unknown"
+    }
+    env_names <- c(env_names, env_name)
+    env <- parent.env(env)
+  }
+  env_names <- c(env_names, "emptyenv()")
+  env_names <- rev(env_names)
+  cat("Current environment path:\n", paste(env_names, collapse = " -> "), "\n")
+}
+
+# Example of nested functions with manually named environments
+f_global <- function() {
+  assign(".__name__", "f_global_env", envir = environment())
+  
+  f_internal <- function() {
+    assign(".__name__", "f_internal_env", envir = environment())
+    print("Debug: Current Environment Path")
+    print_environment_hierarchy(environment())
+  }
+  
+  f_internal()
+}
+
+# Calling the global function
+f_global()
+# > f_global()
+# [1] "Debug: Current Environment Path"
+# Current environment path:
+#  emptyenv() -> base -> unknown -> package:methods -> package:datasets -> package:utils -> package:grDevices -> package:graphics -> package:stats -> unknown -> package:tidyverse -> package:ggplot2 -> package:tibble -> package:tidyr -> package:readr -> package:purrr -> package:dplyr -> package:stringr -> package:forcats -> package:lubridate -> R_GlobalEnv -> f_global_env -> f_internal_env 
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# Function to print the environment hierarchy with more details
+print_environment_hierarchy <- function(env) {
+  env_names <- c()
+  while (!identical(env, emptyenv())) {
+    if (exists(".__name__", envir = env, inherits = FALSE)) {
+      env_name <- get(".__name__", envir = env)
+    } else if (identical(env, globalenv())) {
+      env_name <- "R_GlobalEnv"
+    } else if (identical(env, baseenv())) {
+      env_name <- "base"
+    } else if (!is.null(attr(env, "name")) && grepl("^package:", attr(env, "name"))) {
+      env_name <- attr(env, "name")
+    } else {
+      env_name <- paste("unknown (", format(env), ")", sep = "")
+    }
+    env_names <- c(env_names, env_name)
+    env <- parent.env(env)
+  }
+  env_names <- c(env_names, "emptyenv()")
+  env_names <- rev(env_names)
+  cat("Current environment path:\n", paste(env_names, collapse = " -> "), "\n")
+}
+
+# Example of nested functions with manually named environments
+f_global <- function() {
+  assign(".__name__", "f_global_env", envir = environment())
+  
+  f_internal <- function() {
+    assign(".__name__", "f_internal_env", envir = environment())
+    print("Debug: Current Environment Path")
+    print_environment_hierarchy(environment())
+  }
+  
+  f_internal()
+}
+
+# Calling the global function
+f_global()
+# > f_global()
+# [1] "Debug: Current Environment Path"
+# Current environment path:
+#  emptyenv() -> base -> unknown (<environment: 0x55e96efc8b58>) -> package:methods -> package:datasets -> package:utils -> package:grDevices -> package:graphics -> package:stats -> unknown (<environment: 0x55e970e13a70>) -> package:tidyverse -> package:ggplot2 -> package:tibble -> package:tidyr -> package:readr -> package:purrr -> package:dplyr -> package:stringr -> package:forcats -> package:lubridate -> R_GlobalEnv -> f_global_env -> f_internal_env 
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
+
+
 #|________________________________________________________________________________|#  
 #|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
 #@@ RUN ALL ABOVE: CTRL+ALT+B -----
