91599bf503b3d81bcf9651f45d0bd4195c1bff07
mkim0710 <mkim0710@gmail.com>
2024-07-27 02:43:09 +0900

    rm - cat('%windir%\\explorer.exe "',backup_to_path|>normalizePath(winslash="\\"),'"',"  \n", sep="") ;

--- env1$env.internal.source.r
+++ env1$env.internal.source.r
@@ -190,7 +190,6 @@ env1$env.internal$ f_path_path.backup.overwrite <- function(overwrite_from_path.
     if(createFile || file.exists(destination_path.filename.ext)) {
         if(!is.null(backup_to_path)) {
             env1$env.internal$f_filename.ext.createBackup(backup_from_path.filename.ext = destination_path.filename.ext, backup_to_path=backup_to_path, timeFormat=timeFormat)
-            # cat('%windir%\\explorer.exe "',backup_to_path|>normalizePath(winslash="\\"),'"',"  \n", sep="") ; 
         }
         if(file.copy(from=overwrite_from_path.filename.ext, to=destination_path.filename.ext, overwrite=TRUE)) message(paste0("Update successful: ", destination_path.filename.ext)) else warning(paste0("Update failed: ", destination_path.filename.ext));
     } else {warning(paste0("File does not exist: ", destination_path.filename.ext))}
