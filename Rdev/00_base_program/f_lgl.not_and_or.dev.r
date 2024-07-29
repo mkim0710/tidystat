c7dcb327ab2ba4b0d4de11f348523ff8206d9aa5
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-07-29 15:31:00 +0000

    Rdev/00_base_program/f_lgl.not_and_or.dev.r

---
+++ Rdev/00_base_program/f_lgl.not_and_or.dev.r
@@ -0,0 +1,243 @@
+# Rdev/00_base_program/f_lgl.not_and_or.dev.r  
+# Rdev/00_base_program/f_lgl.not_and_or.dev.Rmd  
+# Rdev/00_base_program/f_lgl.not_and_or.source.r 
+# # source("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.source.r")  
+# # if(!file.exists("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.dev.r")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.dev.r")  
+# # if(!file.exists("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.dev.Rmd")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/templates-00env1.minimum.Rmd", destfile = "~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.dev.Rmd")  
+# # if(!file.exists("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.source.r")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.source.r")  
+# file.edit("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.dev.r"); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));  
+# file.edit("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.dev.Rmd"); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));  
+# file.edit("~/github_tidystat/Rdev/00_base_program/f_lgl.not_and_or.source.r"); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext)); 
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|________________________________________________________________________________|#  ----  
+#| Ctrl(+Shift)+1: Move focus (zoom) to Source Editor 
+#| Ctrl+Shift+.[period]: Switch to tab (Show List of Open Tabs~!!!)
+#| Ctrl+Alt+Shift+Left/Right: First/Last tab
+#| Ctrl+Alt+D: Show diff between current and last commit
+#| Ctrl+Alt+M: Commit changes to Git
+#| Ctrl+Shift+O: Show/Hide Document Outline
+#| Alt+Shift+Drag: Multiline Cursor
+#| Ctrl+Shift+F10: Restart R Session 
+#| Ctrl+Alt+B: Run from start to current line 
+#| Shift+F9: Toggle Breakpoint 
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# file.edit( paste0("[Working Files List] ",basename(getwd()),".r") )  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# cmd /C "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="https://github.com/mkim0710/tidystat/blob/master/rstudio-prefs/templates/default.R"  
+# cmd /C "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="https://github.com/mkim0710/tidystat/blob/master/rstudio-prefs/templates/templates-00env1.minimum.Rmd"  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# rm(list=ls())
+# rstudioapi::restartSession()  # ctrl+shift+f10
+# https://stackoverflow.com/questions/7505547/detach-all-packages-while-working-in-r
+# .sourcename = "function.detachAllPackages" |> paste0(".source.r"); .subpath=r"()"|>str_replace_all("\\\\","/"); .subpath.filename.source.r = paste0(.subpath,ifelse(.subpath=="","","/"),.sourcename); if(!.sourcename %in% names(.GlobalEnv$env1$source)) {cat('> source("',file.path(env1$path$source_base,.subpath.filename.source.r),'")', "  \n", sep=""); .GlobalEnv$env1$source[[.sourcename]] = file.path(env1$path$source_base,.subpath.filename.source.r); source(.GlobalEnv$env1$source[[.sourcename]])}
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# cmd /C "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="https://github.com/mkim0710/tidystat/blob/master/.Rprofile"    
+#| ------------------------- < To be covered at .Rprofile > --------------------- |#  
+if(!exists("env1", envir=.GlobalEnv)) {  cat('> source("https://github.com/mkim0710/tidystat/raw/master/.Rprofile")  \n')  ;  source("https://github.com/mkim0710/tidystat/raw/master/.Rprofile")  ;  .First()  }  
+if(!".Rprofile" %in% names(.GlobalEnv$env1$source)) {  cat('> source("https://github.com/mkim0710/tidystat/raw/master/.Rprofile")  \n')  ;  source("https://github.com/mkim0710/tidystat/raw/master/.Rprofile")  ;  .First()  }  
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+## env1\$path ====
+# tibble( symbol = c("/", "~", ".", "..")) |> mutate(normalizePath = symbol |> normalizePath(winslash="/") ) |> format() |> (\(vec) vec[c(-1,-3)])() |> cat("  \n", sep="  \n") 
+# if (.Platform$OS.type == 'windows') { "." |> normalizePath(winslash="/") |> utils::browseURL() } else { "." |> dir(all.files=TRUE) %>% paste0('"',.,'"') |> paste(collapse = ", \n  ") %>% cat("c(",.,")", "  \n", sep="") }
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+# *** Caution) In Rstudio Notebook, the path of the running Rmd file is set as the working directory~!!!
+# env1$path$CurrentSource.path.filename.ext = rstudioapi::getSourceEditorContext()$path |> normalizePath(winslash="/") |> str_replace(fixed(getwd()|>normalizePath(winslash="/")), "") |> str_replace("^/", "")
+if (requireNamespace("rstudioapi")) {
+    if(Sys.getenv("print.intermediate")==TRUE) {.CodeText2Print = 'requireNamespace("rstudioapi")'; print(ifelse(is.null(eval(parse(text=.CodeText2Print))), paste0("is.null(",.CodeText2Print,") == TRUE"), paste0(.CodeText2Print," == ",eval(parse(text=.CodeText2Print)))))}  
+    if (rstudioapi::isAvailable()) {
+        env1$path$CurrentSource.path.filename.ext = rstudioapi::getSourceEditorContext()$path |> normalizePath(winslash="/") |> str_replace(fixed(env1$path$path1|>normalizePath(winslash="/")), "") |> str_replace("^/", "")
+    } else { if(Sys.getenv("print.intermediate")==TRUE) print('rstudioapi::isAvailable() == FALSE') }
+    if(Sys.getenv("print.intermediate")==TRUE) {.CodeText2Print = 'env1$path$CurrentSource.path.filename.ext'; print(ifelse(is.null(eval(parse(text=.CodeText2Print))), paste0("is.null(",.CodeText2Print,") == TRUE"), paste0(.CodeText2Print," == ",eval(parse(text=.CodeText2Print)))))}  
+} else { if(Sys.getenv("print.intermediate")==TRUE) print('requireNamespace("rstudioapi") == FALSE') }
+file.edit(paste0("[Working Files List] ",basename(getwd()),".r")); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext))
+#|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
+## env1\$env.internal\$f_path.df_dirs_recursive.df_files() ----
+# .sourcename = "f_path.df_dirs_recursive.df_files" |> paste0(".source.r"); .subpath=r"()"|>str_replace_all("\\\\","/"); .subpath.filename.source.r = paste0(.subpath,ifelse(.subpath=="","","/"),.sourcename); if(!.sourcename %in% names(.GlobalEnv$env1$source)) {cat('> source("',file.path(env1$path$source_base,.subpath.filename.source.r),'")', "  \n", sep=""); .GlobalEnv$env1$source[[.sourcename]] = file.path(env1$path$source_base,.subpath.filename.source.r); source(.GlobalEnv$env1$source[[.sourcename]])}
+# env1$path$df_dirs_recursive.df_files = env1$env.internal$f_path.df_dirs_recursive.df_files(input_path=env1$path$path1, print.message=FALSE)
+# env1$path$df_dirs_recursive.df_files$path |> unique() |> paste0(collapse = "\n") |> cat("  \n", sep="")
+# env1$path$df_dirs_recursive.df_files |> dplyr::filter(path.level <= 2) |> dplyr::select(print_tree_path_files.codes) |> unlist() |> paste(collapse="") |> cat("  \n", sep="")
+# # Rdev
+# # Rdev/-dev
+# # Rdev/00_base_program
+# # Rdev/00_protocol
+# # Rdev/00_protocol/01_sample_size
+# # Rdev/00_protocol/05_count_eligible
+# # Rdev/00_protocol/06_count_exposure
+# # Rdev/00_protocol/07_count_outcome
+# # Rdev/10_import_clean_datatype
+# # Rdev/10_import_clean_datatype/11_metadata
+# # Rdev/10_import_clean_datatype/12_import_files
+# # Rdev/10_import_clean_datatype/12_import_sqlite
+# # Rdev/10_import_clean_datatype/12_import_vocabulary
+# # Rdev/10_import_clean_datatype/13_duplicated
+# # Rdev/10_import_clean_datatype/13_missing_value
+# # Rdev/10_import_clean_datatype/13_split_fold
+# # Rdev/10_import_clean_datatype/15_cleaning_text
+# # Rdev/10_import_clean_datatype/15_cleaning_time
+# # Rdev/10_import_clean_datatype/16_categorical_factor
+# # Rdev/10_import_clean_datatype/17_categorical_indicators
+# # Rdev/10_import_clean_datatype/18_dichotomous_logical
+# # Rdev/10_import_clean_datatype/19_datetime
+# # Rdev/10_import_clean_datatype/19_numeric_integer
+# # Rdev/10_import_clean_datatype/array_list
+# # Rdev/20_tidy_group_by_match
+# # Rdev/20_tidy_group_by_match/23_group_by_PersonID
+# # Rdev/20_tidy_group_by_match/25_study_population
+# # Rdev/20_tidy_group_by_match/27_match
+# # Rdev/30_transform_scale_categorical
+# # Rdev/40_visualize_explore_bivariate_stratified
+# # Rdev/40_visualize_explore_bivariate_stratified/43_network
+# # Rdev/40_visualize_explore_bivariate_stratified/44_map
+# # Rdev/40_visualize_explore_bivariate_stratified/45_bivariate_measures
+# # Rdev/40_visualize_explore_bivariate_stratified/47_bivariate_partial_stratified
+# # Rdev/50_model_formula_evaluation
+# # Rdev/50_model_formula_evaluation/51_model_formula
+# # Rdev/50_model_formula_evaluation/53_model_selection
+# # Rdev/50_model_formula_evaluation/55_model_weighted
+# # Rdev/50_model_formula_evaluation/56_model_bootstrap
+# # Rdev/50_model_formula_evaluation/57_model_time2event
+# # Rdev/50_model_formula_evaluation/57_model_trajectory
+# # Rdev/50_model_formula_evaluation/59_model_evaluation
+# # Rdev/60_communicate_report_export
+#|________________________________________________________________________________|#  
+cat("> .tmp |> str(max.level = 1)", "  \n", sep="") 
+.tmp |> str(max.level = 1)
+cat("> env1 |> as.list() |> str(max.level = 2, give.attr = FALSE)", "  \n", sep="") 
+env1 |> as.list() |> str(max.level = 2, give.attr = FALSE)
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#@@ START) source -----
+### @ .subpath, .sourcename ======
+.subpath=r"(Rdev/00_base_program)"|>str_replace_all("\\\\","/")  # Using Raw Strings in R 4.0.0 and Later: The raw string literal, denoted by r"(...)", will not process \ as an escape character.
+# if(.subpath!="") utils::browseURL(normalizePath(.subpath))
+.sourcename = "f_lgl.not_and_or" |> paste0(".source.r")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+env1$path$.subpath = .subpath
+.sourcename_root = .sourcename |> str_replace("\\.source\\.r$", "")
+env1$path$.sourcename_root = .sourcename_root
+env1$path$.subpath.filename.dev.r = paste0(.subpath,ifelse(.subpath=="","","/"),.sourcename_root,".dev.r")
+env1$path$.subpath.filename.dev.Rmd = paste0(.subpath,ifelse(.subpath=="","","/"),.sourcename_root,".dev.Rmd")
+env1$path$.subpath.filename.source.r = paste0(.subpath,ifelse(.subpath=="","","/"),.sourcename)
+cat("# ",'.sourcename_root = "',.sourcename_root,'"', "  \n",
+    "# ",env1$path$.subpath.filename.dev.r, "  \n",
+    "# ",env1$path$.subpath.filename.dev.Rmd, "  \n",
+    "# ",env1$path$.subpath.filename.source.r, "  \n",
+    '# # source("',env1$path$source_base,"/",env1$path$.subpath.filename.source.r,'")', "  \n",
+    '# # if(!file.exists("',env1$path$source_base_local,"/",env1$path$.subpath.filename.dev.r,'")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "',env1$path$source_base_local,"/",env1$path$.subpath.filename.dev.r,'")', "  \n",
+    '# # if(!file.exists("',env1$path$source_base_local,"/",env1$path$.subpath.filename.dev.Rmd,'")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/templates-00env1.minimum.Rmd", destfile = "',env1$path$source_base_local,"/",env1$path$.subpath.filename.dev.Rmd,'")', "  \n",
+    '# # if(!file.exists("',env1$path$source_base_local,"/",env1$path$.subpath.filename.source.r,'")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "',env1$path$source_base_local,"/",env1$path$.subpath.filename.source.r,'")', "  \n",
+    '# file.edit("',env1$path$source_base_local,"/",env1$path$.subpath.filename.dev.r,'"); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));', "  \n",
+    '# file.edit("',env1$path$source_base_local,"/",env1$path$.subpath.filename.dev.Rmd,'"); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));', "  \n",
+    '# file.edit("',env1$path$source_base_local,"/",env1$path$.subpath.filename.source.r,'"); file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));', "  \n",
+    sep="")
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # \% source( file.path(env1$path$source_base,.subpath.filename.source.r) ) ----
+# .subpath.filename.source.r = paste0(.subpath,ifelse(.subpath=="","","/"),.sourcename)
+# if(!.sourcename %in% names(.GlobalEnv$env1$source)) {cat('> source("',file.path(env1$path$source_base,.subpath.filename.source.r),'")', "  \n", sep=""); .GlobalEnv$env1$source[[.sourcename]] = file.path(env1$path$source_base,.subpath.filename.source.r); source(.GlobalEnv$env1$source[[.sourcename]])}
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#@@ RUN ALL ABOVE: CTRL+ALT+B -----
+#| RUN ALL ABOVE: CTRL+ALT+B |#
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+# #@@ START) examples -----
+# https://chatgpt.com/c/d388a642-48b0-42ea-a264-4ae2d77b024e
+
+!TRUE && FALSE
+!TRUE && TRUE
+!FALSE && TRUE
+!TRUE || FALSE
+TRUE && FALSE || TRUE
+!FALSE && TRUE || FALSE
+# > !TRUE && FALSE
+# [1] FALSE
+# > !TRUE && TRUE
+# [1] FALSE
+# > !FALSE && TRUE
+# [1] TRUE
+# > !TRUE || FALSE
+# [1] FALSE
+# > TRUE && FALSE || TRUE
+# [1] TRUE
+# > !FALSE && TRUE || FALSE
+# [1] TRUE
+
+
+ !"A" %in% c("A", "B") &&  "C" %in% c("C", "D") || "E" %in% c("E", "F")
+(!"A" %in% c("A", "B") && "C" %in% c("C", "D")) || "E" %in% c("E", "F")
+ !"A" %in% c("A", "B") && ("C" %in% c("C", "D") || "E" %in% c("E", "F"))
+# >  !"A" %in% c("A", "B") &&  "C" %in% c("C", "D") || "E" %in% c("E", "F")
+# [1] TRUE
+# > (!"A" %in% c("A", "B") && "C" %in% c("C", "D")) || "E" %in% c("E", "F")
+# [1] TRUE
+# >  !"A" %in% c("A", "B") && ("C" %in% c("C", "D") || "E" %in% c("E", "F"))
+# [1] FALSE
+
+
+TRUE * TRUE
+TRUE * FALSE
+FALSE * FALSE
+# > TRUE * TRUE
+# [1] 1
+# > TRUE * FALSE
+# [1] 0
+# > FALSE * FALSE
+# [1] 0
+
+TRUE + TRUE
+TRUE + FALSE
+FALSE + FALSE
+# > TRUE + TRUE
+# [1] 2
+# > TRUE + FALSE
+# [1] 1
+# > FALSE + FALSE
+# [1] 0
+
+any(c(TRUE, TRUE))
+any(c(TRUE, FALSE))
+any(c(FALSE, FALSE))
+# > any(c(TRUE, TRUE))
+# [1] TRUE
+# > any(c(TRUE, FALSE))
+# [1] TRUE
+# > any(c(FALSE, FALSE))
+# [1] FALSE
+
+all(c(TRUE, TRUE))
+all(c(TRUE, FALSE))
+all(c(FALSE, FALSE))
+# > all(c(TRUE, TRUE))
+# [1] TRUE
+# > all(c(TRUE, FALSE))
+# [1] FALSE
+# > all(c(FALSE, FALSE))
+# [1] FALSE
+
+
+
+
+
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#@@ END -----  
+# paste0("https://github.com/mkim0710/",basename(getwd()),"/blob/main/",env1$path$CurrentSource.path.filename.ext) %>% paste0('"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="',.,'"') |> system(intern=TRUE)
+paste0("https://github.com/mkim0710/",basename(getwd()),"/blob/main/",env1$path$CurrentSource.path.filename.ext) %>% paste0('"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="',.,'"') %>% paste0("'",.,"' |> system(intern=TRUE)") |> cat("  \n", sep="")
+# paste0("https://github.com/mkim0710/",basename(getwd()),"/commits/main/",env1$path$CurrentSource.path.filename.ext) %>% paste0('"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="',.,'"') |> system(intern=TRUE)
+paste0("https://github.com/mkim0710/",basename(getwd()),"/commits/main/",env1$path$CurrentSource.path.filename.ext) %>% paste0('"C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" --app="',.,'"') %>% paste0("'",.,"' |> system(intern=TRUE)") |> cat("  \n", sep="")
+cat("* To revert to the last commited file, run the following terminal command:\n", 
+    '"git checkout -- ',rstudioapi::getSourceEditorContext()$path,'" |> system(intern=TRUE)',"  \n", sep="")
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+#|________________________________________________________________________________|#  
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
+
+
