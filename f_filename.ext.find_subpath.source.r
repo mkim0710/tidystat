9b1c40665e2843c4fd14c583c7827e9a1a651c38
mkim0710 <mkim0710@gmail.com>
2024-07-30 04:17:28 +0900

    deparse(width.cutoff=120-15) |> cat("  ", sep="  \n")
    Former-commit-id: 9bbecc5677932524ba9c6d6b1ef9b677c9dd6825

--- f_filename.ext.find_subpath.source.r
+++ f_filename.ext.find_subpath.source.r
@@ -185,7 +185,7 @@ if(!.tmp$objectname %in% names(.GlobalEnv$env1$f)) {
     if (length(list_out) == 0) {
         cat("----------- File not found while searching following subpaths:\n")
         vec_subpath <- unlist(list_subpath, use.names = FALSE)
-        vec_subpath |> deparse(width.cutoff=500) |> cat("  \n", sep="  \n"); # dput(); |> deparse(width.cutoff=120-15) |> cat("  \n", sep="  \n"); # width.cutoff=500 is the max ----
+        vec_subpath |> deparse(width.cutoff=500) |> cat("  \n", sep="  \n"); # dput(); |> deparse(width.cutoff=120-15) |> cat("  ", sep="  \n"); # width.cutoff=500 is the max ----
         return(NULL)
     }
         
