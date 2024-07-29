9bbecc5677932524ba9c6d6b1ef9b677c9dd6825
mkim0710 <mkim0710@gmail.com>
2024-07-30 04:17:28 +0900

    deparse(width.cutoff=120-15) |> cat("  ", sep="  \n")

--- f_objectname.read.checkEntity.source.r
+++ f_objectname.read.checkEntity.source.r
@@ -167,7 +167,7 @@ for (.dependancy in c("f_filename.ext.find_subpath", "f_path.size_files")) {
     cat(strrep("%",80),"\n",sep="")
     cat("> names(",objectname,') |> deparse(width.cutoff=120-15) |> cat(sep="\\n")',"  \n", sep=""); 
     if(ncol(get(objectname)) <= 100 || print.name.dput) {
-        get(objectname) |> names() |> deparse(width.cutoff=120-15) |> cat("  \n", sep="  \n"); # dput(); |> deparse(width.cutoff=120-15) |> cat("  \n", sep="  \n"); # width.cutoff=500 is the max ----
+        get(objectname) |> names() |> deparse(width.cutoff=120-15) |> cat("  ", sep="  \n"); # dput(); |> deparse(width.cutoff=120-15) |> cat("  ", sep="  \n"); # width.cutoff=500 is the max ----
     } else {cat("ncol(get(",objectname,")) > 100 && !print.name.dput \n", sep="")}
     cat(strrep("~",80),"\n",sep="")
     cat("> names(",objectname,') |> paste(collapse=", ") |> cat(sep="\\n")',"  \n", sep=""); 
