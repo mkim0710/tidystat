ae229148f2d98488dc9bdd4accc9fbd4033ab8c0
mkim0710 <mkim0710@gmail.com>
2024-07-30 13:41:21 +0900

    Update default.R

--- rstudio-prefs/templates/default.R
+++ rstudio-prefs/templates/default.R
@@ -5,9 +5,9 @@
 # "default.R" %>% paste0(env1$path$source_base,"/rstudio-prefs/templates/",.) %>% {.[file.exists(.)]} |> file.edit(); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext))
 # "templates-00env1.minimum.Rmd" %>% paste0(env1$path$source_base,"/rstudio-prefs/templates/",.) %>% {.[file.exists(.)]} |> file.edit(); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext))     
 #|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
-# source(paste0(env1$path$source_base,"/","rstudio-prefs/templates/default.R"))  # if (.Platform$OS.type == "windows") update default.R
+# if (.Platform$OS.type == "windows") source(paste0(env1$path$source_base,"/","rstudio-prefs/templates/default.R"))  # update default.R
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|#  
 #|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|#  
-# # source("https://github.com/mkim0710/tidystat/raw/master/rstudio-prefs/templates/default.R")
 # # if (.Platform$OS.type == "windows") {.path4APPDATA_RStudio = file.path(Sys.getenv("APPDATA"), "RStudio")} else if (.Platform$OS.type == "unix") {.path4APPDATA_RStudio = "~/.config/rstudio"}
 # # if(!dir.exists(file.path(.path4APPDATA_RStudio, "templates"))) dir.create(file.path(.path4APPDATA_RStudio, "templates"))
 # # "default.R" %>% file.path(.path4APPDATA_RStudio, "templates", .) %>% {.[file.exists(.)]} |> file.edit(); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext))
