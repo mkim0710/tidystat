079f1d1ef48d147c3a9afe8d7e2a1406130c546a
mkim0710 <mkim0710@gmail.com>
2024-07-31 03:38:02 +0900

    |> paste(collapse=", ") |> strsplit(paste0("(?<=.{",120-20,"})"), perl = TRUE) |> unlist() |> paste0(collapse="  \n") |> cat("  \n", sep=""); # tidyeval) paste(collapse=", ") |> cat("  \n", sep="") ----
    Former-commit-id: 1f9f95b60e0dcd78483aeb4c0ce0139fbfe0af36

--- Rdev/00_base_program/function.deparse.cat.width.dev.r
+++ Rdev/00_base_program/function.deparse.cat.width.dev.r
@@ -3,7 +3,7 @@
 
 objectname = "KoGES201806vars_waves"
 get(objectname) |> names() |> deparse(width.cutoff=120-20) |> paste0(collapse="  \n") |> cat("  \n", sep=""); # dput(); |> deparse(width.cutoff=120-20) |> paste0(collapse="  \n") |> cat("  \n", sep=""); # width.cutoff=500 is the max ----
-get(objectname) |> names() |> paste(collapse=", ") |> strsplit(paste0("(?<=.{",120-20,"})"), perl = TRUE) |> unlist() |> paste0(collapse="  \n") %>% {cat("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    \n", ., "  \n", sep="")}  ;   # tidyselect: paste(collapse=", ") |> cat("  \n", sep="") ----  
+get(objectname) |> names() |> paste(collapse=", ") |> strsplit(paste0("(?<=.{",120-20,"})"), perl = TRUE) |> unlist() |> paste0(collapse="  \n") %>% {cat("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    \n", ., "  \n", sep="")}  ;   # tidyeval) paste(collapse=", ") |> cat("  \n", sep="") ----  
 
 
 objectname = "KoGES201806vars_waves"
@@ -13,7 +13,7 @@ get(objectname) |> names() %>% {cat(deparse(., width.cutoff=120),"\n\n",sep="")}
 objectname = "KoGES201806vars_waves"
 cat(strrep("~",80),"\n",sep=""); get(objectname) |> names() |> deparse() |> cat("  \n", sep="") # dput() cat(deparse()) ----
 cat(strrep("~",80),"\n",sep=""); get(objectname) |> names() |> deparse(width.cutoff=120-20) |> paste0(collapse="  \n") |> cat("  \n", sep=""); # dput(); |> deparse(width.cutoff=120-20) |> paste0(collapse="  \n") |> cat("  \n", sep=""); # width.cutoff=500 is the max ----
-cat(strrep("~",80),"\n",sep=""); get(objectname) |> names() |> paste(collapse=", ") |> cat("  \n", sep=""); # tidyselect: paste(collapse=", ") |> cat("  \n", sep="") ----
+cat(strrep("~",80),"\n",sep=""); get(objectname) |> names() |> paste(collapse=", ") |> strsplit(paste0("(?<=.{",120-20,"})"), perl = TRUE) |> unlist() |> paste0(collapse="  \n") |> cat("  \n", sep=""); # tidyeval) paste(collapse=", ") |> cat("  \n", sep="") ----
 # > data |> names() |> deparse() |> cat("  \n", sep="") #----
 # c("Time2Event", "Event", "Exposure")
 # > data |> names() |> paste(collapse=", ") #----
