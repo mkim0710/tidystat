a5abed97346a8335b74e6e3ded043fd0995cf1d5
mkim0710 <mkim0710@gmail.com>
2024-08-01 05:36:40 +0900

    .path.filename.ext = ifelse( file.exists(.subpath.filename.ext), .subpath.filename.ext, paste0(env1$path$source_base_github,"/data/",DataSetName,".rds") )

--- Rdev/00_base_program/print_environment_hierarchy.dev.r
+++ Rdev/00_base_program/print_environment_hierarchy.dev.r
@@ -486,15 +486,12 @@ cat("# ",'.sourcename_root = "',.sourcename_root,'"', "  \n",
 # # \$ assign( DataSetName, read_rds(paste0(.path4read,"/",DataSetName,".rds")) ) ====  
 # # .path4read = file.path(env1$path$path0,"Rproject_KNHIS.CohortGJ0910 NoHx")
 # # DataSetName = "CohortGJ0910.BaselineJKGJ2085NoHx.drop_na.MetS_NoMeds"
-# .subpath = "data"
+# .subpath = r"(data)"|>str_replace_all("\\\\","/")
 # DataSetName = "CohortGJ0910.BaselineJKGJ2085NoHx...01"
-# path.filename.ext = ifelse(
-#     file.exists(paste0(.subpath,"/",DataSetName,".rds"))
-#     , paste0(.subpath,"/",DataSetName,".rds")
-#     , paste0(env1$path$source_base_github,"/",.subpath,"/",DataSetName,".rds")
-# )
-# cat("path.filename.ext = ", path.filename.ext, "\n", sep = "")
-# assign( DataSetName, read_rds(path.filename.ext) )
+# .subpath.filename.ext = DataSetName |> paste0(".rds") %>% paste0(.subpath,ifelse(.subpath=="","","/"),.)
+# .path.filename.ext = ifelse( file.exists(.subpath.filename.ext), .subpath.filename.ext, paste0(env1$path$source_base_github,"/data/",DataSetName,".rds") )
+# cat(".path.filename.ext = ", .path.filename.ext, "  \n", sep = "")
+# assign( DataSetName, read_rds(.path.filename.ext) )
 
 
 
