be474ea25aad805279b235ba1c25f527d8ca6eb2
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-09-03 14:00:35 +0000

    create
    Former-commit-id: ced80fc0b5ff1cf76f87e8861026f66873b988a0

---
+++ Rdev/10_import_clean_datatype/16_categorical_factor/f_df.add_BMI_ge300.delta.cut5.source.r
@@ -0,0 +1,95 @@
+#%% f_df.add_BMI.cut4() ====  
+# library(dplyr)
+# library(rlang)
+
+
+## \$ f_df.add_BMI_calculated =  ----  
+# https://github.com/mkim0710/tidystat/blob/master/Rdev/10_import_clean_datatype/16_categorical_factor/f_df.add_BMI.cut4.source.r
+.tmp$objectname = "f_df.add_BMI_calculated"
+.tmp$object = function(DataSet, varname_Height = "HEIGHT", varname_Weight = "WEIGHT", varname_BMI = "BMI") {
+
+  # Check if BMI column exists
+  if (varname_BMI %in% names(DataSet)) {
+    warning(paste0(deparse(varname_BMI),' %in% names(DataSet)')); cat("  \n", sep="")
+    DataSet <- DataSet %>%
+      mutate(
+        !!rlang::sym(paste0(varname_BMI,"_calculated")) := as.numeric(!!rlang::sym(varname_Weight)) / (as.numeric(!!rlang::sym(varname_Height))/100)^2
+      )
+  } else {
+    DataSet <- DataSet %>%
+      mutate(
+        !!rlang::sym(varname_BMI) := as.numeric(!!rlang::sym(varname_Weight)) / (as.numeric(!!rlang::sym(varname_Height))/100)^2
+      )
+  }
+    
+  return(DataSet)
+}
+### |> f_function.load2env.internal(.tmp$objectname, env1_subenv_name) ----
+env1$env.internal$f_function.load2env.internal(.tmp$object, .tmp$objectname, env1_subenv_name = "f", show_packageStartupMessage = TRUE)
+
+
+
+## \$ f_df.add_BMI.cut4 =  ----  
+# https://github.com/mkim0710/tidystat/blob/master/Rdev/10_import_clean_datatype/16_categorical_factor/f_df.add_BMI.cut4.source.r
+.tmp$objectname = "f_df.add_BMI.cut4"
+.tmp$object = function(DataSet, varname_BMI = "BMI") {
+
+  # Compute additional BMI-related columns
+  DataSet <- DataSet %>%
+    mutate(
+      !!rlang::sym(paste0(varname_BMI,"_lt185")) := !!rlang::sym(varname_BMI) < 18.5,
+      
+      !!rlang::sym(paste0(varname_BMI,"_ge185lt230")) := !!rlang::sym(varname_BMI) >= 18.5 & !!rlang::sym(varname_BMI) < 23,
+      !!rlang::sym(paste0(varname_BMI,"_ge230lt250")) := !!rlang::sym(varname_BMI) >= 23 & !!rlang::sym(varname_BMI) < 25,
+      !!rlang::sym(paste0(varname_BMI,"_ge185lt250")) := !!rlang::sym(varname_BMI) >= 18.5 & !!rlang::sym(varname_BMI) < 25,
+      !!rlang::sym(paste0(varname_BMI,"_ge250lt300")) := !!rlang::sym(varname_BMI) >= 25 & !!rlang::sym(varname_BMI) < 30,
+      !!rlang::sym(paste0(varname_BMI,"_ge230")) := !!rlang::sym(varname_BMI) >= 23,
+      !!rlang::sym(paste0(varname_BMI,"_ge250")) := !!rlang::sym(varname_BMI) >= 25,
+      !!rlang::sym(paste0(varname_BMI,"_ge300")) := !!rlang::sym(varname_BMI) >= 30,
+      !!rlang::sym(paste0(varname_BMI,"_ge350")) := !!rlang::sym(varname_BMI) >= 35,
+      !!rlang::sym(paste0(varname_BMI,"_ge400")) := !!rlang::sym(varname_BMI) >= 40,
+      !!rlang::sym(paste0(varname_BMI,".cut4")) := cut(!!rlang::sym(varname_BMI), breaks = c(0, 18.5, 25, 30, Inf), include.lowest = T, right = F),
+      !!rlang::sym(paste0(varname_BMI,".cut5")) := cut(!!rlang::sym(varname_BMI), breaks = c(0, 18.5, 23, 25, 30, Inf), include.lowest = T, right = F)
+    )
+  
+  return(DataSet)
+}
+### |> f_function.load2env.internal(.tmp$objectname, env1_subenv_name) ----
+env1$env.internal$f_function.load2env.internal(.tmp$object, .tmp$objectname, env1_subenv_name = "f", show_packageStartupMessage = TRUE)
+
+
+# gj_jk.Date.DTH.recode = 
+#     gj_jk.Date.DTH.recode %>% 
+#     data.add_BMI_calculated(varname_Height = "HEIGHT", varname_Weight = "WEIGHT") %>% 
+#     f_df.add_BMI.cut4
+# gj_jk.Date.DTH.recode %>% select(matches("BMI")) |> summary()
+# all.equal(gj_jk.Date.DTH.recode$BMI_calculated, gj_jk.Date.DTH.recode$BMI)
+
+
+
+# # @ function.df_add_BMI.cut4 ======  
+# function.df_add_BMI.cut4 = function(df) {
+#     df %>% mutate(
+#         BMI = as.numeric(WEIGHT) / (as.numeric(HEIGHT)/100)^2
+#         , BMI_lt185 = BMI < 18.5
+#         , BMI_ge185lt230 = BMI >= 18.5 & BMI < 23
+#         , BMI_ge230lt250 = BMI >= 23 & BMI < 25
+#         , BMI_ge185lt250 = BMI >= 18.5 & BMI < 25
+#         , BMI_ge250lt300 = BMI >= 25 & BMI < 30
+#         , BMI_ge300 = BMI >= 30
+#         , BMI_ge350 = BMI >= 35
+#         , BMI_ge400 = BMI >= 40
+#         , BMI.cut4 = BMI %>% cut(breaks = c(0, 18.5, 25, 30, Inf), include.lowest = T, right = F)
+#     )
+# }
+# 
+# # CohortGJ0910.BaselineJKGJ2085NoHx.drop_na %>% function.df_add_BMI.cut4 %>% select(matches("BMI")) |> summary() #----
+# # # > CohortGJ0910.BaselineJKGJ2085NoHx.drop_na %>% function.df_add_BMI %>% select(matches("BMI")) |> summary() #----  
+# # #       BMI        BMI_lt185       BMI_ge185lt230  BMI_ge230lt250  BMI_ge185lt250  BMI_ge250lt300  BMI_ge300       BMI_ge350       BMI_ge400            BMI_4cat     
+# # #  Min.   :11.96   Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical   Mode :logical   [0,18.5) : 11948  
+# # #  1st Qu.:21.31   FALSE:271850    FALSE:167553    FALSE:214403    FALSE:98158     FALSE:206858    FALSE:274528    FALSE:283030    FALSE:283697    [18.5,25):185640  
+# # #  Median :23.38   TRUE :11948     TRUE :116245    TRUE :69395     TRUE :185640    TRUE :76940     TRUE :9270      TRUE :768       TRUE :101       [25,30)  : 76940  
+# # #  Mean   :23.57                                                                                                                                   [30,Inf] :  9270  
+# # #  3rd Qu.:25.54                                                                                                                                                     
+# # #  Max.   :66.94   
+
