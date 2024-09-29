6f3da69fc460ae757ecca52dfb60b97e9bcef0a3
mkim0710 <mkim0710@gmail.com>
2024-09-29 18:48:49 +0900

    .subpath.filename.r
    Former-commit-id: 4f2d6f7cd0afe0b571f41d72c51df568916b6951

--- git/git log.r
+++ git/git log.r
@@ -68,7 +68,7 @@
 # |\  
 # | *   a091ed7 Merge branch 'master' of github.com:mkim0710/tidystat
 # | |\  
-# | * | 8119876 filename.r = "f_df.t.tribble_construct" |> paste0(c(".source.r",".dev.r")); subpath=r"()"|>str_replace_all("\\\\","/"); .subpath.filename.r = paste0(.subpath,ifelse(.subpath=="","","/"),.filename.r); subpath.filename.r = subpath.filename.r[file.exists(subpath.filename.r)]; file.edit( file.path(env1$path$source_base, .subpath.filename.r) ); file.edit(env1$path$CurrentSource.path.filename.ext %>% {paste0(env1$path$path1,"/",.)}
+# | * | 8119876 filename.r = "f_df.t.tribble_construct" |> paste0(c(".source.r",".dev.r")); .subpath=r"()"|>str_replace_all("\\\\","/"); .subpath.filename.r = paste0(.subpath,ifelse(.subpath=="","","/"),.filename.r); subpath.filename.r = subpath.filename.r[file.exists(subpath.filename.r)]; file.edit( file.path(env1$path$source_base, .subpath.filename.r) ); file.edit(env1$path$CurrentSource.path.filename.ext %>% {paste0(env1$path$path1,"/",.)}
 # * | | f74220b Update f_df.t.tribble_construct.source.r
 # | |/  
 # |/|   
@@ -445,7 +445,7 @@
 # |\  
 # | *   a091ed7 mhkim@rocker@MAGB760M13700KF Fri May 17 02:58:31 2024 Merge branch 'master' of github.com:mkim0710/tidystat
 # | |\  
-# | * | 8119876 mhkim@rocker@MAGB760M13700KF Fri May 17 02:58:25 2024 filename.r = "f_df.t.tribble_construct" |> paste0(c(".source.r",".dev.r")); subpath=r"()"|>str_replace_all("\\\\","/"); .subpath.filename.r = paste0(.subpath,ifelse(.subpath=="","","/"),.filename.r); subpath.filename.r = subpath.filename.r[file.exists(subpath.filename.r)]; file.edit( file.path(env1$path$source_base, .subpath.filename.r) ); file.edit(env1$path$CurrentSource.path.filename.ext %>% {paste0(env1$path$path1,"/",.)}
+# | * | 8119876 mhkim@rocker@MAGB760M13700KF Fri May 17 02:58:25 2024 filename.r = "f_df.t.tribble_construct" |> paste0(c(".source.r",".dev.r")); .subpath=r"()"|>str_replace_all("\\\\","/"); .subpath.filename.r = paste0(.subpath,ifelse(.subpath=="","","/"),.filename.r); subpath.filename.r = subpath.filename.r[file.exists(subpath.filename.r)]; file.edit( file.path(env1$path$source_base, .subpath.filename.r) ); file.edit(env1$path$CurrentSource.path.filename.ext %>% {paste0(env1$path$path1,"/",.)}
 # * | | f74220b mkim0710 Fri May 17 03:02:31 2024 Update f_df.t.tribble_construct.source.r
 # | |/  
 # |/|   
