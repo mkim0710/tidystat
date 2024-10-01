5f08f855245b956fae17f2376fbb7a77a92fcf05
mkim0710 <mkim0710@gmail.com>
2024-10-02 05:18:23 +0900

    Table1
    Former-commit-id: bd4125ead7c04617ad4b013575c1fd4209f80a91

--- Rdev/60_communicate_report_export/f_ModelOutput.tidy.flextable_JAMA.source-pending.r
+++ Rdev/60_communicate_report_export/f_ModelOutput.tidy.flextable_JAMA.source-pending.r
@@ -1,14 +1,14 @@
-# .sourcename_root = "f_table1.flextable"  
-#### Rdev/60_communicate_report_export/f_table1.flextable.dev.r----  
-#### Rdev/60_communicate_report_export/f_table1.flextable.dev.Rmd----  
-#### Rdev/60_communicate_report_export/f_table1.flextable.source.r----  
-# # source(paste0(env1$path$source_base,"/","Rdev/60_communicate_report_export/f_table1.flextable.source.r"))  
-# # if(!file.exists("~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.dev.r")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.dev.r")  
-# # if(!file.exists("~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.dev.Rmd")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/templates-00env1.minimum.Rmd", destfile = "~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.dev.Rmd")  
-# # if(!file.exists("~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.source.r")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.source.r")  
-# file.edit("~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.dev.r"); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));  
-# file.edit("~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.dev.Rmd"); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));  
-# file.edit("~/github_tidystat/Rdev/60_communicate_report_export/f_table1.flextable.source.r"); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext)); 
+# .sourcename_root = "f_Table1.flextable"  
+#### Rdev/60_communicate_report_export/f_Table1.flextable.dev.r----  
+#### Rdev/60_communicate_report_export/f_Table1.flextable.dev.Rmd----  
+#### Rdev/60_communicate_report_export/f_Table1.flextable.source.r----  
+# # source(paste0(env1$path$source_base,"/","Rdev/60_communicate_report_export/f_Table1.flextable.source.r"))  
+# # if(!file.exists("~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.dev.r")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.dev.r")  
+# # if(!file.exists("~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.dev.Rmd")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/templates-00env1.minimum.Rmd", destfile = "~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.dev.Rmd")  
+# # if(!file.exists("~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.source.r")) download.file(url = "https://raw.githubusercontent.com/mkim0710/tidystat/master/rstudio-prefs/templates/default.R", destfile = "~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.source.r")  
+# file.edit("~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.dev.r"); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));  
+# file.edit("~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.dev.Rmd"); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext));  
+# file.edit("~/github_tidystat/Rdev/60_communicate_report_export/f_Table1.flextable.source.r"); if(!is.null(env1$path$CurrentSource.path.filename.ext)) if(env1$path$CurrentSource.path.filename.ext != "") file.edit(paste0(env1$path$path1,"/",env1$path$CurrentSource.path.filename.ext)); 
 #|________________________________________________________________________________|#  
 #|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|#  
 # @@ START) source -----  
@@ -56,7 +56,7 @@ cat("# ",'.sourcename_root = "',.sourcename_root,'"  \n',
 ## \$ f_ModelOutput.tidy.flextable_JAMA =  ----  
 ## -> included in "f_df.t.tribble_construct.source.r"? Not Yet
 # https://chatgpt.com/c/66f444ad-c638-800e-a884-e76216e05329
-# /github_tidystat/Rdev/60_communicate_report_export/f_table1_print_df.flextable.source.r
+# /github_tidystat/Rdev/60_communicate_report_export/f_Table1_print_df.flextable.source.r
 .tmp$objectname = "f_ModelOutput.tidy.flextable_JAMA"
 .tmp$object  <- function(DataSet, VarNames4Exposure = c("InterventionGroup"), Varnames_factor.exclude = NULL, addOverall = TRUE, includeNA = TRUE, print.Varnames_nonnormal = NULL, print.showAllLevels = TRUE) {
     
