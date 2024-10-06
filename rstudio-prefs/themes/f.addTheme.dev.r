64c63dbd69f73f78745a5270463d5fa5024163ab
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-10-06 15:18:34 +0000

    f.addTheme.dev.r
    Former-commit-id: 3b3236606843da428715dc2e9bdd3d0610cb6a2a

---
+++ rstudio-prefs/themes/f.addTheme.dev.r
@@ -0,0 +1,7 @@
+# https://github.com/max-alletsee/rstudio-themes
+# https://github.com/max-alletsee/rstudio-themes/blob/main/data-raw/rstudio_themes.csv
+
+# https://github.com/rio-sanjuan/rstudio-themes
+rstudioapi::addTheme("https://raw.githubusercontent.com/rtjohnson12/rstudio-themes/master/src/github-dark.rstheme", apply=TRUE, force=TRUE)
+
+
