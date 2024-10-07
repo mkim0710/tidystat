b5625a72449c25f2da8c5b6dce6e73a8a9ac9564
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-10-07 08:51:03 +0000

    VSCodeTheme
    Former-commit-id: dc6a388fd410355e5dba2ebc647a7651a5840223

--- rstudio-prefs/themes/f.addTheme.dev.r
+++ rstudio-prefs/themes/f.addTheme.dev.r
@@ -1,7 +1,23 @@
 # https://github.com/max-alletsee/rstudio-themes
 # https://github.com/max-alletsee/rstudio-themes/blob/main/data-raw/rstudio_themes.csv
 
-# https://github.com/rio-sanjuan/rstudio-themes
-rstudioapi::addTheme("https://raw.githubusercontent.com/rtjohnson12/rstudio-themes/master/src/github-dark.rstheme", apply=TRUE, force=TRUE)
+# # https://github.com/rio-sanjuan/rstudio-themes
+# "https://raw.githubusercontent.com/rtjohnson12/rstudio-themes/master/src/github-dark.rstheme" %>% rstudioapi::addTheme(apply=TRUE, force=TRUE)
+# 
+# # https://github.com/thedbcooper/rstudio-theme-monokai-dimmed
+# "https://github.com/thedbcooper/rstudio-theme-monokai-dimmed/blob/main/Monokai-Dimmed.rstheme" %>% rstudioapi::addTheme(apply=TRUE, force=TRUE)
+# 
+# # https://github.com/LeiGuo0812/myRstudioThemes/
+# "https://raw.githubusercontent.com/LeiGuo0812/myRstudioThemes/main/Monokai%20Pro.rstheme" %>% rstudioapi::addTheme(apply=TRUE, force=TRUE)
+# 
+# # https://github.com/gu-stat/RStudio_Theme
+# "https://raw.githubusercontent.com/gu-stat/RStudio_Theme/master/monokai_mod.rstheme" %>% rstudioapi::addTheme(apply=TRUE, force=TRUE)
 
 
+
+# # https://github.com/ellenxtan/customized_Rstudio_theme
+# "https://raw.githubusercontent.com/ellenxtan/customized_Rstudio_theme/master/vscode.rstheme" %>% rstudioapi::addTheme(apply=TRUE, force=TRUE)
+
+# https://github.com/mattia-liuzzo/RStudio-VScode-theme
+# https://user-images.githubusercontent.com/63495216/110667938-0f4d4b00-81cb-11eb-943e-f5951dbed137.png
+"https://raw.githubusercontent.com/mattia-liuzzo/RStudio-VScode-theme/main/rs-vscode.rstheme" %>% rstudioapi::addTheme(apply=TRUE, force=TRUE)
