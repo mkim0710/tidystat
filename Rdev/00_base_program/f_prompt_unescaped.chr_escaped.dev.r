aea3c49306e78d382323bfc7fb338f9ab9baba0d
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-10-13 04:27:26 +0000

    f_prompt_unescaped.chr_escaped.dev.r

---
+++ Rdev/00_base_program/f_prompt_unescaped.chr_escaped.dev.r
@@ -0,0 +1,23 @@
+# https://chatgpt.com/c/670b4a25-1400-800e-80ea-d81cedfee1fa
+
+
+# @ Use scan to read the regex pattern ----
+my.regex <- scan(what = character(), n = 1, allowEscapes = FALSE)
+my.regex
+# When prompted, paste or type your regex pattern
+# > my.regex <- scan(what = character(), n = 1, allowEscapes = FALSE)
+# 1: my\.regex
+# Read 1 item
+# > my.regex
+# [1] "my\\.regex"
+
+# @ Use readLines to read the regex pattern ----
+# my.regex <- readLines(n = 1, warn = FALSE)
+my.regex <- readLines(n = 1)
+my.regex
+# > my.regex <- readLines(n = 1)
+# my\.regex
+# > my.regex
+# [1] "my\\.regex"
+
+
