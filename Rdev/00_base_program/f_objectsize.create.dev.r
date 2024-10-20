bea6e31b4d964c32b23238a49606091171589317
mhkim@rocker@MAGB760M13700KF <mhkim710@gmail.com>
2024-10-20 03:43:07 +0000

    f_objectsize.create.dev.r

---
+++ Rdev/00_base_program/f_objectsize.create.dev.r
@@ -0,0 +1,50 @@
+# f_objectsize.create.dev.r
+
+
+
+f_objectsize.create = function(objectsize = 1 * 1024^2) {
+        .object = numeric(objectsize/8)
+}
+
+
+100*1024^2
+# > 100*1024^2
+# [1] 104857600
+
+100*1024^2 |> f_objectsize.create() %>% str  # str() buggy when object is too large? 
+100*1024^2 |> f_objectsize.create() %>% length
+# > 100*1024^2 |> f_objectsize.create() %>% str  # str() buggy when object is too large? 
+#  num [1:131072] 0 0 0 0 0 0 0 0 0 0 ...
+# numeric(0)
+# > 100*1024^2 |> f_objectsize.create() %>% length
+# [1] 13107200
+
+13107200 * 8
+# > 13107200 * 8
+# [1] 104857600
+
+100*1024^2 |> f_objectsize.create() |> object.size() 
+# > 100*1024^2 |> f_objectsize.create() |> object.size()
+# 104862400 bytes
+104862400 - 104857600
+# > 104862400 - 104857600
+# [1] 4800
+
+
+
+f_objectsize.create = function(objectsize = 1 * 1024^2, createRandomNumbers = FALSE, objectname = NULL) {
+    
+    if (createRandomNumbers) {
+        .object = runif(objectsize/8)
+    } else {
+        .object = numeric(objectsize/8)
+    }
+    if(!is.null(objectname)) {
+        assign(objectname, .object)
+        invisible(.object)
+    } else {
+        .object
+    }
+}
+
+
