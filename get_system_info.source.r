71c9347469970c7d99d676803bd38ff8838d108e
mkim0710 <mkim0710@gmail.com>
2024-10-22 22:36:11 +0900

    # @@ Now included in env1$env.internal.source.r ----

--- get_system_info.source.r
+++ get_system_info.source.r
@@ -1,3 +1,5 @@
+# @@ Now included in env1$env.internal.source.r ----
+
 .sourcename = "get_system_info" |> paste0(".source.r")  
 if(Sys.getenv("VERBOSE")==TRUE) { print(paste0('Sourcing: "',.sourcename,'"')) }  
 # # cat("# ", '.objectname = "', .objectname, '"', "\n",
@@ -101,56 +103,56 @@ if(.subpath!="") utils::browseURL(normalizePath(.subpath))
 ### .GlobalEnv$env1$env.internal = new.env() ====  
 .subenvname = "env.internal"; .parentname = "env1"; if(!.subenvname %in% names(.GlobalEnv[[.parentname]])) { .GlobalEnv[[.parentname]][[.subenvname]] = new.env() }
 
-#% get_system_info() ====  
-.tmp$objectname = "get_system_info"
-.tmp$object = function() {
-    summary_list = list(
-        GUI = .Platform$GUI,
-        os_type = .Platform$OS.type,
-        os_sysname = as.character(Sys.info()["sysname"]),
-        os_version = as.character(Sys.info()["version"]),
-        machine_type = as.character(Sys.info()["machine"]),
-        machine_nodename = as.character(Sys.info()["nodename"]),
-        machine_cpu = if (.Platform$OS.type == "unix" & Sys.info()["sysname"] == "Darwin") 
-            trimws(system("sysctl -n machdep.cpu.brand_string", intern=TRUE))
-        else if (.Platform$OS.type == "unix" & Sys.info()["sysname"] == "Linux") 
-            trimws(system("awk '/model name/' /proc/cpuinfo | uniq | awk -F': ' '{print $2}'", intern=TRUE))
-        else if (Sys.info()["sysname"] == "Windows") 
-            trimws(system("wmic cpu get name", intern=TRUE)[2])
-        else NA,
-        Sys.getlocale = list(
-            LC_COLLATE = Sys.getlocale(category = "LC_COLLATE"), 
-            LC_CTYPE = Sys.getlocale(category = "LC_CTYPE"), 
-            LC_locale_NUMERIC = Sys.getlocale(category = "LC_NUMERIC"), 
-            LC_locale_TIME = Sys.getlocale(category = "LC_TIME")
-        ), 
-        l10n_info = list(
-            localization_UTF8 = l10n_info()$`UTF-8`,
-            localization_Latin1 = l10n_info()$`Latin-1`,  
-            localization_codeset = l10n_info()$codeset,
-            localization_codepage = l10n_info()$codepage,
-            localization_system.codepage = l10n_info()$system.codepage
-        )
-    )
-}
-### |> f_function.load2env.internal(.tmp$objectname, env1_subenv_name) ----
-.tmp$env1_subenv_name = "env.internal"; env1$env.internal$f_function.load2env.internal(function_object = .tmp$object, function_name = .tmp$objectname, env1_subenv_name = .tmp$env1_subenv_name, show_packageStartupMessage = FALSE, function.reload = options()$function.reload, runLoadedFunction = FALSE)
+# #% get_system_info() ====  
+# .tmp$objectname = "get_system_info"
+# .tmp$object = function() {
+#     summary_list = list(
+#         GUI = .Platform$GUI,
+#         os_type = .Platform$OS.type,
+#         os_sysname = as.character(Sys.info()["sysname"]),
+#         os_version = as.character(Sys.info()["version"]),
+#         machine_type = as.character(Sys.info()["machine"]),
+#         machine_nodename = as.character(Sys.info()["nodename"]),
+#         machine_cpu = if (.Platform$OS.type == "unix" & Sys.info()["sysname"] == "Darwin") 
+#             trimws(system("sysctl -n machdep.cpu.brand_string", intern=TRUE))
+#         else if (.Platform$OS.type == "unix" & Sys.info()["sysname"] == "Linux") 
+#             trimws(system("awk '/model name/' /proc/cpuinfo | uniq | awk -F': ' '{print $2}'", intern=TRUE))
+#         else if (Sys.info()["sysname"] == "Windows") 
+#             trimws(system("wmic cpu get name", intern=TRUE)[2])
+#         else NA,
+#         Sys.getlocale = list(
+#             LC_COLLATE = Sys.getlocale(category = "LC_COLLATE"), 
+#             LC_CTYPE = Sys.getlocale(category = "LC_CTYPE"), 
+#             LC_locale_NUMERIC = Sys.getlocale(category = "LC_NUMERIC"), 
+#             LC_locale_TIME = Sys.getlocale(category = "LC_TIME")
+#         ), 
+#         l10n_info = list(
+#             localization_UTF8 = l10n_info()$`UTF-8`,
+#             localization_Latin1 = l10n_info()$`Latin-1`,  
+#             localization_codeset = l10n_info()$codeset,
+#             localization_codepage = l10n_info()$codepage,
+#             localization_system.codepage = l10n_info()$system.codepage
+#         )
+#     )
+# }
+# ### |> f_function.load2env.internal(.tmp$objectname, env1_subenv_name) ----
+# .tmp$env1_subenv_name = "env.internal"; env1$env.internal$f_function.load2env.internal(function_object = .tmp$object, function_name = .tmp$objectname, env1_subenv_name = .tmp$env1_subenv_name, show_packageStartupMessage = FALSE, function.reload = options()$function.reload, runLoadedFunction = FALSE)
 
 
 
-#% get_system_info() ====  
-.tmp$objectname = "get_software_versions"
-.tmp$object = function(library_names = c("tidyverse", "dplyr", "ggplot2", "purrr", "stringr")) {
-  version_list <- list(OS.type = .Platform$OS.type, R.version = R.version$version.string)
-  version_list$RStudio_version <- ifelse(!is.null(.Platform$GUI) && .Platform$GUI == "RStudio" && exists("RStudio.Version"), paste(unlist(RStudio.Version()$version), collapse = "."), NA)
-  version_list$library_versions <- setNames(map(library_names, function(lib) {
-    if (!requireNamespace(lib, quietly = TRUE)) return(paste(lib, "not installed"))
-    paste(unlist(packageVersion(lib)), collapse = ".")
-  }), library_names)
-  return(version_list)
-}
-### |> f_function.load2env.internal(.tmp$objectname, env1_subenv_name) ----
-.tmp$env1_subenv_name = "env.internal"; env1$env.internal$f_function.load2env.internal(function_object = .tmp$object, function_name = .tmp$objectname, env1_subenv_name = .tmp$env1_subenv_name, show_packageStartupMessage = FALSE, function.reload = options()$function.reload, runLoadedFunction = FALSE)
+# #% get_software_versions() ====  
+# .tmp$objectname = "get_software_versions"
+# .tmp$object = function(library_names = c("tidyverse", "dplyr", "ggplot2", "purrr", "stringr")) {
+#   version_list <- list(OS.type = .Platform$OS.type, R.version = R.version$version.string)
+#   version_list$RStudio_version <- ifelse(!is.null(.Platform$GUI) && .Platform$GUI == "RStudio" && exists("RStudio.Version"), paste(unlist(RStudio.Version()$version), collapse = "."), NA)
+#   version_list$library_versions <- setNames(map(library_names, function(lib) {
+#     if (!requireNamespace(lib, quietly = TRUE)) return(paste(lib, "not installed"))
+#     paste(unlist(packageVersion(lib)), collapse = ".")
+#   }), library_names)
+#   return(version_list)
+# }
+# ### |> f_function.load2env.internal(.tmp$objectname, env1_subenv_name) ----
+# .tmp$env1_subenv_name = "env.internal"; env1$env.internal$f_function.load2env.internal(function_object = .tmp$object, function_name = .tmp$objectname, env1_subenv_name = .tmp$env1_subenv_name, show_packageStartupMessage = FALSE, function.reload = options()$function.reload, runLoadedFunction = FALSE)
   
   
 # __________|------  
