69bb1a0641032cdfa7c7025c81ffe62054a875de
mkim0710 <mkim0710@gmail.com>
2024-11-22 03:43:10 +0900

    df_NewDMv3.CensorEND.select971.NA.rmAllNA.lbl.fct

--- git/git lfs untrack.r
+++ git/git lfs untrack.r
@@ -1,8 +1,8 @@
 # > system("git reset --soft HEAD~1")
 # > system("git lfs ls-files")
-# 72e13e4995 * df_NewDMv3.CensorEND.select971.lbl.fct.NA.rmAllNA.recode.rds.xz
-# > system('git lfs untrack "df_NewDMv3.CensorEND.select971.lbl.fct.NA.rmAllNA.recode.rds.xz"')
-# > system('git rm --cached "df_NewDMv3.CensorEND.select971.lbl.fct.NA.rmAllNA.recode.rds.xz"')
-# rm 'df_NewDMv3.CensorEND.select971.lbl.fct.NA.rmAllNA.recode.rds.xz'
+# 72e13e4995 * df_NewDMv3.CensorEND.select971.NA.rmAllNA.lbl.fct.recode.rds.xz
+# > system('git lfs untrack "df_NewDMv3.CensorEND.select971.NA.rmAllNA.lbl.fct.recode.rds.xz"')
+# > system('git rm --cached "df_NewDMv3.CensorEND.select971.NA.rmAllNA.lbl.fct.recode.rds.xz"')
+# rm 'df_NewDMv3.CensorEND.select971.NA.rmAllNA.lbl.fct.recode.rds.xz'
 # > system("git reset --soft HEAD~1")
-# > system('git add -f "df_NewDMv3.CensorEND.select971.lbl.fct.NA.rmAllNA.recode.rds.xz"')
+# > system('git add -f "df_NewDMv3.CensorEND.select971.NA.rmAllNA.lbl.fct.recode.rds.xz"')
