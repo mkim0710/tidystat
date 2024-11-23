f92a6d76d453b35c7837ecbf38d42c9410d1a8e8
mhkim@rocker@MAGB760M13700KF <mkim0710@gmail.com>
2024-11-23 09:44:07 +0000

    f_environment.list_objectnames.map_get.str

--- env1$env.internal.source.r
+++ env1$env.internal.source.r
@@ -163,9 +163,11 @@ env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(subenv_name4object, 
 ## :: f_environment.list_objectnames.map_get.str ====  
 .tmp$env1_subenv_name = "f"
 .tmp$objectname = "f_environment.list_objectnames.map_get.str"
-env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(envir = parent.frame(), all.names = FALSE, pattern, sorted = TRUE, run_dput = FALSE, run_str = TRUE, max.level = 2, give.attr = FALSE, return_map_get = FALSE) {
-    list_objectnames <- ls(envir = envir, all.names = all.names, pattern = pattern, sorted = sorted)   # Get object names, including hidden ones if all.names = TRUE
+env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(environment = parent.frame(), all.names = FALSE, pattern, sorted = TRUE, run_dput = FALSE, run_str = TRUE, max.level = 2, give.attr = FALSE, return_map_get = FALSE) {
+    # environment.list = environment |> as.list(all.names = all.names, sorted = sorted)
+    list_objectnames <- ls(envir = environment, all.names = all.names, pattern = pattern, sorted = sorted)   # Get object names, including hidden ones if all.names = TRUE
     if (run_dput)  list_objectnames |> deparse(width.cutoff = 500L) |> cat("  \n", sep="")
+    
     list_objectnames.map_get = list_objectnames |> set_names() |> map(get)
     if (run_str)   list_objectnames.map_get |> str(max.level = max.level, give.attr = give.attr)
     
@@ -182,7 +184,7 @@ env1$env.internal.attach$f_env1_subenv_objectname.set_alias(subenv_name4object =
 ## ::OPTION:: f_environment.list_objectnames_incl_hidden.map_get.str  ----  
 .tmp$env1_subenv_name = "f"
 .tmp$objectname = "f_environment.list_objectnames_incl_hidden.map_get.str"
-env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(envir = parent.frame(), all.names = TRUE, pattern, sorted = TRUE, run_dput = FALSE, run_str = TRUE, max.level = 2, give.attr = FALSE, return_map_get = FALSE) {
+env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(environment = parent.frame(), all.names = TRUE, pattern, sorted = TRUE, run_dput = FALSE, run_str = TRUE, max.level = 2, give.attr = FALSE, return_map_get = FALSE) {
     # args <- as.list(environment())  # Capture all arguments in the current environment
     list_objectnames <- ls(envir = environment(), all.names = TRUE)  # Use ls(all.names = TRUE) to list all objects, including hidden ones
     args <- mget(list_objectnames, envir = environment())    # Use mget() to get all these objects as a list
@@ -1514,6 +1516,30 @@ env1$f$f.updateTemplates = function(.path4APPDATA_RStudio = NULL) {
 #|________________________________________________________________________________|#  ----  
 ##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 # @@ source() ----  
+##________________________________________________________________________________  
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+## :: f_sourcePath.execute_if_not_sourced =  ----  
+.tmp$env1_subenv_name = "env.internal.attach"
+.tmp$objectname = "f_sourcePath.execute_if_not_sourced"
+env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(.subpath_filename.source.r = NULL, .subpath = NULL, .sourcename = NULL,  option.function.reload = isTRUE(options()$function.reload)) {
+    if(is.null(.subpath_filename.source.r) && !is.null(.subpath) && !is.null(.sourcename)) .subpath_filename.source.r = .sourcename %>% paste0(.subpath,ifelse(.subpath=="","","/"),.)
+    if(option.function.reload || !.sourcename %in% names(.GlobalEnv$env1$source)) {  message('> source("',file.path(env1$path$source_base,.subpath_filename.source.r),'")'); .GlobalEnv$env1$source[[.sourcename]] = file.path(env1$path$source_base,.subpath_filename.source.r); source(.GlobalEnv$env1$source[[.sourcename]])  }
+}
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+### & alias = source_if_not_sourced  ----  
+env1$env.internal.attach$f_env1_subenv_objectname.set_alias(subenv_name4object = .tmp$env1_subenv_name, objectname = .tmp$objectname, subenv_name4alias = "env.internal.attach", aliasname = "source_if_not_sourced")
+
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  
+## ::OPTION:: f_sourcePath.execute.force_reload  ----  
+.tmp$env1_subenv_name = "f"
+.tmp$objectname = "f_sourcePath.execute.force_reload"
+env1[[.tmp$env1_subenv_name]][[.tmp$objectname]] = function(envir = parent.frame(), all.names = TRUE, pattern, sorted = TRUE, run_dput = FALSE, run_str = TRUE, max.level = 2, give.attr = FALSE, return_map_get = FALSE) {
+    # args <- as.list(environment())  # Capture all arguments in the current environment
+    list_objectnames <- ls(envir = environment(), all.names = TRUE)  # Use ls(all.names = TRUE) to list all objects, including hidden ones
+    args <- mget(list_objectnames, envir = environment())    # Use mget() to get all these objects as a list
+    do.call(env1$f$f_env1_subenv_objectname.set_alias, args)   # Dynamically pass the args to another function
+}
+
 ##________________________________________________________________________________  
 ##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 # ## @ .sourcename = "internal.f_path0.list_path_hierarchy" |> paste0(".source.r") =======  
