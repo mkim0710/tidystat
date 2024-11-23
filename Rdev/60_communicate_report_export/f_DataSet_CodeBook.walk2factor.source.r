da5bc41be52275e332c85bc1e412c9a3ec62ec8b
mkim0710 <mkim0710@gmail.com>
2024-11-23 23:31:33 +0900

    env1$f$f_sourcePath.execute_if_not_sourced(.subpath_filename.source.r = paste0(.subpath,ifelse(.subpath=="","","/"),.filename.source.r))

--- Rdev/60_communicate_report_export/f_DataSet_CodeBook.walk2factor.source.r
+++ Rdev/60_communicate_report_export/f_DataSet_CodeBook.walk2factor.source.r
@@ -199,7 +199,7 @@ if(!is.null(env1$path$LastSourceEditorContext.path)) env1$path$.path4write = .pa
 # if(.subpath!="") utils::browseURL(normalizePath(.subpath))
 .filename.source.r = "f_DataSet_CodeBook.walk2factor.source.r"
 ### \% source( file.path(env1$path$source_base,.subpath_filename.source.r) ) ----  
-# .subpath_filename.source.r = .filename.source.r %>% paste0(.subpath,ifelse(.subpath=="","","/"),.); if(!.filename.source.r %in% names(.GlobalEnv$env1$source)) {message('> source("',file.path(env1$path$source_base,.subpath_filename.source.r),'")'); source( file.path(env1$path$source_base,.subpath_filename.source.r) ); .GlobalEnv$env1$source[[.filename.source.r]] = TRUE}
+# env1$f$f_sourcePath.execute_if_not_sourced(.subpath_filename.source.r = paste0(.subpath,ifelse(.subpath=="","","/"),.filename.source.r))
 
 
 
