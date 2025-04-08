9c02c7a3c2c9147bf2d9e15519ff2da47e81f7f0
mkim0710 <mkim0710@gmail.com>
2025-04-08 18:34:37 +0900

    Create fSys-fEnv-source.R

---
+++ R/fSys-fEnv-source.R
@@ -0,0 +1,34 @@
+##HHHHHHHHHHHHHHHHHH BEGINNING OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
+# TABLE OF CONTENTS (level 1) ----  
+# @@ system, environment                                                    #L312
+# @@ f_function, f_expression, f_CodeText                                   #L555
+# @@ f_file, f_URL, f_path                                                  #L1006
+# @@ f_RegEx                                                                #L2928
+# @@ source()                                                               #L2990
+# \% source( file.path(env1$path$source_base,.RelativeSubPath_FileName.source.r) )       #L3118
+##HHHHHHHHHHHHHHHHHHHH THE END OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
+##HHHHHHHHHHHHHHHHHH BEGINNING OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
+# TABLE OF CONTENTS (level 2) ----  
+## .GlobalEnv$env1 = new.env()                                              #L273
+# @@ system, environment                                                    #L312
+## :: get_system_info                                                       #L315
+## :: get_software_versions                                                 #L348
+## :: warnings.summary                                                      #L359
+## :: warnings.last                                                         #L360
+## :: warnings.last10                                                       #L361
+## :: message_if_VERBOSE                                                    #L366
+## :: cat_if_VERBOSE                                                        #L371
+## :: f_env1_subenv_objectname.set_ALIAS                                    #L376
+## :: f_environment.list_objectnames.map_get.str                            #L387
+## ::OPTION:: f_environment.list_objectnames_incl_hidden.map_get.str        #L410
+## :: library.load.install_if_not_already_installed                         #L420
+## :: library.load.warning_if_not_available                                 #L437
+## :: requireNamespace.library_load_if_necessary                            #L458
+## :: f_environment.detach_and_reattach                                     #L481
+## :: reattach.env1_f_env.internal.attach                                   #L495
+## :: f_environment.attached_search_path.dput                               #L503
+## :: Sys.time.set_t0                                                       #L514
+## :: Sys.time.set_t1                                                       #L519
+## :: f_object.is_null.or_na.or_blank =                                     #L527
+## :: f_object.is_not_null.nor_na.nor_blank =                               #L540
+##HHHHHHHHHHHHHHHHHHHH THE END OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
