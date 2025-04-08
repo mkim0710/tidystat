d7f75ce10f5a13fd23da9f4171605cdbced875cd
mkim0710 <mkim0710@gmail.com>
2025-04-08 18:34:41 +0900

    Create fChr-fFile-fPath-fURL.R

---
+++ R/fChr-fFile-fPath-fURL.R
@@ -0,0 +1,61 @@
+##HHHHHHHHHHHHHHHHHH BEGINNING OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
+# TABLE OF CONTENTS (level 1) ----  
+# @@ system, environment                                                    #L312
+# @@ f_function, f_expression, f_CodeText                                   #L555
+# @@ f_file, f_URL, f_path                                                  #L1006
+# @@ f_RegEx                                                                #L2928
+# @@ source()                                                               #L2990
+# \% source( file.path(env1$path$source_base,.RelativeSubPath_FileName.source.r) )       #L3118
+##HHHHHHHHHHHHHHHHHHHH THE END OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
+##HHHHHHHHHHHHHHHHHH BEGINNING OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
+# TABLE OF CONTENTS (level 2) ----  
+# @@ f_file, f_URL, f_path                                                  #L1006
+## :: getSourceEditorContext.path_FileNameExt                               #L1009
+## :: f_path_file.paste0_collapse_if_not_empty                              #L1026
+## :: f_path.relative                                                       #L1056
+## :: f_path.URL_github.browse_in_edge_app                                  #L1071
+## :: f_path_relative.path_normalized_based_on_path1                        #L1099
+## :: getSourceEditorContext.update_LastSourceEditorContext.path_FileNameExt =        #L1118
+## :: f_path.file.info                                                      #L1181
+## :: f_list_df.write_xlsx_table                                            #L1230
+## :: f_path.file.info.xlsx                                                 #L1281
+## ::OPTION:: f_path.documents.file.info.xlsx                               #L1339
+## :: f_path.DOCs_Rcode.file.info.xlsx                                      #L1352
+## :: f_path.size_files =                                                   #L1416
+## :: f_path0.list_path_hierarchy =                                         #L1468
+## :: f_vec_path.vec_FileName_sans_ext_extended =                           #L1507
+## :: f_vec_path.vec_ext_extended =                                         #L1530
+## :: f_path.df_FileNameExt_extended =                                      #L1549
+## :: f_path_fileRegEX.rename =                                             #L1588
+## :: f_path.list.files_orphan_nb_html.remove                               #L1659
+## :: f_FileNameExt.append_suffix =                                         #L1721
+## :: f_file.vec_TABLE_OF_CONTENTS.trim.add_line_numbers.browseTXT =        #L1755
+## :: f_file.vec_TABLE_OF_CONTENTS.NESTED =                                 #L1835
+## :: f_vec_chr.df_token_n_rowNum = function()                              #L1929
+## :: f_file.df_token_n_rowNum = function()                                 #L2019
+## :: f_file.str_replace_all.old.ObjectName =                               #L2060
+## ::OPTION:: f_CurrentSourceEditorContext.str_replace_all.old.ObjectName        #L2104
+## @ f_file.edit, f_file.open, f_URL.open                                   #L2126
+## :: f_file.edit_if_exists                                                 #L2128
+## ::OPTION:: f_file.edit_if_exists.return2LastSourceEditorContext          #L2167
+## :: f_file.edit_windows.or_browseURL                                      #L2183
+## :: f_URL.browse_in_edge =                                                #L2215
+## :: f_URL.browse_in_brave -PENDING                                        #L2305
+## :: f_file.systemStart                                                    #L2312
+## :: f_file_PDF.sumatra                                                    #L2324
+## :: f_file.system_switch_open                                             #L2367
+## :: f_file_xls.open_or_browseURL                                          #L2397
+## @ f_file.BACKUP, f_path.BACKUP                                           #L2418
+## :: f_FileNameExt.createBACKUP                                            #L2420
+## :: f_path_path.BACKUP.overwrite                                          #L2459
+## @ f_git                                                                  #L2474
+## :: f_path.is_git_tracked                                                 #L2477
+## :: f_file.git_lfs_track_add_f                                            #L2528
+## :: function_name = f_gitattributes.split()                               #L2570
+## @ f_file.save                                                            #L2607
+## :: f_objectname.size.write_rds.git_lfs_track_add_f                       #L2610
+## :: f_object.get_objectname.size.write_rds.git_lfs_track_add_f  - PENDING       #L2745
+## @ f_file.update                                                          #L2769
+## :: env.internal\$f_file2.compare                                         #L2772
+## :: env.internal\$f_url_destfile.DownloadIfDifferent                      #L2809
+##HHHHHHHHHHHHHHHHHHHH THE END OF TABLE OF CONTENTS HHHHHHHHHHHHHHHHHHHHHH##  
