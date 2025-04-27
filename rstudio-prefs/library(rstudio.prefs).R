2cbf9a9d181cec56366817be90fe71d8ebe6f853
mhkim@rocker@MAGB760M13700KF <mkim0710@gmail.com>
2025-04-27 15:49:39 +0000

    library(rstudio.prefs).R

---
+++ rstudio-prefs/library(rstudio.prefs).R
@@ -0,0 +1,7 @@
+# https://www.danieldsjoberg.com/rstudio.prefs/
+
+if(!require("rstudio.prefs") ) { install.packages("rstudio.prefs") }
+
+library(rstudio.prefs)
+pref_list <- list(editor_scroll_multiplier = 30L)
+use_rstudio_prefs(!!!pref_list)   # note the !!! to splice the list
