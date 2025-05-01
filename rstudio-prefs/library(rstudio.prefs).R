901e72ccb708058a1c750ab2ae0e46df28750ab8
mhkim@rocker@MAGB760M13700KF <mkim0710@gmail.com>
2025-05-01 13:08:34 +0000

    update

--- rstudio-prefs/library(rstudio.prefs).R
+++ rstudio-prefs/library(rstudio.prefs).R
@@ -3,5 +3,5 @@
 if(!require("rstudio.prefs") ) { install.packages("rstudio.prefs") }
 
 library(rstudio.prefs)
-pref_list <- list(editor_scroll_multiplier = 30L)
-use_rstudio_prefs(!!!pref_list)   # note the !!! to splice the list
+# pref_list <- list(editor_scroll_multiplier = 30L)
+# use_rstudio_prefs(!!!pref_list)   # note the !!! to splice the list
