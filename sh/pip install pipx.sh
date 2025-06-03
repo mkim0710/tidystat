4389553edda6aa6c6ea8ed719a650d524a96fb39
mkim0710 <mkim0710@gmail.com>
2025-06-04 01:50:43 +0900

    Create pip install pipx.sh

---
+++ sh/pip install pipx.sh
@@ -0,0 +1,12 @@
+pip install pipx
+python -m pipx ensurepath
+# PS C:\Users\mkim0> python -m pipx ensurepath
+# C:\Users\mkim0\.local\bin has been been added to PATH, but you need to open a new terminal or re-login for this PATH change to take effect.
+#     Alternatively, you can source your shell's config file with e.g. 'source ~/.bashrc'.
+# C:\Users\mkim0\AppData\Roaming\Python\Python313\Scripts has been been added to PATH, but you need to open a new terminal or re-login for this   
+#     PATH change to take effect. Alternatively, you can source your shell's config file with e.g. 'source ~/.bashrc'.
+
+# You will need to open a new terminal or re-login for the PATH changes to take effect. Alternatively, you can source your shell's config file    
+# with e.g. 'source ~/.bashrc'.
+
+# Otherwise pipx is ready to go! ✨ 🌟 ✨
